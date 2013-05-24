usernameisnull/django

commit fe6664485e8e089eb0ed3a38a8c7b2663c3d753e

Delayed settings.DATABASE_ROUTERS usage by ConnectionRouter
Refs #20474.

django/db/utils.py
@@ -214,14 +214,23 @@ class ConnectionHandler(object):
 
 
 class ConnectionRouter(object):
-    def __init__(self, routers):
-        self.routers = []
-        for r in routers:
+    def __init__(self, routers=None):
+        """
+        If routers is not specified, will default to settings.DATABASE_ROUTERS.
+        """
+        self._routers = routers
+
+    @cached_property
+    def routers(self):
+        if self._routers is None:
+            self._routers = settings.DATABASE_ROUTERS
+        for r in self._routers:
             if isinstance(r, six.string_types):
                 router = import_by_path(r)()
             else:
                 router = r
-            self.routers.append(router)
+            self._routers.append(router)
+        return self._routers
 
     def _router_func(action):
         def _route_db(self, model, **hints):

django/db/__init__.py
@@ -1,6 +1,5 @@
 import warnings
 
-from django.conf import settings
 from django.core import signals
 from django.db.utils import (DEFAULT_DB_ALIAS,
     DataError, OperationalError, IntegrityError, InternalError,
@@ -14,7 +13,7 @@ __all__ = ('backend', 'connection', 'connections', 'router', 'DatabaseError',
 
 connections = ConnectionHandler()
 
-router = ConnectionRouter(settings.DATABASE_ROUTERS)
+router = ConnectionRouter()
 
 # `connection`, `DatabaseError` and `IntegrityError` are convenient aliases
 # for backend bits.

commit 2862e8f60b0abe1d926823451f8687ef3ce37f54

Fixed a regression in router initialization
Regression was introduced in fe6664485. Thanks Bas Peschier for the
report.

django/db/utils.py
@@ -224,13 +224,14 @@ class ConnectionRouter(object):
     def routers(self):
         if self._routers is None:
             self._routers = settings.DATABASE_ROUTERS
+        routers = []
         for r in self._routers:
             if isinstance(r, six.string_types):
                 router = import_by_path(r)()
             else:
                 router = r
-            self._routers.append(router)
-        return self._routers
+            routers.append(router)
+        return routers
 
     def _router_func(action):
         def _route_db(self, model, **hints):

tests/multiple_database/tests.py
@@ -5,11 +5,13 @@ import pickle
 from operator import attrgetter
 import warnings
 
+from django.conf import settings
 from django.contrib.auth.models import User
 from django.contrib.contenttypes.models import ContentType
 from django.core import management
 from django.db import connections, router, DEFAULT_DB_ALIAS
 from django.db.models import signals
+from django.db.utils import ConnectionRouter
 from django.test import TestCase
 from django.test.utils import override_settings
 from django.utils.six import StringIO
@@ -918,6 +920,7 @@ class QueryTestCase(TestCase):
                                   published=datetime.date(2009, 5, 4),
                                   extra_arg=True)
 
+
 class TestRouter(object):
     # A test router. The behavior is vaguely master/slave, but the
     # databases aren't assumed to propagate changes.
@@ -971,6 +974,30 @@ class WriteRouter(object):
     # A router that only expresses an opinion on writes
     def db_for_write(self, model, **hints):
         return 'writer'
+
+
+class ConnectionRouterTestCase(TestCase):
+    @override_settings(DATABASE_ROUTERS=[
+        'multiple_database.tests.TestRouter',
+        'multiple_database.tests.WriteRouter'])
+    def test_router_init_default(self):
+        router = ConnectionRouter()
+        self.assertListEqual([r.__class__.__name__ for r in router.routers],
+                             ['TestRouter', 'WriteRouter'])
+
+    def test_router_init_arg(self):
+        router = ConnectionRouter([
+            'multiple_database.tests.TestRouter',
+            'multiple_database.tests.WriteRouter'
+        ])
+        self.assertListEqual([r.__class__.__name__ for r in router.routers],
+                             ['TestRouter', 'WriteRouter'])
+
+        # Init with instances instead of strings
+        router = ConnectionRouter([TestRouter(), WriteRouter()])
+        self.assertListEqual([r.__class__.__name__ for r in router.routers],
+                             ['TestRouter', 'WriteRouter'])
+
 
 class RouterTestCase(TestCase):
     multi_db = True