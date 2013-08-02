usernameisnull/django

commit 62b5260fe0a8385120190d3f35a0d9fd5d922646

Fixed #18169 -- NoReverseMatch not silenced if from block.super

tests/template_tests/tests.py
@@ -444,6 +444,15 @@ class Templates(TestCase):
         output = template.render(Context({}))
         self.assertEqual(output, '1st time')
 
+    def test_super_errors(self):
+        """
+        Test behavior of the raise errors into included blocks.
+        See #18169
+        """
+        t = loader.get_template('included_content.html')
+        with self.assertRaises(urlresolvers.NoReverseMatch):
+            t.render(Context({}))
+
     def test_templates(self):
         template_tests = self.get_template_tests()
         filter_tests = filters.get_filter_tests()

django/core/urlresolvers.py
@@ -75,8 +75,7 @@ class Resolver404(Http404):
     pass
 
 class NoReverseMatch(Exception):
-    # Don't make this raise an error when used in a template.
-    silent_variable_failure = True
+    pass
 
 def get_callable(lookup_view, can_fail=False):
     """

tests/template_tests/templates/included_base.html
@@ -1,0 +1,3 @@
+{% block content %}
+  {% block error_here %}{% endblock %}
+{% endblock %}

tests/template_tests/templates/included_content.html
@@ -1,0 +1,11 @@
+{% extends "included_base.html" %}
+
+{% block content %}
+  content
+  {{ block.super }}
+{% endblock %}
+
+{% block error_here %}
+  error here
+  {% url "non_existing_url" %}
+{% endblock %}

commit bc1504f8609fa0c62fa1e0b0ca39db1b71536d57

Fixed LogEntry.get_admin_url() for non-existent models.
Regression introduced by [62b5260]; refs #18169.

django/contrib/admin/models.py
@@ -4,7 +4,7 @@ from django.db import models
 from django.conf import settings
 from django.contrib.contenttypes.models import ContentType
 from django.contrib.admin.util import quote
-from django.core.urlresolvers import reverse
+from django.core.urlresolvers import reverse, NoReverseMatch
 from django.utils.translation import ugettext, ugettext_lazy as _
 from django.utils.encoding import smart_text
 from django.utils.encoding import python_2_unicode_compatible
@@ -74,5 +74,8 @@ class LogEntry(models.Model):
         """
         if self.content_type and self.object_id:
             url_name = 'admin:%s_%s_change' % (self.content_type.app_label, self.content_type.model)
-            return reverse(url_name, args=(quote(self.object_id),))
+            try:
+                return reverse(url_name, args=(quote(self.object_id),))
+            except NoReverseMatch:
+                pass
         return None

tests/admin_views/tests.py
@@ -1533,6 +1533,17 @@ class AdminViewStringPrimaryKeyTest(TestCase):
         self.assertEqual(counted_presence_before - 1,
                           counted_presence_after)
 
+    def test_logentry_get_admin_url(self):
+        "LogEntry.get_admin_url returns a URL to edit the entry's object or None for non-existent (possibly deleted) models"
+        log_entry_name = "Model with string primary key"  # capitalized in Recent Actions
+        logentry = LogEntry.objects.get(content_type__name__iexact=log_entry_name)
+        model = "modelwithstringprimarykey"
+        desired_admin_url = "/test_admin/admin/admin_views/%s/%s/" % (model, escape(iri_to_uri(urlquote(quote(self.pk)))))
+        self.assertEqual(logentry.get_admin_url(), desired_admin_url)
+
+        logentry.content_type.model = "non-existent"
+        self.assertEqual(logentry.get_admin_url(), None)
+
     def test_deleteconfirmation_link(self):
         "The link from the delete confirmation page referring back to the changeform of the object should be quoted"
         response = self.client.get('/test_admin/admin/admin_views/modelwithstringprimarykey/%s/delete/' % quote(self.pk))