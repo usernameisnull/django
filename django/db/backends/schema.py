usernameisnull/django

commit 4f767b07b933e81258679cd7007f8fda5117a98f

Adding 'sqlmigrate' command and quote_parameter to support it.

django/db/backends/schema.py
@@ -54,14 +54,17 @@ class BaseDatabaseSchemaEditor(object):
     sql_create_fk = "ALTER TABLE %(table)s ADD CONSTRAINT %(name)s FOREIGN KEY (%(column)s) REFERENCES %(to_table)s (%(to_column)s) DEFERRABLE INITIALLY DEFERRED"
     sql_delete_fk = "ALTER TABLE %(table)s DROP CONSTRAINT %(name)s"
 
-    sql_create_index = "CREATE INDEX %(name)s ON %(table)s (%(columns)s)%(extra)s;"
+    sql_create_index = "CREATE INDEX %(name)s ON %(table)s (%(columns)s)%(extra)s"
     sql_delete_index = "DROP INDEX %(name)s"
 
     sql_create_pk = "ALTER TABLE %(table)s ADD CONSTRAINT %(name)s PRIMARY KEY (%(columns)s)"
     sql_delete_pk = "ALTER TABLE %(table)s DROP CONSTRAINT %(name)s"
 
-    def __init__(self, connection):
+    def __init__(self, connection, collect_sql=False):
         self.connection = connection
+        self.collect_sql = collect_sql
+        if self.collect_sql:
+            self.collected_sql = []
 
     # State-managing methods
 
@@ -86,7 +89,10 @@ class BaseDatabaseSchemaEditor(object):
         cursor = self.connection.cursor()
         # Log the command we're running, then run it
         logger.debug("%s; (params %r)" % (sql, params))
-        cursor.execute(sql, params)
+        if self.collect_sql:
+            self.collected_sql.append((sql % map(self.connection.ops.quote_parameter, params)) + ";")
+        else:
+            cursor.execute(sql, params)
 
     def quote_name(self, name):
         return self.connection.ops.quote_name(name)

django/db/backends/oracle/schema.py
@@ -93,11 +93,4 @@ class DatabaseSchemaEditor(BaseDatabaseSchemaEditor):
         return self.normalize_name(for_name + "_" + suffix)
 
     def prepare_default(self, value):
-        if isinstance(value, (datetime.date, datetime.time, datetime.datetime)):
-            return "'%s'" % value
-        elif isinstance(value, six.string_types):
-            return repr(value)
-        elif isinstance(value, bool):
-            return "1" if value else "0"
-        else:
-            return str(value)
+        return self.connection.ops.quote_parameter(value)

django/db/backends/sqlite3/schema.py
@@ -55,7 +55,7 @@ class DatabaseSchemaEditor(BaseDatabaseSchemaEditor):
         self.create_model(temp_model)
         # Copy data from the old table
         field_maps = list(mapping.items())
-        self.execute("INSERT INTO %s (%s) SELECT %s FROM %s;" % (
+        self.execute("INSERT INTO %s (%s) SELECT %s FROM %s" % (
             self.quote_name(temp_model._meta.db_table),
             ', '.join(x for x, y in field_maps),
             ', '.join(y for x, y in field_maps),
@@ -137,7 +137,7 @@ class DatabaseSchemaEditor(BaseDatabaseSchemaEditor):
         # Make a new through table
         self.create_model(new_field.rel.through)
         # Copy the data across
-        self.execute("INSERT INTO %s (%s) SELECT %s FROM %s;" % (
+        self.execute("INSERT INTO %s (%s) SELECT %s FROM %s" % (
             self.quote_name(new_field.rel.through._meta.db_table),
             ', '.join([
                 "id",

django/core/management/commands/sqlmigrate.py
@@ -1,0 +1,52 @@
+# encoding: utf8
+from __future__ import unicode_literals
+from optparse import make_option
+
+from django.core.management.base import BaseCommand, CommandError
+from django.db import connections, DEFAULT_DB_ALIAS
+from django.db.migrations.executor import MigrationExecutor
+from django.db.migrations.loader import AmbiguityError
+
+
+class Command(BaseCommand):
+
+    option_list = BaseCommand.option_list + (
+        make_option('--database', action='store', dest='database',
+            default=DEFAULT_DB_ALIAS, help='Nominates a database to create SQL for. '
+                'Defaults to the "default" database.'),
+        make_option('--backwards', action='store_true', dest='backwards',
+            default=False, help='Creates SQL to unapply the migration, rather than to apply it'),
+    )
+
+    help = "Prints the SQL statements for the named migration."
+
+    def handle(self, *args, **options):
+
+        # Get the database we're operating from
+        db = options.get('database')
+        connection = connections[db]
+
+        # Load up an executor to get all the migration data
+        executor = MigrationExecutor(connection)
+
+        # Resolve command-line arguments into a migration
+        if len(args) != 2:
+            raise CommandError("Wrong number of arguments (expecting 'sqlmigrate appname migrationname')")
+        else:
+            app_label, migration_name = args
+            if app_label not in executor.loader.migrated_apps:
+                raise CommandError("App '%s' does not have migrations" % app_label)
+            try:
+                migration = executor.loader.get_migration_by_prefix(app_label, migration_name)
+            except AmbiguityError:
+                raise CommandError("More than one migration matches '%s' in app '%s'. Please be more specific." % (app_label, migration_name))
+            except KeyError:
+                raise CommandError("Cannot find a migration matching '%s' from app '%s'. Is it in INSTALLED_APPS?" % (app_label, migration_name))
+            targets = [(app_label, migration.name)]
+
+        # Make a plan that represents just the requested migrations and show SQL
+        # for it
+        plan = [(executor.loader.graph.nodes[targets[0]], options.get("backwards", False))]
+        sql_statements = executor.collect_sql(plan)
+        for statement in sql_statements:
+            self.stdout.write(statement)

django/db/backends/__init__.py
@@ -521,7 +521,7 @@ class BaseDatabaseWrapper(object):
         """
         raise NotImplementedError
 
-    def schema_editor(self):
+    def schema_editor(self, *args, **kwargs):
         "Returns a new instance of this backend's SchemaEditor"
         raise NotImplementedError()
 
@@ -955,6 +955,15 @@ class BaseDatabaseOperations(object):
         """
         Returns a quoted version of the given table, index or column name. Does
         not quote the given name if it's already been quoted.
+        """
+        raise NotImplementedError()
+
+    def quote_parameter(self, value):
+        """
+        Returns a quoted version of the value so it's safe to use in an SQL
+        string. This should NOT be used to prepare SQL statements to send to
+        the database; it is meant for outputting SQL statements to a file
+        or the console for later execution by a developer/DBA.
         """
         raise NotImplementedError()
 

django/db/backends/mysql/base.py
@@ -305,6 +305,11 @@ class DatabaseOperations(BaseDatabaseOperations):
             return name # Quoting once is enough.
         return "`%s`" % name
 
+    def quote_parameter(self, value):
+        # Inner import to allow module to fail to load gracefully
+        import MySQLdb.converters
+        return MySQLdb.escape(value, MySQLdb.converters.conversions)
+
     def random_function_sql(self):
         return 'RAND()'
 
@@ -518,9 +523,9 @@ class DatabaseWrapper(BaseDatabaseWrapper):
                         table_name, column_name, bad_row[1],
                         referenced_table_name, referenced_column_name))
 
-    def schema_editor(self):
+    def schema_editor(self, *args, **kwargs):
         "Returns a new instance of this backend's SchemaEditor"
-        return DatabaseSchemaEditor(self)
+        return DatabaseSchemaEditor(self, *args, **kwargs)
 
     def is_usable(self):
         try:

django/db/backends/oracle/base.py
@@ -320,6 +320,16 @@ WHEN (new.%(col_name)s IS NULL)
         name = name.replace('%', '%%')
         return name.upper()
 
+    def quote_parameter(self, value):
+        if isinstance(value, (datetime.date, datetime.time, datetime.datetime)):
+            return "'%s'" % value
+        elif isinstance(value, six.string_types):
+            return repr(value)
+        elif isinstance(value, bool):
+            return "1" if value else "0"
+        else:
+            return str(value)
+
     def random_function_sql(self):
         return "DBMS_RANDOM.RANDOM"
 
@@ -628,9 +638,9 @@ class DatabaseWrapper(BaseDatabaseWrapper):
                     six.reraise(utils.IntegrityError, utils.IntegrityError(*tuple(e.args)), sys.exc_info()[2])
                 raise
 
-    def schema_editor(self):
+    def schema_editor(self, *args, **kwargs):
         "Returns a new instance of this backend's SchemaEditor"
-        return DatabaseSchemaEditor(self)
+        return DatabaseSchemaEditor(self, *args, **kwargs)
 
     # Oracle doesn't support savepoint commits.  Ignore them.
     def _savepoint_commit(self, sid):

django/db/backends/postgresql_psycopg2/base.py
@@ -205,9 +205,9 @@ class DatabaseWrapper(BaseDatabaseWrapper):
         else:
             return True
 
-    def schema_editor(self):
+    def schema_editor(self, *args, **kwargs):
         "Returns a new instance of this backend's SchemaEditor"
-        return DatabaseSchemaEditor(self)
+        return DatabaseSchemaEditor(self, *args, **kwargs)
 
     @cached_property
     def psycopg2_version(self):

commit 9c06f5b0f28c85d42bfa1463ebef3585c6ec4b53

Fixed regression introduced by 4f767b0, 'map' returns an iterator on PY3.

django/db/backends/schema.py
@@ -90,7 +90,7 @@ class BaseDatabaseSchemaEditor(object):
         # Log the command we're running, then run it
         logger.debug("%s; (params %r)" % (sql, params))
         if self.collect_sql:
-            self.collected_sql.append((sql % map(self.connection.ops.quote_parameter, params)) + ";")
+            self.collected_sql.append((sql % list(map(self.connection.ops.quote_parameter, params))) + ";")
         else:
             cursor.execute(sql, params)
 