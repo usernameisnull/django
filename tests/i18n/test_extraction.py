usernameisnull/django

commit e883c0b21e84b00111e7b4cc5e39fd3f47f8657e

Fixed #20422 -- Applied makemessage's --ignore patterns to full path
Fix makemessage's --ignore patterns being applied to the full path
instead of the file name. Thanks to nnseva for the report and the
original patch.

django/core/management/commands/makemessages.py
@@ -309,10 +309,9 @@ class Command(NoArgsCommand):
             """
             Check if the given path should be ignored or not.
             """
-            for pattern in ignore_patterns:
-                if fnmatch.fnmatchcase(path, pattern):
-                    return True
-            return False
+            filename = os.path.basename(path)
+            ignore = lambda pattern: fnmatch.fnmatchcase(filename, pattern)
+            return any(ignore(pattern) for pattern in ignore_patterns)
 
         dir_suffix = '%s*' % os.sep
         norm_patterns = [p[:-len(dir_suffix)] if p.endswith(dir_suffix) else p for p in self.ignore_patterns]

tests/i18n/commands/extraction.py
@@ -279,17 +279,22 @@ class IgnoredExtractorTests(ExtractorTests):
 
     def test_ignore_option(self):
         os.chdir(self.test_dir)
-        pattern1 = os.path.join('ignore_dir', '*')
+        ignore_patterns = [
+            os.path.join('ignore_dir', '*'),
+            'xxx_*',
+        ]
         stdout = StringIO()
         management.call_command('makemessages', locale=LOCALE, verbosity=2,
-            ignore_patterns=[pattern1], stdout=stdout)
+            ignore_patterns=ignore_patterns, stdout=stdout)
         data = stdout.getvalue()
         self.assertTrue("ignoring directory ignore_dir" in data)
+        self.assertTrue("ignoring file xxx_ignored.html" in data)
         self.assertTrue(os.path.exists(self.PO_FILE))
         with open(self.PO_FILE, 'r') as fp:
             po_contents = fp.read()
             self.assertMsgId('This literal should be included.', po_contents)
             self.assertNotMsgId('This should be ignored.', po_contents)
+            self.assertNotMsgId('This should be ignored too.', po_contents)
 
 
 class SymlinkExtractorTests(ExtractorTests):

tests/i18n/commands/templates/xxx_ignored.html
@@ -1,0 +1,2 @@
+{% load i18n %}
+{% trans "This should be ignored too." %}

commit 68ba0533536ca24253235cf477098e9522a22a4c

Fixed #22336 -- Added path matching for makemessages ignore option
This fixes a regression introduced by e883c0b21e.

django/core/management/commands/makemessages.py
@@ -360,7 +360,8 @@ class Command(BaseCommand):
             Check if the given path should be ignored or not.
             """
             filename = os.path.basename(path)
-            ignore = lambda pattern: fnmatch.fnmatchcase(filename, pattern)
+            ignore = lambda pattern: (fnmatch.fnmatchcase(filename, pattern) or
+                fnmatch.fnmatchcase(path, pattern))
             return any(ignore(pattern) for pattern in ignore_patterns)
 
         dir_suffix = '%s*' % os.sep

tests/i18n/commands/templates/subdir/ignored.html
@@ -1,0 +1,2 @@
+{% load i18n %}
+{% trans "This subdir should be ignored too." %}

tests/i18n/test_extraction.py
@@ -360,35 +360,47 @@ class JavascriptExtractorTests(ExtractorTests):
 
 class IgnoredExtractorTests(ExtractorTests):
 
-    def test_ignore_option(self):
+    def _run_makemessages(self, **options):
         os.chdir(self.test_dir)
-        ignore_patterns = [
-            os.path.join('ignore_dir', '*'),
-            'xxx_*',
-        ]
         stdout = StringIO()
         management.call_command('makemessages', locale=[LOCALE], verbosity=2,
-            ignore_patterns=ignore_patterns, stdout=stdout)
+            stdout=stdout, **options)
         data = stdout.getvalue()
-        self.assertTrue("ignoring directory ignore_dir" in data)
-        self.assertTrue("ignoring file xxx_ignored.html" in data)
         self.assertTrue(os.path.exists(self.PO_FILE))
         with open(self.PO_FILE, 'r') as fp:
             po_contents = fp.read()
-            self.assertMsgId('This literal should be included.', po_contents)
-            self.assertNotMsgId('This should be ignored.', po_contents)
-            self.assertNotMsgId('This should be ignored too.', po_contents)
+        return data, po_contents
+
+    def test_ignore_directory(self):
+        out, po_contents = self._run_makemessages(ignore_patterns=[
+            os.path.join('ignore_dir', '*'),
+        ])
+        self.assertTrue("ignoring directory ignore_dir" in out)
+        self.assertMsgId('This literal should be included.', po_contents)
+        self.assertNotMsgId('This should be ignored.', po_contents)
+
+    def test_ignore_subdirectory(self):
+        out, po_contents = self._run_makemessages(ignore_patterns=[
+            'templates/*/ignore.html',
+            'templates/subdir/*',
+        ])
+        self.assertTrue("ignoring directory subdir" in out)
+        self.assertNotMsgId('This subdir should be ignored too.', po_contents)
+
+    def test_ignore_file_patterns(self):
+        out, po_contents = self._run_makemessages(ignore_patterns=[
+            'xxx_*',
+        ])
+        self.assertTrue("ignoring file xxx_ignored.html" in out)
+        self.assertNotMsgId('This should be ignored too.', po_contents)
 
     @override_settings(
         STATIC_ROOT=os.path.join(this_directory, 'commands', 'static_root/'),
         MEDIA_ROOT=os.path.join(this_directory, 'commands', 'media_root/'))
     def test_media_static_dirs_ignored(self):
-        os.chdir(self.test_dir)
-        stdout = StringIO()
-        management.call_command('makemessages', locale=[LOCALE], verbosity=2, stdout=stdout)
-        data = stdout.getvalue()
-        self.assertIn("ignoring directory static_root", data)
-        self.assertIn("ignoring directory media_root", data)
+        out, _ = self._run_makemessages()
+        self.assertIn("ignoring directory static_root", out)
+        self.assertIn("ignoring directory media_root", out)
 
 
 class SymlinkExtractorTests(ExtractorTests):