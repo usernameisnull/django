usernameisnull/django

commit 802d2bc1a614231f9f35bf36096ffc9b1fee9b65

Ignored STATIC_ROOT and MEDIA_ROOT in makemessages
Also alleviate issues with weird file names typically found in
MEDIA_ROOT directories (#23010).
Thanks Tim Graham for the review.

django/core/management/commands/makemessages.py
@@ -9,6 +9,7 @@ import sys
 from itertools import dropwhile
 
 import django
+from django.conf import settings
 from django.core.management.base import CommandError, BaseCommand
 from django.core.management.utils import (handle_extensions, find_command,
     popen_wrapper)
@@ -56,8 +57,6 @@ class TranslatableFile(object):
 
         Uses the xgettext GNU gettext utility.
         """
-
-        from django.conf import settings
         from django.utils.translation import templatize
 
         if command.verbosity > 1:
@@ -218,9 +217,20 @@ class Command(BaseCommand):
         process_all = options.get('all')
         extensions = options.get('extensions')
         self.symlinks = options.get('symlinks')
+
+        # Need to ensure that the i18n framework is enabled
+        if settings.configured:
+            settings.USE_I18N = True
+        else:
+            settings.configure(USE_I18N=True)
+
         ignore_patterns = options.get('ignore_patterns')
         if options.get('use_default_ignore_patterns'):
             ignore_patterns += ['CVS', '.*', '*~', '*.pyc']
+        base_path = os.path.abspath('.')
+        for path in (settings.MEDIA_ROOT, settings.STATIC_ROOT):
+            if path and path.startswith(base_path):
+                ignore_patterns.append('%s*' % path[len(base_path) + 1:])
         self.ignore_patterns = list(set(ignore_patterns))
 
         # Avoid messing with mutable class variables
@@ -250,13 +260,6 @@ class Command(BaseCommand):
         if (locale is None and not exclude and not process_all) or self.domain is None:
             raise CommandError("Type '%s help %s' for usage information." % (
                 os.path.basename(sys.argv[0]), sys.argv[1]))
-
-        # Need to ensure that the i18n framework is enabled
-        from django.conf import settings
-        if settings.configured:
-            settings.USE_I18N = True
-        else:
-            settings.configure(USE_I18N=True)
 
         if self.verbosity > 1:
             self.stdout.write('examining files with the extensions: %s\n'

tests/i18n/test_extraction.py
@@ -25,12 +25,13 @@ from django.utils.translation import TranslatorCommentWarning
 
 LOCALE = 'de'
 has_xgettext = find_command('xgettext')
+this_directory = os.path.dirname(upath(__file__))
 
 
 @skipUnless(has_xgettext, 'xgettext is mandatory for extraction tests')
 class ExtractorTests(SimpleTestCase):
 
-    test_dir = os.path.abspath(os.path.join(os.path.dirname(upath(__file__)), 'commands'))
+    test_dir = os.path.abspath(os.path.join(this_directory, 'commands'))
 
     PO_FILE = 'locale/%s/LC_MESSAGES/django.po' % LOCALE
 
@@ -378,6 +379,17 @@ class IgnoredExtractorTests(ExtractorTests):
             self.assertNotMsgId('This should be ignored.', po_contents)
             self.assertNotMsgId('This should be ignored too.', po_contents)
 
+    @override_settings(
+        STATIC_ROOT=os.path.join(this_directory, 'commands', 'static_root/'),
+        MEDIA_ROOT=os.path.join(this_directory, 'commands', 'media_root/'))
+    def test_media_static_dirs_ignored(self):
+        os.chdir(self.test_dir)
+        stdout = StringIO()
+        management.call_command('makemessages', locale=[LOCALE], verbosity=2, stdout=stdout)
+        data = stdout.getvalue()
+        self.assertIn("ignoring directory static_root", data)
+        self.assertIn("ignoring directory media_root", data)
+
 
 class SymlinkExtractorTests(ExtractorTests):
 
@@ -550,7 +562,7 @@ class ExcludedLocaleExtractionTests(ExtractorTests):
     LOCALES = ['en', 'fr', 'it']
     PO_FILE = 'locale/%s/LC_MESSAGES/django.po'
 
-    test_dir = os.path.abspath(os.path.join(os.path.dirname(upath(__file__)), 'exclude'))
+    test_dir = os.path.abspath(os.path.join(this_directory, 'exclude'))
 
     def _set_times_for_all_po_files(self):
         """
@@ -608,7 +620,7 @@ class CustomLayoutExtractionTests(ExtractorTests):
 
     def setUp(self):
         self._cwd = os.getcwd()
-        self.test_dir = os.path.join(os.path.dirname(upath(__file__)), 'project_dir')
+        self.test_dir = os.path.join(this_directory, 'project_dir')
 
     def test_no_locale_raises(self):
         os.chdir(self.test_dir)
@@ -618,7 +630,7 @@ class CustomLayoutExtractionTests(ExtractorTests):
 
     @override_settings(
         LOCALE_PATHS=(os.path.join(
-            os.path.dirname(upath(__file__)), 'project_dir', 'project_locale'),)
+            this_directory, 'project_dir', 'project_locale'),)
     )
     def test_project_locale_paths(self):
         """

commit 39688d4173d942c3b71082bb401ade4533f78b5f

Fixed #23583 -- More selectively ignored static/media roots
Fixed a regression introduced by 802d2bc1a6.
Thanks Michal Čihař for the report and initial patch, and
Collin Anderson and Tim Graham for the reviews.

django/core/management/commands/makemessages.py
@@ -227,10 +227,6 @@ class Command(BaseCommand):
         ignore_patterns = options.get('ignore_patterns')
         if options.get('use_default_ignore_patterns'):
             ignore_patterns += ['CVS', '.*', '*~', '*.pyc']
-        base_path = os.path.abspath('.')
-        for path in (settings.MEDIA_ROOT, settings.STATIC_ROOT):
-            if path and path.startswith(base_path):
-                ignore_patterns.append('%s*' % path[len(base_path) + 1:])
         self.ignore_patterns = list(set(ignore_patterns))
 
         # Avoid messing with mutable class variables
@@ -376,9 +372,11 @@ class Command(BaseCommand):
                 norm_patterns.append(p)
 
         all_files = []
+        ignored_roots = [os.path.normpath(p) for p in (settings.MEDIA_ROOT, settings.STATIC_ROOT)]
         for dirpath, dirnames, filenames in os.walk(root, topdown=True, followlinks=self.symlinks):
             for dirname in dirnames[:]:
-                if is_ignored(os.path.normpath(os.path.join(dirpath, dirname)), norm_patterns):
+                if (is_ignored(os.path.normpath(os.path.join(dirpath, dirname)), norm_patterns) or
+                        os.path.join(os.path.abspath(dirpath), dirname) in ignored_roots):
                     dirnames.remove(dirname)
                     if self.verbosity > 1:
                         self.stdout.write('ignoring directory %s\n' % dirname)

tests/i18n/commands/someapp/static/javascript.js
@@ -1,0 +1,1 @@
+gettext('Static content inside app should be included.')

tests/i18n/commands/static/javascript_ignored.js
@@ -1,0 +1,1 @@
+gettext('Content from STATIC_ROOT should not be included.')

tests/i18n/test_extraction.py
@@ -55,6 +55,17 @@ class ExtractorTests(SimpleTestCase):
             pass
         os.chdir(self._cwd)
 
+    def _run_makemessages(self, **options):
+        os.chdir(self.test_dir)
+        stdout = StringIO()
+        management.call_command('makemessages', locale=[LOCALE], verbosity=2,
+            stdout=stdout, **options)
+        output = stdout.getvalue()
+        self.assertTrue(os.path.exists(self.PO_FILE))
+        with open(self.PO_FILE, 'r') as fp:
+            po_contents = fp.read()
+        return output, po_contents
+
     def assertMsgId(self, msgid, s, use_quotes=True):
         q = '"'
         if use_quotes:
@@ -339,37 +350,34 @@ class JavascriptExtractorTests(ExtractorTests):
 
     def test_javascript_literals(self):
         os.chdir(self.test_dir)
-        management.call_command('makemessages', domain='djangojs', locale=[LOCALE], verbosity=0)
-        self.assertTrue(os.path.exists(self.PO_FILE))
-        with open(self.PO_FILE, 'r') as fp:
-            po_contents = fp.read()
-            self.assertMsgId('This literal should be included.', po_contents)
-            self.assertMsgId('This one as well.', po_contents)
-            self.assertMsgId(r'He said, \"hello\".', po_contents)
-            self.assertMsgId("okkkk", po_contents)
-            self.assertMsgId("TEXT", po_contents)
-            self.assertMsgId("It's at http://example.com", po_contents)
-            self.assertMsgId("String", po_contents)
-            self.assertMsgId("/* but this one will be too */ 'cause there is no way of telling...", po_contents)
-            self.assertMsgId("foo", po_contents)
-            self.assertMsgId("bar", po_contents)
-            self.assertMsgId("baz", po_contents)
-            self.assertMsgId("quz", po_contents)
-            self.assertMsgId("foobar", po_contents)
+        _, po_contents = self._run_makemessages(domain='djangojs')
+        self.assertMsgId('This literal should be included.', po_contents)
+        self.assertMsgId('This one as well.', po_contents)
+        self.assertMsgId(r'He said, \"hello\".', po_contents)
+        self.assertMsgId("okkkk", po_contents)
+        self.assertMsgId("TEXT", po_contents)
+        self.assertMsgId("It's at http://example.com", po_contents)
+        self.assertMsgId("String", po_contents)
+        self.assertMsgId("/* but this one will be too */ 'cause there is no way of telling...", po_contents)
+        self.assertMsgId("foo", po_contents)
+        self.assertMsgId("bar", po_contents)
+        self.assertMsgId("baz", po_contents)
+        self.assertMsgId("quz", po_contents)
+        self.assertMsgId("foobar", po_contents)
+
+    @override_settings(
+        STATIC_ROOT=os.path.join(this_directory, 'commands', 'static/'),
+        MEDIA_ROOT=os.path.join(this_directory, 'commands', 'media_root/'))
+    def test_media_static_dirs_ignored(self):
+        """
+        Regression test for #23583.
+        """
+        _, po_contents = self._run_makemessages(domain='djangojs')
+        self.assertMsgId("Static content inside app should be included.", po_contents)
+        self.assertNotMsgId("Content from STATIC_ROOT should not be included", po_contents)
 
 
 class IgnoredExtractorTests(ExtractorTests):
-
-    def _run_makemessages(self, **options):
-        os.chdir(self.test_dir)
-        stdout = StringIO()
-        management.call_command('makemessages', locale=[LOCALE], verbosity=2,
-            stdout=stdout, **options)
-        data = stdout.getvalue()
-        self.assertTrue(os.path.exists(self.PO_FILE))
-        with open(self.PO_FILE, 'r') as fp:
-            po_contents = fp.read()
-        return data, po_contents
 
     def test_ignore_directory(self):
         out, po_contents = self._run_makemessages(ignore_patterns=[
@@ -395,11 +403,11 @@ class IgnoredExtractorTests(ExtractorTests):
         self.assertNotMsgId('This should be ignored too.', po_contents)
 
     @override_settings(
-        STATIC_ROOT=os.path.join(this_directory, 'commands', 'static_root/'),
+        STATIC_ROOT=os.path.join(this_directory, 'commands', 'static/'),
         MEDIA_ROOT=os.path.join(this_directory, 'commands', 'media_root/'))
     def test_media_static_dirs_ignored(self):
         out, _ = self._run_makemessages()
-        self.assertIn("ignoring directory static_root", out)
+        self.assertIn("ignoring directory static", out)
         self.assertIn("ignoring directory media_root", out)
 
 