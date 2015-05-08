usernameisnull/django

commit a7465059b6ca1e9a6efe6cb9891166a8ac341b8b

Removed old import aliases.

django/utils/log.py
@@ -12,10 +12,6 @@ from django.utils.encoding import force_text
 from django.utils.module_loading import import_string
 from django.views.debug import ExceptionReporter, get_exception_reporter_filter
 
-# Imports kept for backwards-compatibility in Django 1.7.
-from logging import NullHandler  # NOQA
-from logging.config import dictConfig  # NOQA
-
 getLogger = logging.getLogger
 
 # Default logging for Django. This sends an email to the site admins on every
@@ -80,7 +76,7 @@ def configure_logging(logging_config, logging_settings):
         # First find the logging configuration function ...
         logging_config_func = import_string(logging_config)
 
-        dictConfig(DEFAULT_LOGGING)
+        logging.config.dictConfig(DEFAULT_LOGGING)
 
         # ... then invoke it with the logging settings
         if logging_settings:

django/core/servers/basehttp.py
@@ -12,7 +12,6 @@ from __future__ import unicode_literals
 import socket
 import sys
 from wsgiref import simple_server
-from wsgiref.util import FileWrapper   # NOQA: for backwards compatibility
 
 from django.core.exceptions import ImproperlyConfigured
 from django.core.handlers.wsgi import ISO_8859_1, UTF_8

django/forms/fields.py
@@ -33,10 +33,6 @@ from django.utils import six
 from django.utils.six.moves.urllib.parse import urlsplit, urlunsplit
 from django.utils.translation import ugettext_lazy as _, ungettext_lazy
 
-# Provide this import for backwards compatibility.
-from django.core.validators import EMPTY_VALUES  # NOQA
-
-
 __all__ = (
     'Field', 'CharField', 'IntegerField',
     'DateField', 'TimeField', 'DateTimeField', 'DurationField',

django/test/testcases.py
@@ -14,7 +14,6 @@ import sys
 import threading
 import unittest
 import warnings
-from unittest import skipIf         # NOQA: Imported here for backward compatibility
 from unittest.util import safe_repr
 
 from django.apps import apps

tests/admin_views/admin.py
@@ -3,6 +3,7 @@ from __future__ import unicode_literals
 
 import tempfile
 import os
+from wsgiref.util import FileWrapper
 
 from django import forms
 from django.contrib import admin
@@ -10,7 +11,6 @@ from django.contrib.admin.views.main import ChangeList
 from django.core.exceptions import ValidationError
 from django.core.files.storage import FileSystemStorage
 from django.core.mail import EmailMessage
-from django.core.servers.basehttp import FileWrapper
 from django.conf.urls import url
 from django.forms.models import BaseModelFormSet
 from django.http import HttpResponse, StreamingHttpResponse

commit eec53a786fa4bd80bc86328a9eaef6a143986d69

Fixed logging regression introduced in a7465059b6ca1e9a6efe6cb9891166a8ac341b8b

django/utils/log.py
@@ -1,6 +1,7 @@
 from __future__ import unicode_literals
 
 import logging
+import logging.config  # needed when logging_config doesn't start with logging.config
 import sys
 import warnings
 