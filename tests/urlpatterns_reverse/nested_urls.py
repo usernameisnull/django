usernameisnull/django

commit ef7e523178941647eedf000703cfb974e1b52840

Fixed #23601 -- Ensured view exists in URLconf before importing it in admindocs.

tests/admin_docs/tests.py
@@ -1,3 +1,4 @@
+import sys
 import unittest
 
 from django.conf import settings
@@ -83,6 +84,16 @@ class AdminDocViewTests(AdminDocsTestCase):
                     args=['django.contrib.admindocs.views.BaseAdminDocsView']))
         # View docstring
         self.assertContains(response, 'Base view for admindocs views.')
+
+    def test_view_detail_illegal_import(self):
+        """
+        #23601 - Ensure the view exists in the URLconf.
+        """
+        response = self.client.get(
+            reverse('django-admindocs-views-detail',
+                    args=['urlpatterns_reverse.nonimported_module.view']))
+        self.assertEqual(response.status_code, 404)
+        self.assertNotIn("urlpatterns_reverse.nonimported_module", sys.modules)
 
     def test_model_index(self):
         response = self.client.get(reverse('django-admindocs-models-index'))

django/contrib/admindocs/views.py
@@ -143,10 +143,11 @@ class ViewDetailView(BaseAdminDocsView):
 
     def get_context_data(self, **kwargs):
         view = self.kwargs['view']
-        mod, func = urlresolvers.get_mod_func(view)
-        try:
+        urlconf = urlresolvers.get_urlconf()
+        if urlresolvers.get_resolver(urlconf)._is_callback(view):
+            mod, func = urlresolvers.get_mod_func(view)
             view_func = getattr(import_module(mod), func)
-        except (ImportError, AttributeError):
+        else:
             raise Http404
         title, body, metadata = utils.parse_docstring(view_func.__doc__)
         if title:

django/core/urlresolvers.py
@@ -353,6 +353,11 @@ class RegexURLResolver(LocaleRegexProvider):
             self._populate()
         return self._app_dict[language_code]
 
+    def _is_callback(self, name):
+        if not self._populated:
+            self._populate()
+        return name in self._callback_strs
+
     def resolve(self, path):
         path = force_text(path)  # path may be a reverse_lazy object
         tried = []
@@ -430,7 +435,7 @@ class RegexURLResolver(LocaleRegexProvider):
 
         original_lookup = lookup_view
         try:
-            if lookup_view in self._callback_strs:
+            if self._is_callback(lookup_view):
                 lookup_view = get_callable(lookup_view, True)
         except (ImportError, AttributeError) as e:
             raise NoReverseMatch("Error importing '%s': %s." % (lookup_view, e))

commit 5e67d2ac5a44aa3b71eea02a00a26c894dbc3306

Fixed #24931 -- Made namespaced RegexURLResolvers populate callback strings.
Fixed a regression in ef7e523178941647eedf000703cfb974e1b52840.

Thanks Tim Graham for the review.

django/urls/resolvers.py
@@ -164,6 +164,7 @@ class RegexURLResolver(LocaleRegexProvider):
         # urlpatterns
         self._callback_strs = set()
         self._populated = False
+        self._populating = False
 
     def __repr__(self):
         if isinstance(self.urlconf_name, list) and len(self.urlconf_name):
@@ -177,6 +178,9 @@ class RegexURLResolver(LocaleRegexProvider):
         )
 
     def _populate(self):
+        if self._populating:
+            return
+        self._populating = True
         lookups = MultiValueDict()
         namespaces = {}
         apps = {}
@@ -209,7 +213,9 @@ class RegexURLResolver(LocaleRegexProvider):
                         namespaces[namespace] = (p_pattern + prefix, sub_pattern)
                     for app_name, namespace_list in pattern.app_dict.items():
                         apps.setdefault(app_name, []).extend(namespace_list)
-                    self._callback_strs.update(pattern._callback_strs)
+                if not pattern._populating:
+                    pattern._populate()
+                self._callback_strs.update(pattern._callback_strs)
             else:
                 bits = normalize(p_pattern)
                 lookups.appendlist(pattern.callback, (bits, p_pattern, pattern.default_args))
@@ -219,6 +225,7 @@ class RegexURLResolver(LocaleRegexProvider):
         self._namespace_dict[language_code] = namespaces
         self._app_dict[language_code] = apps
         self._populated = True
+        self._populating = False
 
     @property
     def reverse_dict(self):

tests/admin_docs/namespace_urls.py
@@ -1,0 +1,14 @@
+from django.conf.urls import include, url
+from django.contrib import admin
+
+from . import views
+
+backend_urls = ([
+    url(r'^something/$', views.XViewClass.as_view(), name='something'),
+], 'backend')
+
+urlpatterns = [
+    url(r'^admin/doc/', include('django.contrib.admindocs.urls')),
+    url(r'^admin/', admin.site.urls),
+    url(r'^api/backend/', include(backend_urls, namespace='backend')),
+]

tests/admin_docs/tests.py
@@ -88,6 +88,12 @@ class AdminDocViewTests(TestDataMixin, AdminDocsTestCase):
         # View docstring
         self.assertContains(response, 'Base view for admindocs views.')
 
+    @override_settings(ROOT_URLCONF='admin_docs.namespace_urls')
+    def test_namespaced_view_detail(self):
+        url = reverse('django-admindocs-views-detail', args=['admin_docs.views.XViewClass'])
+        response = self.client.get(url)
+        self.assertContains(response, '<h1>admin_docs.views.XViewClass</h1>')
+
     def test_view_detail_illegal_import(self):
         """
         #23601 - Ensure the view exists in the URLconf.

tests/urlpatterns_reverse/nested_urls.py
@@ -1,0 +1,25 @@
+from django.conf.urls import include, url
+from django.views import View
+
+
+def view1(request):
+    pass
+
+
+def view2(request):
+    pass
+
+
+class View3(View):
+    pass
+
+
+nested = ([
+    url(r'^view1/$', view1, name='view1'),
+    url(r'^view3/$', View3.as_view(), name='view3'),
+], 'backend')
+
+urlpatterns = [
+    url(r'^some/path/', include(nested, namespace='nested')),
+    url(r'^view2/$', view2, name='view2'),
+]

tests/urlpatterns_reverse/tests.py
@@ -428,6 +428,13 @@ class ResolverTests(unittest.TestCase):
                                 'Wrong URL name.  Expected "%s", got "%s".' % (e['name'], t.name)
                             )
 
+    def test_namespaced_view_detail(self):
+        resolver = get_resolver('urlpatterns_reverse.nested_urls')
+        self.assertTrue(resolver._is_callback('urlpatterns_reverse.nested_urls.view1'))
+        self.assertTrue(resolver._is_callback('urlpatterns_reverse.nested_urls.view2'))
+        self.assertTrue(resolver._is_callback('urlpatterns_reverse.nested_urls.View3'))
+        self.assertFalse(resolver._is_callback('urlpatterns_reverse.nested_urls.blub'))
+
 
 @override_settings(ROOT_URLCONF='urlpatterns_reverse.reverse_lazy_urls')
 class ReverseLazyTest(TestCase):