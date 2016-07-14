usernameisnull/django

commit 5e67d2ac5a44aa3b71eea02a00a26c894dbc3306

Fixed #24931 -- Made namespaced RegexURLResolvers populate callback strings.
Fixed a regression in ef7e523178941647eedf000703cfb974e1b52840.

Thanks Tim Graham for the review.

django/urls/resolvers.py
@@ -164,6 +164,7 @@ class RegexURLResolver(LocaleRegexProvider):
         # urlpatterns
         self._callback_strs = set()
         self._populated = False
+        self._populating = False
 
     def __repr__(self):
         if isinstance(self.urlconf_name, list) and len(self.urlconf_name):
@@ -177,6 +178,9 @@ class RegexURLResolver(LocaleRegexProvider):
         )
 
     def _populate(self):
+        if self._populating:
+            return
+        self._populating = True
         lookups = MultiValueDict()
         namespaces = {}
         apps = {}
@@ -209,7 +213,9 @@ class RegexURLResolver(LocaleRegexProvider):
                         namespaces[namespace] = (p_pattern + prefix, sub_pattern)
                     for app_name, namespace_list in pattern.app_dict.items():
                         apps.setdefault(app_name, []).extend(namespace_list)
-                    self._callback_strs.update(pattern._callback_strs)
+                if not pattern._populating:
+                    pattern._populate()
+                self._callback_strs.update(pattern._callback_strs)
             else:
                 bits = normalize(p_pattern)
                 lookups.appendlist(pattern.callback, (bits, p_pattern, pattern.default_args))
@@ -219,6 +225,7 @@ class RegexURLResolver(LocaleRegexProvider):
         self._namespace_dict[language_code] = namespaces
         self._app_dict[language_code] = apps
         self._populated = True
+        self._populating = False
 
     @property
     def reverse_dict(self):

tests/urlpatterns_reverse/tests.py
@@ -428,6 +428,13 @@ class ResolverTests(unittest.TestCase):
                                 'Wrong URL name.  Expected "%s", got "%s".' % (e['name'], t.name)
                             )
 
+    def test_namespaced_view_detail(self):
+        resolver = get_resolver('urlpatterns_reverse.nested_urls')
+        self.assertTrue(resolver._is_callback('urlpatterns_reverse.nested_urls.view1'))
+        self.assertTrue(resolver._is_callback('urlpatterns_reverse.nested_urls.view2'))
+        self.assertTrue(resolver._is_callback('urlpatterns_reverse.nested_urls.View3'))
+        self.assertFalse(resolver._is_callback('urlpatterns_reverse.nested_urls.blub'))
+
 
 @override_settings(ROOT_URLCONF='urlpatterns_reverse.reverse_lazy_urls')
 class ReverseLazyTest(TestCase):

tests/admin_docs/tests.py
@@ -88,6 +88,12 @@ class AdminDocViewTests(TestDataMixin, AdminDocsTestCase):
         # View docstring
         self.assertContains(response, 'Base view for admindocs views.')
 
+    @override_settings(ROOT_URLCONF='admin_docs.namespace_urls')
+    def test_namespaced_view_detail(self):
+        url = reverse('django-admindocs-views-detail', args=['admin_docs.views.XViewClass'])
+        response = self.client.get(url)
+        self.assertContains(response, '<h1>admin_docs.views.XViewClass</h1>')
+
     def test_view_detail_illegal_import(self):
         """
         #23601 - Ensure the view exists in the URLconf.

tests/admin_docs/namespace_urls.py
@@ -1,0 +1,14 @@
+from django.conf.urls import include, url
+from django.contrib import admin
+
+from . import views
+
+backend_urls = ([
+    url(r'^something/$', views.XViewClass.as_view(), name='something'),
+], 'backend')
+
+urlpatterns = [
+    url(r'^admin/doc/', include('django.contrib.admindocs.urls')),
+    url(r'^admin/', admin.site.urls),
+    url(r'^api/backend/', include(backend_urls, namespace='backend')),
+]

tests/urlpatterns_reverse/nested_urls.py
@@ -1,0 +1,25 @@
+from django.conf.urls import include, url
+from django.views import View
+
+
+def view1(request):
+    pass
+
+
+def view2(request):
+    pass
+
+
+class View3(View):
+    pass
+
+
+nested = ([
+    url(r'^view1/$', view1, name='view1'),
+    url(r'^view3/$', View3.as_view(), name='view3'),
+], 'backend')
+
+urlpatterns = [
+    url(r'^some/path/', include(nested, namespace='nested')),
+    url(r'^view2/$', view2, name='view2'),
+]

commit d4442e4e50ec55170ed770d791437ff058e441e6

Fixed #26888 -- Fixed concurrency issue in URL resolver.
Fixed a regression in 5e67d2ac5a44aa3b71eea02a00a26c894dbc3306:
improper short-circuiting could lead to a KeyError when threads
concurrently call RegexURLResolver._populate().

django/urls/resolvers.py
@@ -9,6 +9,7 @@ from __future__ import unicode_literals
 
 import functools
 import re
+import threading
 from importlib import import_module
 
 from django.conf import settings
@@ -164,7 +165,7 @@ class RegexURLResolver(LocaleRegexProvider):
         # urlpatterns
         self._callback_strs = set()
         self._populated = False
-        self._populating = False
+        self._local = threading.local()
 
     def __repr__(self):
         if isinstance(self.urlconf_name, list) and len(self.urlconf_name):
@@ -178,9 +179,13 @@ class RegexURLResolver(LocaleRegexProvider):
         )
 
     def _populate(self):
-        if self._populating:
+        # Short-circuit if called recursively in this thread to prevent
+        # infinite recursion. Concurrent threads may call this at the same
+        # time and will need to continue, so set 'populating' on a
+        # thread-local variable.
+        if getattr(self._local, 'populating', False):
             return
-        self._populating = True
+        self._local.populating = True
         lookups = MultiValueDict()
         namespaces = {}
         apps = {}
@@ -213,7 +218,7 @@ class RegexURLResolver(LocaleRegexProvider):
                         namespaces[namespace] = (p_pattern + prefix, sub_pattern)
                     for app_name, namespace_list in pattern.app_dict.items():
                         apps.setdefault(app_name, []).extend(namespace_list)
-                if not pattern._populating:
+                if not getattr(pattern._local, 'populating', False):
                     pattern._populate()
                 self._callback_strs.update(pattern._callback_strs)
             else:
@@ -225,7 +230,7 @@ class RegexURLResolver(LocaleRegexProvider):
         self._namespace_dict[language_code] = namespaces
         self._app_dict[language_code] = apps
         self._populated = True
-        self._populating = False
+        self._local.populating = False
 
     @property
     def reverse_dict(self):

tests/urlpatterns_reverse/tests.py
@@ -5,6 +5,7 @@ Unit tests for reverse URL lookups.
 from __future__ import unicode_literals
 
 import sys
+import threading
 
 from admin_scripts.tests import AdminScriptTestCase
 
@@ -428,6 +429,18 @@ class ResolverTests(SimpleTestCase):
         self.assertTrue(resolver._is_callback('urlpatterns_reverse.nested_urls.view2'))
         self.assertTrue(resolver._is_callback('urlpatterns_reverse.nested_urls.View3'))
         self.assertFalse(resolver._is_callback('urlpatterns_reverse.nested_urls.blub'))
+
+    def test_populate_concurrency(self):
+        """
+        RegexURLResolver._populate() can be called concurrently, but not more
+        than once per thread (#26888).
+        """
+        resolver = RegexURLResolver(r'^/', 'urlpatterns_reverse.urls')
+        resolver._local.populating = True
+        thread = threading.Thread(target=resolver._populate)
+        thread.start()
+        thread.join()
+        self.assertNotEqual(resolver._reverse_dict, {})
 
 
 @override_settings(ROOT_URLCONF='urlpatterns_reverse.reverse_lazy_urls')