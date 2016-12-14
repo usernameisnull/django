usernameisnull/django

commit e65c6b88d20cb5a72e79243a7180348575671543

Removed deprecated html_translator_class sphinx config option.

docs/_ext/djangodocs.py
@@ -63,6 +63,7 @@ def setup(app):
                  man=(visit_snippet_literal, depart_snippet_literal),
                  text=(visit_snippet_literal, depart_snippet_literal),
                  texinfo=(visit_snippet_literal, depart_snippet_literal))
+    app.set_translator('djangohtml', DjangoHTMLTranslator)
     return {'parallel_read_safe': True}
 
 

docs/conf.py
@@ -192,9 +192,6 @@ html_last_updated_fmt = '%b %d, %Y'
 # typographically correct entities.
 html_use_smartypants = True
 
-# HTML translator class for the builder
-html_translator_class = "djangodocs.DjangoHTMLTranslator"
-
 # Content template for the index page.
 # html_index = ''
 

commit 7e4081086887ac6ba6767e53a2b979a0daeee666

Fixed missing versionadded/changed annotations on docs.djangoproject.com.
Fixed a regression in e65c6b88d20cb5a72e79243a7180348575671543.

docs/_ext/djangodocs.py
@@ -64,6 +64,7 @@ def setup(app):
                  text=(visit_snippet_literal, depart_snippet_literal),
                  texinfo=(visit_snippet_literal, depart_snippet_literal))
     app.set_translator('djangohtml', DjangoHTMLTranslator)
+    app.set_translator('json', DjangoHTMLTranslator)
     return {'parallel_read_safe': True}
 
 