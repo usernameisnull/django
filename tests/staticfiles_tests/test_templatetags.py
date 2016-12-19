usernameisnull/django

commit 48772496651f177978ab5b02824ccabb42714873

Fixed #25484 -- Made {% static %} render escaped URLs.

django/templatetags/static.py
@@ -1,7 +1,8 @@
 from django import template
 from django.apps import apps
 from django.utils.encoding import iri_to_uri
-from django.utils.six.moves.urllib.parse import urljoin
+from django.utils.html import conditional_escape
+from django.utils.six.moves.urllib.parse import unquote, urljoin
 
 register = template.Library()
 
@@ -101,7 +102,9 @@ class StaticNode(template.Node):
         return self.handle_simple(path)
 
     def render(self, context):
-        url = self.url(context)
+        url = unquote(self.url(context))
+        if context.autoescape:
+            url = conditional_escape(url)
         if self.varname is None:
             return url
         context[self.varname] = url

tests/staticfiles_tests/test_templatetags.py
@@ -8,3 +8,5 @@ class TestTemplateTag(StaticFilesTestCase):
     def test_template_tag(self):
         self.assertStaticRenders("does/not/exist.png", "/static/does/not/exist.png")
         self.assertStaticRenders("testfile.txt", "/static/testfile.txt")
+        self.assertStaticRenders("test.html?foo=1&bar=2", "/static/test.html?foo=1&bar=2", autoescape=False)
+        self.assertStaticRenders("test.html?foo=1&bar=2", "/static/test.html?foo=1&amp;bar=2", autoescape=True)

tests/staticfiles_tests/cases.py
@@ -35,7 +35,7 @@ class BaseStaticFilesMixin(object):
     def render_template(self, template, **kwargs):
         if isinstance(template, six.string_types):
             template = Template(template)
-        return template.render(Context(kwargs)).strip()
+        return template.render(Context(**kwargs)).strip()
 
     def static_template_snippet(self, path, asvar=False):
         if asvar:

tests/template_tests/syntax_tests/test_static.py
@@ -50,3 +50,14 @@ class StaticTagTests(SimpleTestCase):
     def test_static_statictag04(self):
         output = self.engine.render_to_string('static-statictag04', {'base_css': 'admin/base.css'})
         self.assertEqual(output, urljoin(settings.STATIC_URL, 'admin/base.css'))
+
+    @setup({'static-statictag05': '{% load static %}{% static "test.html?foo=1&bar=2" %}'})
+    def test_static_escapes_urls(self):
+        output = self.engine.render_to_string('static-statictag05')
+        self.assertEqual(output, urljoin(settings.STATIC_URL, '/static/test.html?foo=1&amp;bar=2'))
+
+    @setup({'static-statictag06': '{% load static %}'
+                                  '{% autoescape off %}{% static "test.html?foo=1&bar=2" %}{% endautoescape %}'})
+    def test_static_escapes_urls_autoescape_off(self):
+        output = self.engine.render_to_string('static-statictag06')
+        self.assertEqual(output, urljoin(settings.STATIC_URL, '/static/test.html?foo=1&bar=2'))

commit 030b9b811263e893994e7bb4a90ef929c92d47f2

Refs #25484 -- Removed incorrect unquoting in {% static %}.
Regression in 48772496651f177978ab5b02824ccabb42714873.

Thanks Florian Apolloner for the report and analysis.

django/templatetags/static.py
@@ -2,7 +2,7 @@ from django import template
 from django.apps import apps
 from django.utils.encoding import iri_to_uri
 from django.utils.html import conditional_escape
-from django.utils.six.moves.urllib.parse import unquote, urljoin
+from django.utils.six.moves.urllib.parse import urljoin
 
 register = template.Library()
 
@@ -102,7 +102,7 @@ class StaticNode(template.Node):
         return self.handle_simple(path)
 
     def render(self, context):
-        url = unquote(self.url(context))
+        url = self.url(context)
         if context.autoescape:
             url = conditional_escape(url)
         if self.varname is None:

tests/staticfiles_tests/storage.py
@@ -58,6 +58,11 @@ class PathNotImplementedStorage(storage.Storage):
         raise NotImplementedError
 
 
+class QueryStringStorage(storage.Storage):
+    def url(self, path):
+        return path + '?a=b&c=d'
+
+
 class SimpleCachedStaticFilesStorage(CachedStaticFilesStorage):
 
     def file_hash(self, name, content=None):

tests/staticfiles_tests/test_templatetags.py
@@ -1,5 +1,7 @@
 from __future__ import unicode_literals
 
+from django.test import override_settings
+
 from .cases import StaticFilesTestCase
 
 
@@ -8,5 +10,14 @@ class TestTemplateTag(StaticFilesTestCase):
     def test_template_tag(self):
         self.assertStaticRenders("does/not/exist.png", "/static/does/not/exist.png")
         self.assertStaticRenders("testfile.txt", "/static/testfile.txt")
-        self.assertStaticRenders("test.html?foo=1&bar=2", "/static/test.html?foo=1&bar=2", autoescape=False)
-        self.assertStaticRenders("test.html?foo=1&bar=2", "/static/test.html?foo=1&amp;bar=2", autoescape=True)
+        self.assertStaticRenders("special?chars&quoted.html", "/static/special%3Fchars%26quoted.html")
+
+    @override_settings(STATICFILES_STORAGE='staticfiles_tests.storage.QueryStringStorage')
+    def test_template_tag_escapes(self):
+        """
+        Storage.url() should return an encoded path and might be overridden
+        to also include a querystring. {% static %} escapes the URL to avoid
+        raw '&', for example.
+        """
+        self.assertStaticRenders('a.html', 'a.html?a=b&amp;c=d')
+        self.assertStaticRenders('a.html', 'a.html?a=b&c=d', autoescape=False)