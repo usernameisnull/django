usernameisnull/django

commit b371a04e8203237893c6278fd69bbfb0feae6c9e

Refs #13110 -- Removed SyndicationFeed.add_item()'s enclosure argument.
Per deprecation timeline.

django/utils/feedgenerator.py
@@ -24,10 +24,8 @@ http://web.archive.org/web/20110718035220/http://diveintomark.org/archives/2004/
 from __future__ import unicode_literals
 
 import datetime
-import warnings
 
 from django.utils import datetime_safe, six
-from django.utils.deprecation import RemovedInDjango20Warning
 from django.utils.encoding import force_text, iri_to_uri
 from django.utils.six import StringIO
 from django.utils.six.moves.urllib.parse import urlparse
@@ -119,9 +117,8 @@ class SyndicationFeed(object):
 
     def add_item(self, title, link, description, author_email=None,
                  author_name=None, author_link=None, pubdate=None, comments=None,
-                 unique_id=None, unique_id_is_permalink=None, enclosure=None,
-                 categories=(), item_copyright=None, ttl=None, updateddate=None,
-                 enclosures=None, **kwargs):
+                 unique_id=None, unique_id_is_permalink=None, categories=(),
+                 item_copyright=None, ttl=None, updateddate=None, enclosures=None, **kwargs):
         """
         Adds an item to the feed. All args are expected to be Python Unicode
         objects except pubdate and updateddate, which are datetime.datetime
@@ -135,16 +132,6 @@ class SyndicationFeed(object):
         if ttl is not None:
             # Force ints to unicode
             ttl = force_text(ttl)
-        if enclosure is None:
-            enclosures = [] if enclosures is None else enclosures
-        else:
-            warnings.warn(
-                "The enclosure keyword argument is deprecated, "
-                "use enclosures instead.",
-                RemovedInDjango20Warning,
-                stacklevel=2,
-            )
-            enclosures = [enclosure]
         item = {
             'title': to_unicode(title),
             'link': iri_to_uri(link),

tests/syndication_tests/tests.py
@@ -9,10 +9,7 @@ from django.core.exceptions import ImproperlyConfigured
 from django.test import TestCase, override_settings
 from django.test.utils import requires_tz_support
 from django.utils import timezone
-from django.utils.deprecation import RemovedInDjango20Warning
-from django.utils.feedgenerator import (
-    Enclosure, SyndicationFeed, rfc2822_date, rfc3339_date,
-)
+from django.utils.feedgenerator import rfc2822_date, rfc3339_date
 
 from .models import Article, Entry
 
@@ -520,16 +517,3 @@ class SyndicationFeedTest(FeedTestCase):
             views.add_domain('example.com', '//example.com/foo/?arg=value'),
             'http://example.com/foo/?arg=value'
         )
-
-
-class FeedgeneratorTestCase(TestCase):
-    def test_add_item_warns_when_enclosure_kwarg_is_used(self):
-        feed = SyndicationFeed(title='Example', link='http://example.com', description='Foo')
-        msg = 'The enclosure keyword argument is deprecated, use enclosures instead.'
-        with self.assertRaisesMessage(RemovedInDjango20Warning, msg):
-            feed.add_item(
-                title='Example Item',
-                link='https://example.com/item',
-                description='bar',
-                enclosure=Enclosure('http://example.com/favicon.ico', 0, 'image/png'),
-            )

commit b49709be4dbaee529e7bbb013a63677d9c204154

Fixed #27879 -- Fixed crash if enclosures aren't provided to Atom1Feed.add_item().
Regression in b371a04e8203237893c6278fd69bbfb0feae6c9e

AUTHORS
@@ -612,6 +612,7 @@ answer newbie questions, and generally made Django that much better:
     Paulo Scardine <paulo@scardine.com.br>
     Paul Smith <blinkylights23@gmail.com>
     pavithran s <pavithran.s@gmail.com>
+    Pavlo Kapyshin <i@93z.org>
     permonik@mesias.brnonet.cz
     Petar Marić <http://www.petarmaric.com/>
     Pete Crosier <pete.crosier@gmail.com>

django/utils/feedgenerator.py
@@ -137,7 +137,7 @@ class SyndicationFeed:
             'comments': to_str(comments),
             'unique_id': to_str(unique_id),
             'unique_id_is_permalink': unique_id_is_permalink,
-            'enclosures': enclosures,
+            'enclosures': enclosures or (),
             'categories': categories or (),
             'item_copyright': to_str(item_copyright),
             'ttl': ttl,

tests/utils_tests/test_feedgenerator.py
@@ -120,6 +120,12 @@ class FeedgeneratorTest(unittest.TestCase):
         self.assertIn('href="/feed/"', feed_content)
         self.assertIn('rel="self"', feed_content)
 
+    def test_atom_add_item(self):
+        # Not providing any optional arguments to Atom1Feed.add_item()
+        feed = feedgenerator.Atom1Feed('title', '/link/', 'descr')
+        feed.add_item('item_title', 'item_link', 'item_description')
+        feed.writeString('utf-8')
+
 
 class FeedgeneratorDBTest(TestCase):
 