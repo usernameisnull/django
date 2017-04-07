usernameisnull/django

commit 96b344f15990b303147328edbe4e443d12a21bc8

Removed unnecessary casting when using builtin lookups on PostgreSQL.

django/db/backends/postgresql/operations.py
@@ -83,6 +83,8 @@ class DatabaseOperations(BaseDatabaseOperations):
                            'istartswith', 'endswith', 'iendswith', 'regex', 'iregex'):
             if internal_type in ('IPAddressField', 'GenericIPAddressField'):
                 lookup = "HOST(%s)"
+            elif internal_type in ('CharField', 'TextField'):
+                lookup = '%s'
             else:
                 lookup = "%s::text"
 

commit 9bd670b21d02f1bbc7aa5847fa315646716d8870

Fixed #28038 -- Restored casting to text of builtin lookups on PostgreSQL.
Reverted 96b344f which broke code using __icontains's implicit cast to ::text
on ArrayField.

Thanks Peter J. Farrell for the report.

django/db/backends/postgresql/operations.py
@@ -74,8 +74,6 @@ class DatabaseOperations(BaseDatabaseOperations):
                            'istartswith', 'endswith', 'iendswith', 'regex', 'iregex'):
             if internal_type in ('IPAddressField', 'GenericIPAddressField'):
                 lookup = "HOST(%s)"
-            elif internal_type in ('CharField', 'TextField'):
-                lookup = '%s'
             else:
                 lookup = "%s::text"
 

tests/postgres_tests/test_array.py
@@ -216,6 +216,14 @@ class TestQuerying(PostgreSQLTestCase):
             self.objs[1:3]
         )
 
+    def test_icontains(self):
+        # Using the __icontains lookup with ArrayField is inefficient.
+        instance = CharArrayModel.objects.create(field=['FoO'])
+        self.assertSequenceEqual(
+            CharArrayModel.objects.filter(field__icontains='foo'),
+            [instance]
+        )
+
     def test_contains_charfield(self):
         # Regression for #22907
         self.assertSequenceEqual(