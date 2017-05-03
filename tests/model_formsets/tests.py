usernameisnull/django

commit 89283b8f81dc095538fc0c4f63d741ef228d6bb7

Refs #17235 -- Made MultiPartParser leave request.POST immutable.

tests/requests/tests.py
@@ -533,6 +533,25 @@ class RequestsTests(SimpleTestCase):
         self.assertEqual(request.read(13), b'--boundary\r\nC')
         self.assertEqual(request.POST, {'name': ['value']})
 
+    def test_POST_immutable_for_mutipart(self):
+        """
+        MultiPartParser.parse() leaves request.POST immutable.
+        """
+        payload = FakePayload("\r\n".join([
+            '--boundary',
+            'Content-Disposition: form-data; name="name"',
+            '',
+            'value',
+            '--boundary--',
+        ]))
+        request = WSGIRequest({
+            'REQUEST_METHOD': 'POST',
+            'CONTENT_TYPE': 'multipart/form-data; boundary=boundary',
+            'CONTENT_LENGTH': len(payload),
+            'wsgi.input': payload,
+        })
+        self.assertFalse(request.POST._mutable)
+
     def test_POST_connection_error(self):
         """
         If wsgi.input.read() raises an exception while trying to read() the

django/contrib/admin/options.py
@@ -1785,7 +1785,7 @@ class ModelAdmin(BaseModelAdmin):
             }
             if request.method == 'POST':
                 formset_params.update({
-                    'data': request.POST,
+                    'data': request.POST.copy(),
                     'files': request.FILES,
                     'save_as_new': '_saveasnew' in request.POST
                 })

django/contrib/auth/admin.py
@@ -205,6 +205,7 @@ class UserAdmin(admin.ModelAdmin):
         # * The user has pressed the 'Save and add another' button
         # * We are adding a user in a popup
         if '_addanother' not in request.POST and IS_POPUP_VAR not in request.POST:
+            request.POST = request.POST.copy()
             request.POST['_continue'] = 1
         return super(UserAdmin, self).response_add(request, obj,
                                                    post_url_continue)

django/http/multipartparser.py
@@ -289,6 +289,7 @@ class MultiPartParser(object):
             if retval:
                 break
 
+        self._post._mutable = False
         return self._post, self._files
 
     def handle_file_complete(self, old_field_name, counters):

commit b240818ad21c980c9e5fbf4cf29a0d4962c39e94

Fixed #28159 -- Fixed BaseInlineFormSet._construct_form() crash when using save_as_new.
Regression in 89283b8f81dc095538fc0c4f63d741ef228d6bb7.

django/forms/models.py
@@ -884,12 +884,17 @@ class BaseInlineFormSet(BaseModelFormSet):
     def _construct_form(self, i, **kwargs):
         form = super()._construct_form(i, **kwargs)
         if self.save_as_new:
+            mutable = getattr(form.data, '_mutable', None)
+            # Allow modifying an immutable QueryDict.
+            if mutable is not None:
+                form.data._mutable = True
             # Remove the primary key from the form's data, we are only
             # creating new instances
             form.data[form.add_prefix(self._pk_field.name)] = None
-
             # Remove the foreign key from the form's data
             form.data[form.add_prefix(self.fk.name)] = None
+            if mutable is not None:
+                form.data._mutable = mutable
 
         # Set the fk value here so that the form can do its validation.
         fk_value = self.instance.pk

tests/model_formsets/tests.py
@@ -10,6 +10,7 @@ from django.forms.models import (
     BaseModelFormSet, _get_foreign_key, inlineformset_factory,
     modelformset_factory,
 )
+from django.http import QueryDict
 from django.test import TestCase, skipUnlessDBFeature
 
 from .models import (
@@ -699,7 +700,9 @@ class ModelFormsetTest(TestCase):
         AuthorBooksFormSet = inlineformset_factory(Author, Book, can_delete=False, extra=2, fields="__all__")
         Author.objects.create(name='Charles Baudelaire')
 
-        data = {
+        # An immutable QueryDict simulates request.POST.
+        data = QueryDict(mutable=True)
+        data.update({
             'book_set-TOTAL_FORMS': '3',  # the number of forms rendered
             'book_set-INITIAL_FORMS': '2',  # the number of forms with initial data
             'book_set-MAX_NUM_FORMS': '',  # the max number of forms
@@ -708,10 +711,12 @@ class ModelFormsetTest(TestCase):
             'book_set-1-id': '2',
             'book_set-1-title': 'Les Paradis Artificiels',
             'book_set-2-title': '',
-        }
+        })
+        data._mutable = False
 
         formset = AuthorBooksFormSet(data, instance=Author(), save_as_new=True)
         self.assertTrue(formset.is_valid())
+        self.assertIs(data._mutable, False)
 
         new_author = Author.objects.create(name='Charles Baudelaire')
         formset = AuthorBooksFormSet(data, instance=new_author, save_as_new=True)