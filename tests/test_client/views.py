usernameisnull/django

commit 9879268f51c84abfe3fa030c5368a7c1989a20b4

Refs #23919 -- Replaced six.reraise by raise

django/db/models/query.py
@@ -21,7 +21,7 @@ from django.db.models.fields import AutoField
 from django.db.models.functions import Trunc
 from django.db.models.query_utils import InvalidQuery, Q
 from django.db.models.sql.constants import CURSOR
-from django.utils import six, timezone
+from django.utils import timezone
 from django.utils.functional import cached_property, partition
 from django.utils.version import get_version
 
@@ -498,7 +498,7 @@ class QuerySet:
                 return self.get(**lookup), False
             except self.model.DoesNotExist:
                 pass
-            six.reraise(*exc_info)
+            raise exc_info[0](exc_info[1]).with_traceback(exc_info[2])
 
     def _extract_model_params(self, defaults, **kwargs):
         """

django/utils/autoreload.py
@@ -40,7 +40,6 @@ import _thread
 from django.apps import apps
 from django.conf import settings
 from django.core.signals import request_finished
-from django.utils import six
 
 # This import does nothing, but it's necessary to avoid some race conditions
 # in the threading module. See http://code.djangoproject.com/ticket/2330 .
@@ -247,7 +246,7 @@ def check_errors(fn):
 def raise_last_exception():
     global _exception
     if _exception is not None:
-        six.reraise(*_exception)
+        raise _exception[0](_exception[1]).with_traceback(_exception[2])
 
 
 def ensure_echo_on():

django/test/client.py
@@ -20,7 +20,6 @@ from django.template import TemplateDoesNotExist
 from django.test import signals
 from django.test.utils import ContextList
 from django.urls import resolve
-from django.utils import six
 from django.utils.encoding import force_bytes, uri_to_iri
 from django.utils.functional import SimpleLazyObject, curry
 from django.utils.http import urlencode
@@ -494,7 +493,7 @@ class Client(RequestFactory):
             if self.exc_info:
                 exc_info = self.exc_info
                 self.exc_info = None
-                six.reraise(*exc_info)
+                raise exc_info[0](exc_info[1]).with_traceback(exc_info[2])
 
             # Save the client and request that stimulated the response.
             response.client = self

tests/gis_tests/tests.py
@@ -1,9 +1,7 @@
-import sys
 import unittest
 
 from django.core.exceptions import ImproperlyConfigured
 from django.db import ProgrammingError
-from django.utils import six
 
 try:
     from django.contrib.gis.db.backends.postgis.operations import PostGISOperations
@@ -19,7 +17,7 @@ except ImproperlyConfigured as e:
     if e.args and e.args[0].startswith('Could not import user-defined GEOMETRY_BACKEND'):
         HAS_POSTGRES = False
     else:
-        six.reraise(*sys.exc_info())
+        raise
 
 
 if HAS_POSTGRES:

django/contrib/admin/views/main.py
@@ -1,4 +1,3 @@
-import sys
 from collections import OrderedDict
 
 from django.contrib.admin import FieldListFilter
@@ -17,7 +16,6 @@ from django.core.exceptions import (
 from django.core.paginator import InvalidPage
 from django.db import models
 from django.urls import reverse
-from django.utils import six
 from django.utils.encoding import force_text
 from django.utils.http import urlencode
 from django.utils.translation import ugettext
@@ -151,7 +149,7 @@ class ChangeList:
                 use_distinct = use_distinct or lookup_needs_distinct(self.lookup_opts, key)
             return filter_specs, bool(filter_specs), lookup_params, use_distinct
         except FieldDoesNotExist as e:
-            six.reraise(IncorrectLookupParameters, IncorrectLookupParameters(e), sys.exc_info()[2])
+            raise IncorrectLookupParameters(e) from e
 
     def get_query_string(self, new_params=None, remove=None):
         if new_params is None:

django/contrib/gis/db/backends/oracle/introspection.py
@@ -1,9 +1,6 @@
-import sys
-
 import cx_Oracle
 
 from django.db.backends.oracle.introspection import DatabaseIntrospection
-from django.utils import six
 
 
 class OracleIntrospection(DatabaseIntrospection):
@@ -24,12 +21,11 @@ class OracleIntrospection(DatabaseIntrospection):
                     (table_name.upper(), geo_col.upper())
                 )
                 row = cursor.fetchone()
-            except Exception as msg:
-                new_msg = (
+            except Exception as exc:
+                raise Exception(
                     'Could not find entry in USER_SDO_GEOM_METADATA '
-                    'corresponding to "%s"."%s"\n'
-                    'Error message: %s.') % (table_name, geo_col, msg)
-                six.reraise(Exception, Exception(new_msg), sys.exc_info()[2])
+                    'corresponding to "%s"."%s"' % (table_name, geo_col)
+                ) from exc
 
             # TODO: Research way to find a more specific geometry field type for
             # the column's contents.

django/contrib/gis/db/backends/spatialite/base.py
@@ -1,4 +1,3 @@
-import sys
 from ctypes.util import find_library
 
 from django.conf import settings
@@ -6,7 +5,6 @@ from django.core.exceptions import ImproperlyConfigured
 from django.db.backends.sqlite3.base import (
     DatabaseWrapper as SQLiteDatabaseWrapper, SQLiteCursorWrapper,
 )
-from django.utils import six
 
 from .client import SpatiaLiteClient
 from .features import DatabaseFeatures
@@ -53,11 +51,10 @@ class DatabaseWrapper(SQLiteDatabaseWrapper):
         cur = conn.cursor(factory=SQLiteCursorWrapper)
         try:
             cur.execute("SELECT load_extension(%s)", (self.spatialite_lib,))
-        except Exception as msg:
-            new_msg = (
-                'Unable to load the SpatiaLite library extension '
-                '"%s" because: %s') % (self.spatialite_lib, msg)
-            six.reraise(ImproperlyConfigured, ImproperlyConfigured(new_msg), sys.exc_info()[2])
+        except Exception as exc:
+            raise ImproperlyConfigured(
+                'Unable to load the SpatiaLite library extension "%s"' % self.spatialite_lib
+            ) from exc
         cur.close()
         return conn
 

django/contrib/gis/db/backends/spatialite/operations.py
@@ -4,7 +4,6 @@ https://web.archive.org/web/20130407175746/http://www.gaia-gis.it/gaia-sins/spat
 http://www.gaia-gis.it/gaia-sins/spatialite-sql-4.2.1.html
 """
 import re
-import sys
 
 from django.contrib.gis.db.backends.base.operations import \
     BaseSpatialOperations
@@ -15,7 +14,6 @@ from django.contrib.gis.geometry.backend import Geometry
 from django.contrib.gis.measure import Distance
 from django.core.exceptions import ImproperlyConfigured
 from django.db.backends.sqlite3.operations import DatabaseOperations
-from django.utils import six
 from django.utils.functional import cached_property
 
 
@@ -123,12 +121,13 @@ class SpatiaLiteOperations(BaseSpatialOperations, DatabaseOperations):
         """Determine the version of the SpatiaLite library."""
         try:
             version = self.spatialite_version_tuple()[1:]
-        except Exception as msg:
-            new_msg = (
-                'Cannot determine the SpatiaLite version for the "%s" '
-                'database (error was "%s").  Was the SpatiaLite initialization '
-                'SQL loaded on this database?') % (self.connection.settings_dict['NAME'], msg)
-            six.reraise(ImproperlyConfigured, ImproperlyConfigured(new_msg), sys.exc_info()[2])
+        except Exception as exc:
+            raise ImproperlyConfigured(
+                'Cannot determine the SpatiaLite version for the "%s" database. '
+                'Was the SpatiaLite initialization SQL loaded on this database?' % (
+                    self.connection.settings_dict['NAME'],
+                )
+            ) from exc
         if version < (4, 0, 0):
             raise ImproperlyConfigured('GeoDjango only supports SpatiaLite versions 4.0.0 and above.')
         return version

commit 0436ffec89bcfc5919cb8f87936b856c8aa2a095

Fixed #28837 -- Fixed test client crash if an exception with more than one arg is raised.
Also removed usage of the problematic pattern elsewhere.

Regression in 9879268f51c84abfe3fa030c5368a7c1989a20b4.

django/db/models/query.py
@@ -4,7 +4,6 @@ The main QuerySet implementation. This provides the public API for the ORM.
 
 import copy
 import operator
-import sys
 import warnings
 from collections import OrderedDict, namedtuple
 from functools import lru_cache
@@ -521,13 +520,12 @@ class QuerySet:
                 params = {k: v() if callable(v) else v for k, v in params.items()}
                 obj = self.create(**params)
             return obj, True
-        except IntegrityError:
-            exc_info = sys.exc_info()
+        except IntegrityError as e:
             try:
                 return self.get(**lookup), False
             except self.model.DoesNotExist:
                 pass
-            raise exc_info[0](exc_info[1]).with_traceback(exc_info[2])
+            raise e
 
     def _extract_model_params(self, defaults, **kwargs):
         """

django/utils/autoreload.py
@@ -245,7 +245,7 @@ def check_errors(fn):
 def raise_last_exception():
     global _exception
     if _exception is not None:
-        raise _exception[0](_exception[1]).with_traceback(_exception[2])
+        raise _exception[1]
 
 
 def ensure_echo_on():

django/test/client.py
@@ -480,9 +480,9 @@ class Client(RequestFactory):
             # Also make sure that the signalled exception is cleared from
             # the local cache!
             if self.exc_info:
-                exc_info = self.exc_info
+                _, exc_value, _ = self.exc_info
                 self.exc_info = None
-                raise exc_info[0](exc_info[1]).with_traceback(exc_info[2])
+                raise exc_value
 
             # Save the client and request that stimulated the response.
             response.client = self

tests/test_client/tests.py
@@ -29,7 +29,7 @@ from django.test import (
 )
 from django.urls import reverse_lazy
 
-from .views import get_view, post_view, trace_view
+from .views import TwoArgException, get_view, post_view, trace_view
 
 
 @override_settings(ROOT_URLCONF='test_client.urls')
@@ -712,6 +712,11 @@ class ClientTest(TestCase):
         """
         with self.assertRaisesMessage(Exception, 'exception message'):
             self.client.get('/nesting_exception_view/')
+
+    def test_response_raises_multi_arg_exception(self):
+        """A request may raise an exception with more than one required arg."""
+        with self.assertRaises(TwoArgException):
+            self.client.get('/two_arg_exception/')
 
     def test_uploading_temp_file(self):
         with tempfile.TemporaryFile() as test_file:

tests/test_client/urls.py
@@ -34,6 +34,7 @@ urlpatterns = [
     url(r'^mass_mail_sending_view/$', views.mass_mail_sending_view),
     url(r'^nesting_exception_view/$', views.nesting_exception_view),
     url(r'^django_project_redirect/$', views.django_project_redirect),
+    url(r'^two_arg_exception/$', views.two_arg_exception),
 
     url(r'^accounts/$', RedirectView.as_view(url='login/')),
     url(r'^accounts/no_trailing_slash$', RedirectView.as_view(url='login/')),

tests/test_client/views.py
@@ -331,3 +331,12 @@ def django_project_redirect(request):
 def upload_view(request):
     """Prints keys of request.FILES to the response."""
     return HttpResponse(', '.join(request.FILES))
+
+
+class TwoArgException(Exception):
+    def __init__(self, one, two):
+        pass
+
+
+def two_arg_exception(request):
+    raise TwoArgException('one', 'two')