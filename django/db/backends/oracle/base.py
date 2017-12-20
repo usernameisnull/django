usernameisnull/django

commit fd0b1acfc0318d83e5933ebb8c6616de033bcf80

Fixed #23991 -- Apparently, Oracle doesn't need the decimal field converter
Thanks Josh Smeaton for review.

django/db/backends/oracle/base.py
@@ -274,8 +274,6 @@ WHEN (new.%(col_name)s IS NULL)
             converters.append(self.convert_binaryfield_value)
         elif internal_type in ['BooleanField', 'NullBooleanField']:
             converters.append(self.convert_booleanfield_value)
-        elif internal_type == 'DecimalField':
-            converters.append(self.convert_decimalfield_value)
         elif internal_type == 'DateField':
             converters.append(self.convert_datefield_value)
         elif internal_type == 'TimeField':
@@ -310,9 +308,6 @@ WHEN (new.%(col_name)s IS NULL)
         if value in (1, 0):
             value = bool(value)
         return value
-
-    def convert_decimalfield_value(self, value, field):
-        return backend_utils.typecast_decimal(field.format_number(value))
 
     # cx_Oracle always returns datetime.datetime objects for
     # DATE and TIMESTAMP columns, but Django wants to see a

commit 93573f8ede4774afb24e063512f2928bad71258c

Fixed #28932 -- Prevented Oracle from truncating trailing zeros in the fractional part of DecimalField.
Fixes the test added in 3bfb7aa8d36edd001208a5473c6a3aef733bbd98.
Regression in fd0b1acfc0318d83e5933ebb8c6616de033bcf80.

django/db/backends/oracle/base.py
@@ -402,6 +402,14 @@ class FormatStylePlaceholderCursor:
         return decimal.Decimal(value) if '.' in value else int(value)
 
     @staticmethod
+    def _get_decimal_converter(precision, scale):
+        if scale == 0:
+            return int
+        context = decimal.Context(prec=precision)
+        quantize_value = decimal.Decimal(1).scaleb(-scale)
+        return lambda v: decimal.Decimal(v).quantize(quantize_value, context=context)
+
+    @staticmethod
     def _output_type_handler(cursor, name, defaultType, length, precision, scale):
         """
         Called for each db column fetched from cursors. Return numbers as the
@@ -421,7 +429,7 @@ class FormatStylePlaceholderCursor:
             elif precision > 0:
                 # NUMBER(p,s) column: decimal-precision fixed point.
                 # This comes from IntegerField and DecimalField columns.
-                outconverter = int if scale == 0 else decimal.Decimal
+                outconverter = FormatStylePlaceholderCursor._get_decimal_converter(precision, scale)
             else:
                 # No type information. This normally comes from a
                 # mathematical expression in the SELECT list. Guess int