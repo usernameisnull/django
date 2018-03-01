usernameisnull/django

commit cf4d6215edfede80736eadfa496f2cca68b93d5c

Fixed #27728 -- Allowed overriding admin templatetags templates.

django/contrib/admin/templatetags/base.py
@@ -1,0 +1,31 @@
+from inspect import getfullargspec
+
+from django.template.library import InclusionNode, parse_bits
+
+
+class InclusionAdminNode(InclusionNode):
+    def __init__(self, parser, token, func, template_name, takes_context=True):
+        self.template_name = template_name
+
+        params, varargs, varkw, defaults, kwonly, kwonly_defaults, _ = getfullargspec(func)
+        if len(params) > 0 and params[0] == 'self':
+            params = params[1:]  # ignore 'self'
+        bits = token.split_contents()
+        args, kwargs = parse_bits(
+            parser, bits[1:], params, varargs, varkw, defaults, kwonly, kwonly_defaults, takes_context, bits[0]
+        )
+        super().__init__(
+            func=func, takes_context=takes_context, args=args, kwargs=kwargs, filename=None
+        )
+
+    def render(self, context):
+        opts = context['opts']
+        app_label = opts.app_label.lower()
+        object_name = opts.object_name.lower()
+        self.filename = [
+            'admin/%s/%s/%s' % (app_label, object_name, self.template_name),
+            'admin/%s/%s' % (app_label, self.template_name),
+            'admin/%s' % (self.template_name,),
+        ]
+
+        return super().render(context)

django/contrib/admin/templates/admin/actions.html
@@ -1,7 +1,13 @@
 {% load i18n %}
 <div class="actions">
+  {% block actions %}
+    {% block actions-form %}
     {% for field in action_form %}{% if field.label %}<label>{{ field.label }} {% endif %}{{ field }}{% if field.label %}</label>{% endif %}{% endfor %}
+    {% endblock %}
+    {% block actions-submit %}
     <button type="submit" class="button" title="{% trans "Run the selected action" %}" name="index" value="{{ action_index|default:0 }}">{% trans "Go" %}</button>
+    {% endblock %}
+    {% block actions-counter %}
     {% if actions_selection_counter %}
         <span class="action-counter" data-actions-icnt="{{ cl.result_list|length }}">{{ selection_note }}</span>
         {% if cl.result_count != cl.result_list|length %}
@@ -12,4 +18,6 @@
         <span class="clear"><a href="#">{% trans "Clear selection" %}</a></span>
         {% endif %}
     {% endif %}
+    {% endblock %}
+  {% endblock %}
 </div>

django/contrib/admin/templates/admin/change_form.html
@@ -28,11 +28,7 @@
 {% if change %}{% if not is_popup %}
   <ul class="object-tools">
     {% block object-tools-items %}
-    <li>
-        {% url opts|admin_urlname:'history' original.pk|admin_urlquote as history_url %}
-        <a href="{% add_preserved_filters history_url %}" class="historylink">{% trans "History" %}</a>
-    </li>
-    {% if has_absolute_url %}<li><a href="{{ absolute_url }}" class="viewsitelink">{% trans "View on site" %}</a></li>{% endif %}
+      {% change_form_object_tools %}
     {% endblock %}
   </ul>
 {% endif %}{% endif %}

django/contrib/admin/templates/admin/change_form_object_tools.html
@@ -1,0 +1,8 @@
+{% load i18n admin_urls %}
+{% block object-tools-items %}
+<li>
+    {% url opts|admin_urlname:'history' original.pk|admin_urlquote as history_url %}
+    <a href="{% add_preserved_filters history_url %}" class="historylink">{% trans "History" %}</a>
+</li>
+{% if has_absolute_url %}<li><a href="{{ absolute_url }}" class="viewsitelink">{% trans "View on site" %}</a></li>{% endif %}
+{% endblock %}

django/contrib/admin/templates/admin/change_list.html
@@ -42,14 +42,7 @@
     {% block object-tools %}
         <ul class="object-tools">
           {% block object-tools-items %}
-            {% if has_add_permission %}
-            <li>
-              {% url cl.opts|admin_urlname:'add' as add_url %}
-              <a href="{% add_preserved_filters add_url is_popup to_field %}" class="addlink">
-                {% blocktrans with cl.opts.verbose_name as name %}Add {{ name }}{% endblocktrans %}
-              </a>
-            </li>
-            {% endif %}
+            {% change_list_object_tools %}
           {% endblock %}
         </ul>
     {% endblock %}

django/contrib/admin/templates/admin/change_list_object_tools.html
@@ -1,0 +1,12 @@
+{% load i18n admin_urls %}
+
+{% block object-tools-items %}
+  {% if has_add_permission %}
+  <li>
+    {% url cl.opts|admin_urlname:'add' as add_url %}
+    <a href="{% add_preserved_filters add_url is_popup to_field %}" class="addlink">
+      {% blocktrans with cl.opts.verbose_name as name %}Add {{ name }}{% endblocktrans %}
+    </a>
+  </li>
+  {% endif %}
+{% endblock %}

django/contrib/admin/templates/admin/date_hierarchy.html
@@ -1,10 +1,16 @@
 {% if show %}
 <div class="xfull">
 <ul class="toplinks">
+{% block date-hierarchy-toplinks %}
+{% block date-hierarchy-back %}
 {% if back %}<li class="date-back"><a href="{{ back.link }}">&lsaquo; {{ back.title }}</a></li>{% endif %}
+{% endblock %}
+{% block date-hierarchy-choices %}
 {% for choice in choices %}
 <li> {% if choice.link %}<a href="{{ choice.link }}">{% endif %}{{ choice.title }}{% if choice.link %}</a>{% endif %}</li>
 {% endfor %}
+{% endblock %}
+{% endblock %}
 </ul><br class="clear">
 </div>
 {% endif %}

django/contrib/admin/templates/admin/submit_line.html
@@ -1,5 +1,6 @@
 {% load i18n admin_urls %}
 <div class="submit-row">
+{% block submit-row %}
 {% if show_save %}<input type="submit" value="{% trans 'Save' %}" class="default" name="_save">{% endif %}
 {% if show_delete_link %}
     {% url opts|admin_urlname:'delete' original.pk|admin_urlquote as delete_url %}
@@ -8,4 +9,5 @@
 {% if show_save_as_new %}<input type="submit" value="{% trans 'Save as new' %}" name="_saveasnew">{% endif %}
 {% if show_save_and_add_another %}<input type="submit" value="{% trans 'Save and add another' %}" name="_addanother">{% endif %}
 {% if show_save_and_continue %}<input type="submit" value="{% trans 'Save and continue editing' %}" name="_continue">{% endif %}
+{% endblock %}
 </div>

commit e7536152ad0cd00bf711b6946ba63fc58370d8cf

Refs #27728 -- Fixed thread safety of admin template tags.
Regression in cf4d6215edfede80736eadfa496f2cca68b93d5c.

django/contrib/admin/templatetags/base.py
@@ -23,10 +23,11 @@ class InclusionAdminNode(InclusionNode):
         opts = context['opts']
         app_label = opts.app_label.lower()
         object_name = opts.object_name.lower()
-        self.filename = [
+        # Load template for this render call. (Setting self.filename isn't
+        # thread-safe.)
+        context.render_context[self] = context.template.engine.select_template([
             'admin/%s/%s/%s' % (app_label, object_name, self.template_name),
             'admin/%s/%s' % (app_label, self.template_name),
             'admin/%s' % (self.template_name,),
-        ]
-
+        ])
         return super().render(context)