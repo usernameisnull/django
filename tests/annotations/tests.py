usernameisnull/django

commit 1bf3e0f2742a1b6806b4d336588f321015dff43d

Fixed #25414 -- Fixed QuerySet.annotate() with pk in values() on MySQL.
Thanks Tim Graham and Simon Charette for the reviews.

django/db/models/sql/compiler.py
@@ -135,9 +135,7 @@ class SQLCompiler:
         # include the primary key of every table, but for MySQL it is enough to
         # have the main table's primary key.
         if self.connection.features.allows_group_by_pk:
-            # The logic here is: if the main model's primary key is in the
-            # query, then set new_expressions to that field. If that happens,
-            # then also add having expressions to group by.
+            # Determine if the main model's primary key is in the query.
             pk = None
             for expr in expressions:
                 # Is this a reference to query's base table primary key? If the
@@ -146,9 +144,18 @@ class SQLCompiler:
                         getattr(expr, 'alias', None) == self.query.tables[0]):
                     pk = expr
                     break
+            # If the main model's primary key is in the query, group by that
+            # field, HAVING expressions, and expressions associated with tables
+            # that don't have a primary key included in the grouped columns.
             if pk:
-                # MySQLism: Columns in HAVING clause must be added to the GROUP BY.
-                expressions = [pk] + [expr for expr in expressions if expr in having]
+                pk_aliases = {
+                    expr.alias for expr in expressions
+                    if hasattr(expr, 'target') and expr.target.primary_key
+                }
+                expressions = [pk] + [
+                    expr for expr in expressions
+                    if expr in having or getattr(expr, 'alias', None) not in pk_aliases
+                ]
         elif self.connection.features.allows_group_by_selected_pks:
             # Filter out all expressions associated with a table's primary key
             # present in the grouped columns. This is done by identifying all

tests/annotations/tests.py
@@ -297,6 +297,12 @@ class NonAggregateAnnotationTestCase(TestCase):
         self.assertEqual(book['other_rating'], 4)
         self.assertEqual(book['other_isbn'], '155860191')
 
+    def test_values_with_pk_annotation(self):
+        # annotate references a field in values() with pk
+        publishers = Publisher.objects.values('id', 'book__rating').annotate(total=Sum('book__rating'))
+        for publisher in publishers.filter(pk=self.p1.pk):
+            self.assertEqual(publisher['book__rating'], publisher['total'])
+
     def test_defer_annotation(self):
         """
         Deferred attributes can be referenced by an annotation,

commit 550c76b6deb504976f379dcfb98380c580424097

Fixed #29416 -- Removed unnecesary subquery from GROUP BY clause on MySQL when using a RawSQL annotation.
Regression in 1bf3e0f2742a1b6806b4d336588f321015dff43d.

django/db/models/sql/compiler.py
@@ -157,7 +157,9 @@ class SQLCompiler:
                 }
                 expressions = [pk] + [
                     expr for expr in expressions
-                    if expr in having or getattr(expr, 'alias', None) not in pk_aliases
+                    if expr in having or (
+                        getattr(expr, 'alias', None) is not None and expr.alias not in pk_aliases
+                    )
                 ]
         elif self.connection.features.allows_group_by_selected_pks:
             # Filter out all expressions associated with a table's primary key

tests/annotations/tests.py
@@ -6,6 +6,7 @@ from django.db.models import (
     BooleanField, CharField, Count, DateTimeField, ExpressionWrapper, F, Func,
     IntegerField, NullBooleanField, Q, Sum, Value,
 )
+from django.db.models.expressions import RawSQL
 from django.db.models.functions import Length, Lower
 from django.test import TestCase, skipUnlessDBFeature
 
@@ -321,6 +322,17 @@ class NonAggregateAnnotationTestCase(TestCase):
         publishers = Publisher.objects.values('id', 'book__rating').annotate(total=Sum('book__rating'))
         for publisher in publishers.filter(pk=self.p1.pk):
             self.assertEqual(publisher['book__rating'], publisher['total'])
+
+    @skipUnlessDBFeature('allows_group_by_pk')
+    def test_rawsql_group_by_collapse(self):
+        raw = RawSQL('SELECT MIN(id) FROM annotations_book', [])
+        qs = Author.objects.values('id').annotate(
+            min_book_id=raw,
+            count_friends=Count('friends'),
+        ).order_by()
+        _, _, group_by = qs.query.get_compiler(using='default').pre_sql_setup()
+        self.assertEqual(len(group_by), 1)
+        self.assertNotEqual(raw, group_by[0])
 
     def test_defer_annotation(self):
         """