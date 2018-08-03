usernameisnull/django

commit a96b89b2fc3e3dc4469963268afb7272db07c7b5

Fixed #28116 -- Used error code filtering in PostgreSQL test database creation.
Thanks Claude Paroz and Tim Graham for reviews.

django/db/backends/postgresql/creation.py
@@ -1,5 +1,7 @@
 import sys
 
+from psycopg2 import errorcodes
+
 from django.db.backends.base.creation import BaseDatabaseCreation
 
 
@@ -32,9 +34,8 @@ class DatabaseCreation(BaseDatabaseCreation):
         try:
             super()._execute_create_test_db(cursor, parameters, keepdb)
         except Exception as e:
-            exc_msg = 'database %s already exists' % parameters['dbname']
-            if exc_msg not in str(e):
-                # All errors except "database already exists" cancel tests
+            if getattr(e.__cause__, 'pgcode', '') != errorcodes.DUPLICATE_DATABASE:
+                # All errors except "database already exists" cancel tests.
                 sys.stderr.write('Got an error creating the test database: %s\n' % e)
                 sys.exit(2)
             elif not keepdb:

tests/backends/test_creation.py
@@ -12,10 +12,17 @@ from django.db.backends.mysql.creation import \
     DatabaseCreation as MySQLDatabaseCreation
 from django.db.backends.oracle.creation import \
     DatabaseCreation as OracleDatabaseCreation
-from django.db.backends.postgresql.creation import \
-    DatabaseCreation as PostgreSQLDatabaseCreation
 from django.db.utils import DatabaseError
 from django.test import SimpleTestCase, TestCase
+
+try:
+    import psycopg2  # NOQA
+except ImportError:
+    pass
+else:
+    from psycopg2 import errorcodes
+    from django.db.backends.postgresql.creation import \
+        DatabaseCreation as PostgreSQLDatabaseCreation
 
 
 class TestDbSignatureTests(SimpleTestCase):
@@ -98,10 +105,14 @@ class PostgreSQLDatabaseCreationTests(SimpleTestCase):
         self.check_sql_table_creation_suffix(settings, '''WITH ENCODING 'UTF8' TEMPLATE "template0"''')
 
     def _execute_raise_database_already_exists(self, cursor, parameters, keepdb=False):
-        raise DatabaseError('database %s already exists' % parameters['dbname'])
+        error = DatabaseError('database %s already exists' % parameters['dbname'])
+        error.pgcode = errorcodes.DUPLICATE_DATABASE
+        raise DatabaseError() from error
 
     def _execute_raise_permission_denied(self, cursor, parameters, keepdb=False):
-        raise DatabaseError('permission denied to create database')
+        error = DatabaseError('permission denied to create database')
+        error.pgcode = errorcodes.INSUFFICIENT_PRIVILEGE
+        raise DatabaseError() from error
 
     def patch_test_db_creation(self, execute_create_test_db):
         return mock.patch.object(BaseDatabaseCreation, '_execute_create_test_db', execute_create_test_db)

commit 06cb08cf942a0536ba0cec50622f7decf4c6f1c1

Fixed #29613 -- Fixed --keepdb on PostgreSQL if the database exists and the user can't create databases.
Regression in a96b89b2fc3e3dc4469963268afb7272db07c7b5.

Thanks Tim Graham for the review.

django/db/backends/postgresql/creation.py
@@ -3,6 +3,7 @@ import sys
 from psycopg2 import errorcodes
 
 from django.db.backends.base.creation import BaseDatabaseCreation
+from django.db.backends.utils import strip_quotes
 
 
 class DatabaseCreation(BaseDatabaseCreation):
@@ -28,8 +29,16 @@ class DatabaseCreation(BaseDatabaseCreation):
             template=test_settings.get('TEMPLATE'),
         )
 
+    def _database_exists(self, cursor, database_name):
+        cursor.execute('SELECT 1 FROM pg_catalog.pg_database WHERE datname = %s', [strip_quotes(database_name)])
+        return cursor.fetchone() is not None
+
     def _execute_create_test_db(self, cursor, parameters, keepdb=False):
         try:
+            if keepdb and self._database_exists(cursor, parameters['dbname']):
+                # If the database should be kept and it already exists, don't
+                # try to create a new one.
+                return
             super()._execute_create_test_db(cursor, parameters, keepdb)
         except Exception as e:
             if getattr(e.__cause__, 'pgcode', '') != errorcodes.DUPLICATE_DATABASE:

tests/backends/postgresql/test_creation.py
@@ -89,7 +89,14 @@ class DatabaseCreationTests(SimpleTestCase):
             creation._create_test_db(verbosity=0, autoclobber=False, keepdb=True)
         # Simulate test database creation raising unexpected error
         with self.patch_test_db_creation(self._execute_raise_permission_denied):
-            with self.assertRaises(SystemExit):
-                creation._create_test_db(verbosity=0, autoclobber=False, keepdb=False)
-            with self.assertRaises(SystemExit):
+            with mock.patch.object(DatabaseCreation, '_database_exists', return_value=False):
+                with self.assertRaises(SystemExit):
+                    creation._create_test_db(verbosity=0, autoclobber=False, keepdb=False)
+                with self.assertRaises(SystemExit):
+                    creation._create_test_db(verbosity=0, autoclobber=False, keepdb=True)
+        # Simulate test database creation raising "insufficient privileges".
+        # An error shouldn't appear when keepdb is on and the database already
+        # exists.
+        with self.patch_test_db_creation(self._execute_raise_permission_denied):
+            with mock.patch.object(DatabaseCreation, '_database_exists', return_value=True):
                 creation._create_test_db(verbosity=0, autoclobber=False, keepdb=True)