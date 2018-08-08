usernameisnull/django

commit dab4f745bbe88cd4d4e76d8b9aa4f2c874fdc986

Fixed #28629 -- Made tree.Node instances hashable.
Regression in b9e99d42fff82645e7dc33678421eb8dbc4a2d53 which
added Node.__eq__().

django/utils/tree.py
@@ -70,6 +70,9 @@ class Node:
             self.children == other.children
         )
 
+    def __hash__(self):
+        return hash((self.__class__, self.connector, self.negated) + tuple(self.children))
+
     def add(self, data, conn_type, squash=True):
         """
         Combine this tree and the data represented by data using the

tests/utils_tests/test_tree.py
@@ -19,6 +19,16 @@ class NodeTests(unittest.TestCase):
                          "<Node: (DEFAULT: ('a', 1), ('b', 2))>")
         self.assertEqual(repr(self.node2), "<Node: (DEFAULT: )>")
 
+    def test_hash(self):
+        node3 = Node(self.node1_children, negated=True)
+        node4 = Node(self.node1_children, connector='OTHER')
+        node5 = Node(self.node1_children)
+        self.assertNotEqual(hash(self.node1), hash(self.node2))
+        self.assertNotEqual(hash(self.node1), hash(node3))
+        self.assertNotEqual(hash(self.node1), hash(node4))
+        self.assertEqual(hash(self.node1), hash(node5))
+        self.assertEqual(hash(self.node2), hash(Node()))
+
     def test_len(self):
         self.assertEqual(len(self.node1), 2)
         self.assertEqual(len(self.node2), 0)

commit 6a20c928bfef3cb3bc6129e3fa343b454c56eb5e

Fixed #29643 -- Fixed crash when combining Q objects with __in lookups and lists.
Regression in dab4f745bbe88cd4d4e76d8b9aa4f2c874fdc986.

django/utils/tree.py
@@ -71,7 +71,10 @@ class Node:
         )
 
     def __hash__(self):
-        return hash((self.__class__, self.connector, self.negated) + tuple(self.children))
+        return hash((self.__class__, self.connector, self.negated) + tuple([
+            tuple(child) if isinstance(child, list) else child
+            for child in self.children
+        ]))
 
     def add(self, data, conn_type, squash=True):
         """

tests/utils_tests/test_tree.py
@@ -23,10 +23,12 @@ class NodeTests(unittest.TestCase):
         node3 = Node(self.node1_children, negated=True)
         node4 = Node(self.node1_children, connector='OTHER')
         node5 = Node(self.node1_children)
+        node6 = Node([['a', 1], ['b', 2]])
         self.assertNotEqual(hash(self.node1), hash(self.node2))
         self.assertNotEqual(hash(self.node1), hash(node3))
         self.assertNotEqual(hash(self.node1), hash(node4))
         self.assertEqual(hash(self.node1), hash(node5))
+        self.assertEqual(hash(self.node1), hash(node6))
         self.assertEqual(hash(self.node2), hash(Node()))
 
     def test_len(self):