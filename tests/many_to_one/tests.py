usernameisnull/django

commit f171d03507c6f85d2229b34ff34e965ce0afdbc4

Fixed #28147 -- Fixed loss of assigned parent when saving child after parent.
Thanks Erwin Junge for the initial patch.

django/db/models/fields/related.py
@@ -1031,6 +1031,10 @@ class OneToOneField(ForeignKey):
             setattr(instance, self.name, data)
         else:
             setattr(instance, self.attname, data)
+            # Remote field object must be cleared otherwise Model.save()
+            # will reassign attname using the related object pk.
+            if data is None:
+                setattr(instance, self.name, data)
 
     def _check_unique(self, **kwargs):
         # Override ForeignKey since check isn't applicable here.

tests/many_to_one/tests.py
@@ -8,8 +8,8 @@ from django.test import TestCase
 from django.utils.translation import gettext_lazy
 
 from .models import (
-    Article, Category, Child, City, District, First, Parent, Record, Relation,
-    Reporter, School, Student, Third, ToFieldChild,
+    Article, Category, Child, ChildNullableParent, City, District, First,
+    Parent, Record, Relation, Reporter, School, Student, Third, ToFieldChild,
 )
 
 
@@ -521,6 +521,14 @@ class ManyToOneTests(TestCase):
         c = Child(parent_id=p.id)
         self.assertIsNot(c.parent, p)
         self.assertEqual(c.parent, p)
+
+    def test_save_nullable_fk_after_parent(self):
+        parent = Parent()
+        child = ChildNullableParent(parent=parent)
+        parent.save()
+        child.save()
+        child.refresh_from_db()
+        self.assertEqual(child.parent, parent)
 
     def test_save_nullable_fk_after_parent_with_to_field(self):
         parent = Parent(name='jeff')

AUTHORS
@@ -271,6 +271,7 @@ answer newbie questions, and generally made Django that much better:
     Erik Karulf <erik@karulf.com>
     Erik Romijn <django@solidlinks.nl>
     eriks@win.tue.nl
+    Erwin Junge <erwin@junge.nl>
     Esdras Beleza <linux@esdrasbeleza.com>
     Espen Grindhaug <http://grindhaug.org/>
     Eugene Lazutkin <http://lazutkin.com/blog/>
@@ -744,6 +745,7 @@ answer newbie questions, and generally made Django that much better:
     Robert Wittams
     Rob Golding-Day <rob@golding-day.com>
     Rob Hudson <https://rob.cogit8.org/>
+    Rob Nguyen <tienrobertnguyenn@gmail.com>
     Robin Munn <http://www.geekforgod.com/>
     Rodrigo Pinheiro Marques de Araújo <fenrrir@gmail.com>
     Romain Garrigues <romain.garrigues.cs@gmail.com>

django/db/models/base.py
@@ -684,14 +684,19 @@ class Model(metaclass=ModelBase):
                 # database to raise an IntegrityError if applicable. If
                 # constraints aren't supported by the database, there's the
                 # unavoidable risk of data corruption.
-                if obj and obj.pk is None:
-                    # Remove the object from a related instance cache.
-                    if not field.remote_field.multiple:
-                        field.remote_field.delete_cached_value(obj)
-                    raise ValueError(
-                        "save() prohibited to prevent data loss due to "
-                        "unsaved related object '%s'." % field.name
-                    )
+                if obj:
+                    if obj.pk is None:
+                        # Remove the object from a related instance cache.
+                        if not field.remote_field.multiple:
+                            field.remote_field.delete_cached_value(obj)
+                        raise ValueError(
+                            "save() prohibited to prevent data loss due to "
+                            "unsaved related object '%s'." % field.name
+                        )
+                    elif getattr(self, field.attname) is None:
+                        # Use pk from related object if it has been saved after
+                        # an assignment.
+                        setattr(self, field.attname, obj.pk)
                 # If the relationship's pk/to_field was changed, clear the
                 # cached relationship.
                 if obj and getattr(obj, field.target_field.attname) != getattr(self, field.attname):

tests/many_to_one/models.py
@@ -70,6 +70,10 @@ class Child(models.Model):
     parent = models.ForeignKey(Parent, models.CASCADE)
 
 
+class ChildNullableParent(models.Model):
+    parent = models.ForeignKey(Parent, models.CASCADE, null=True)
+
+
 class ToFieldChild(models.Model):
     parent = models.ForeignKey(Parent, models.CASCADE, to_field='name', related_name='to_field_children')
 

commit c1d3f756756edd4ce8423c3c3917abb006e632fd

Refs #28147 -- Fixed setting of OneToOne and Foreign Key fields to None when using attnames.
Regression in f171d03507c6f85d2229b34ff34e965ce0afdbc4.

django/db/models/fields/related.py
@@ -19,9 +19,9 @@ from django.utils.translation import gettext_lazy as _
 from . import Field
 from .mixins import FieldCacheMixin
 from .related_descriptors import (
-    ForwardManyToOneDescriptor, ForwardOneToOneDescriptor,
-    ManyToManyDescriptor, ReverseManyToOneDescriptor,
-    ReverseOneToOneDescriptor,
+    ForeignKeyDeferredAttribute, ForwardManyToOneDescriptor,
+    ForwardOneToOneDescriptor, ManyToManyDescriptor,
+    ReverseManyToOneDescriptor, ReverseOneToOneDescriptor,
 )
 from .related_lookups import (
     RelatedExact, RelatedGreaterThan, RelatedGreaterThanOrEqual, RelatedIn,
@@ -764,7 +764,7 @@ class ForeignKey(ForeignObject):
     By default ForeignKey will target the pk of the remote model but this
     behavior can be changed by using the ``to_field`` argument.
     """
-
+    descriptor_class = ForeignKeyDeferredAttribute
     # Field flags
     many_to_many = False
     many_to_one = True

django/db/models/fields/related_descriptors.py
@@ -67,7 +67,15 @@ from django.core.exceptions import FieldError
 from django.db import connections, router, transaction
 from django.db.models import Q, signals
 from django.db.models.query import QuerySet
+from django.db.models.query_utils import DeferredAttribute
 from django.utils.functional import cached_property
+
+
+class ForeignKeyDeferredAttribute(DeferredAttribute):
+    def __set__(self, instance, value):
+        if instance.__dict__.get(self.field.attname) != value and self.field.is_cached(instance):
+            self.field.delete_cached_value(instance)
+        instance.__dict__[self.field.attname] = value
 
 
 class ForwardManyToOneDescriptor:

tests/many_to_one/tests.py
@@ -168,6 +168,18 @@ class ManyToOneTests(TestCase):
         parent.bestchild_id = child2.pk
         self.assertTrue(Parent.bestchild.is_cached(parent))
 
+    def test_assign_fk_id_none(self):
+        parent = Parent.objects.create(name='jeff')
+        child = Child.objects.create(name='frank', parent=parent)
+        parent.bestchild = child
+        parent.save()
+        parent.bestchild_id = None
+        parent.save()
+        self.assertIsNone(parent.bestchild_id)
+        self.assertFalse(Parent.bestchild.is_cached(parent))
+        self.assertIsNone(parent.bestchild)
+        self.assertTrue(Parent.bestchild.is_cached(parent))
+
     def test_selects(self):
         self.r.article_set.create(headline="John's second story", pub_date=datetime.date(2005, 7, 29))
         self.r2.article_set.create(headline="Paul's story", pub_date=datetime.date(2006, 1, 17))

tests/one_to_one/tests.py
@@ -217,6 +217,15 @@ class OneToOneTests(TestCase):
         b.place_id = self.p2.pk
         self.assertTrue(UndergroundBar.place.is_cached(b))
 
+    def test_assign_o2o_id_none(self):
+        b = UndergroundBar.objects.create(place=self.p1)
+        b.place_id = None
+        b.save()
+        self.assertIsNone(b.place_id)
+        self.assertFalse(UndergroundBar.place.is_cached(b))
+        self.assertIsNone(b.place)
+        self.assertTrue(UndergroundBar.place.is_cached(b))
+
     def test_related_object_cache(self):
         """ Regression test for #6886 (the related-object cache) """
 