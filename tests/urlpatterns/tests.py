usernameisnull/django

commit 29ebf28c317f05071328fa5524f9e814711e2877

Fixed #26431 -- Prevented django.urls.resolve() from returning missing optional parameters.
Previous behavior was inconsistent with django.urls.reverse() and
caused that translate_url() created an incorrect URL when an optional
parameter was missing.

django/urls/resolvers.py
@@ -153,7 +153,7 @@ class RegexPattern(CheckURLMixin):
             # If there are any named groups, use those as kwargs, ignoring
             # non-named groups. Otherwise, pass all non-named arguments as
             # positional arguments.
-            kwargs = match.groupdict()
+            kwargs = {k: v for k, v in match.groupdict().items() if v is not None}
             args = () if kwargs else match.groups()
             return path[match.end():], args, kwargs
         return None

tests/urlpatterns/tests.py
@@ -55,10 +55,18 @@ class SimplifiedURLTests(SimpleTestCase):
         self.assertEqual(match.route, '^regex/(?P<pk>[0-9]+)/$')
 
     def test_re_path_with_optional_parameter(self):
-        match = resolve('/regex_optional/1/2/')
-        self.assertEqual(match.url_name, 'regex_optional')
-        self.assertEqual(match.kwargs, {'arg1': '1', 'arg2': '2'})
-        self.assertEqual(match.route, r'^regex_optional/(?P<arg1>\d+)/(?:(?P<arg2>\d+)/)?')
+        for url, kwargs in (
+            ('/regex_optional/1/2/', {'arg1': '1', 'arg2': '2'}),
+            ('/regex_optional/1/', {'arg1': '1'}),
+        ):
+            with self.subTest(url=url):
+                match = resolve(url)
+                self.assertEqual(match.url_name, 'regex_optional')
+                self.assertEqual(match.kwargs, kwargs)
+                self.assertEqual(
+                    match.route,
+                    r'^regex_optional/(?P<arg1>\d+)/(?:(?P<arg2>\d+)/)?',
+                )
 
     def test_path_lookup_with_inclusion(self):
         match = resolve('/included_urls/extra/something/')

tests/i18n/patterns/tests.py
@@ -160,6 +160,10 @@ class URLTranslationTests(URLTestCaseBase):
             self.assertEqual(translation.get_language(), 'en')
             # URL with parameters.
             self.assertEqual(
+                translate_url('/en/with-arguments/regular-argument/', 'nl'),
+                '/nl/with-arguments/regular-argument/',
+            )
+            self.assertEqual(
                 translate_url('/en/with-arguments/regular-argument/optional.html', 'nl'),
                 '/nl/with-arguments/regular-argument/optional.html',
             )

commit ef14417c7119914e5b8dbf0d626fe542914a15f4

Fixed #31061 -- Ignored positional args in django.urls.resolve() when all optional named parameters are missing.
Regression in 29ebf28c317f05071328fa5524f9e814711e2877.

Thanks Claude Paroz for the report and Carlton Gibson for reviews.

django/urls/resolvers.py
@@ -158,8 +158,9 @@ class RegexPattern(CheckURLMixin):
             # If there are any named groups, use those as kwargs, ignoring
             # non-named groups. Otherwise, pass all non-named arguments as
             # positional arguments.
-            kwargs = {k: v for k, v in match.groupdict().items() if v is not None}
+            kwargs = match.groupdict()
             args = () if kwargs else match.groups()
+            kwargs = {k: v for k, v in kwargs.items() if v is not None}
             return path[match.end():], args, kwargs
         return None
 

tests/urlpatterns/path_urls.py
@@ -12,6 +12,11 @@ urlpatterns = [
     path('included_urls/', include('urlpatterns.included_urls')),
     re_path(r'^regex/(?P<pk>[0-9]+)/$', views.empty_view, name='regex'),
     re_path(r'^regex_optional/(?P<arg1>\d+)/(?:(?P<arg2>\d+)/)?', views.empty_view, name='regex_optional'),
+    re_path(
+        r'^regex_only_optional/(?:(?P<arg1>\d+)/)?',
+        views.empty_view,
+        name='regex_only_optional',
+    ),
     path('', include('urlpatterns.more_urls')),
     path('<lang>/<path:url>/', views.empty_view, name='lang-and-path'),
 ]

tests/urlpatterns/tests.py
@@ -68,6 +68,16 @@ class SimplifiedURLTests(SimpleTestCase):
                     r'^regex_optional/(?P<arg1>\d+)/(?:(?P<arg2>\d+)/)?',
                 )
 
+    def test_re_path_with_missing_optional_parameter(self):
+        match = resolve('/regex_only_optional/')
+        self.assertEqual(match.url_name, 'regex_only_optional')
+        self.assertEqual(match.kwargs, {})
+        self.assertEqual(match.args, ())
+        self.assertEqual(
+            match.route,
+            r'^regex_only_optional/(?:(?P<arg1>\d+)/)?',
+        )
+
     def test_path_lookup_with_inclusion(self):
         match = resolve('/included_urls/extra/something/')
         self.assertEqual(match.url_name, 'inner-extra')