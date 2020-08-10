usernameisnull/django

commit cbdfd96e32d3ee883811d35132f5182db4452db3

Fixed #31180 -- Configured applications automatically.

django/apps/config.py
@@ -1,9 +1,13 @@
+import inspect
 import os
+import warnings
 from importlib import import_module
 
 from django.core.exceptions import ImproperlyConfigured
-from django.utils.module_loading import module_has_submodule
+from django.utils.deprecation import RemovedInDjango41Warning
+from django.utils.module_loading import import_string, module_has_submodule
 
+APPS_MODULE_NAME = 'apps'
 MODELS_MODULE_NAME = 'models'
 
 
@@ -83,73 +87,139 @@ class AppConfig:
         """
         Factory that creates an app config from an entry in INSTALLED_APPS.
         """
+        # create() eventually returns app_config_class(app_name, app_module).
+        app_config_class = None
+        app_name = None
+        app_module = None
+
+        # If import_module succeeds, entry points to the app module.
         try:
-            # If import_module succeeds, entry is a path to an app module,
-            # which may specify an app config class with default_app_config.
-            # Otherwise, entry is a path to an app config class or an error.
-            module = import_module(entry)
-
-        except ImportError:
-            # Track that importing as an app module failed. If importing as an
-            # app config class fails too, we'll trigger the ImportError again.
-            module = None
-
-            mod_path, _, cls_name = entry.rpartition('.')
-
-            # Raise the original exception when entry cannot be a path to an
-            # app config class.
-            if not mod_path:
-                raise
-
+            app_module = import_module(entry)
+        except Exception:
+            pass
         else:
-            try:
-                # If this works, the app module specifies an app config class.
-                entry = module.default_app_config
-            except AttributeError:
-                # Otherwise, it simply uses the default app config class.
-                return cls(entry, module)
-            else:
-                mod_path, _, cls_name = entry.rpartition('.')
-
-        # If we're reaching this point, we must attempt to load the app config
-        # class located at <mod_path>.<cls_name>
-        mod = import_module(mod_path)
-        try:
-            cls = getattr(mod, cls_name)
-        except AttributeError:
-            if module is None:
-                # If importing as an app module failed, check if the module
-                # contains any valid AppConfigs and show them as choices.
-                # Otherwise, that error probably contains the most informative
-                # traceback, so trigger it again.
-                candidates = sorted(
-                    repr(name) for name, candidate in mod.__dict__.items()
-                    if isinstance(candidate, type) and
-                    issubclass(candidate, AppConfig) and
-                    candidate is not AppConfig
-                )
-                if candidates:
-                    raise ImproperlyConfigured(
-                        "'%s' does not contain a class '%s'. Choices are: %s."
-                        % (mod_path, cls_name, ', '.join(candidates))
+            # If app_module has an apps submodule that defines a single
+            # AppConfig subclass, use it automatically.
+            # To prevent this, an AppConfig subclass can declare a class
+            # variable default = False.
+            # If the apps module defines more than one AppConfig subclass,
+            # the default one can declare default = True.
+            if module_has_submodule(app_module, APPS_MODULE_NAME):
+                mod_path = '%s.%s' % (entry, APPS_MODULE_NAME)
+                mod = import_module(mod_path)
+                # Check if there's exactly one AppConfig candidate,
+                # excluding those that explicitly define default = False.
+                app_configs = [
+                    (name, candidate)
+                    for name, candidate in inspect.getmembers(mod, inspect.isclass)
+                    if (
+                        issubclass(candidate, cls) and
+                        candidate is not cls and
+                        getattr(candidate, 'default', True)
                     )
-                import_module(entry)
+                ]
+                if len(app_configs) == 1:
+                    app_config_class = app_configs[0][1]
+                    app_config_name = '%s.%s' % (mod_path, app_configs[0][0])
+                else:
+                    # Check if there's exactly one AppConfig subclass,
+                    # among those that explicitly define default = True.
+                    app_configs = [
+                        (name, candidate)
+                        for name, candidate in app_configs
+                        if getattr(candidate, 'default', False)
+                    ]
+                    if len(app_configs) > 1:
+                        candidates = [repr(name) for name, _ in app_configs]
+                        raise RuntimeError(
+                            '%r declares more than one default AppConfig: '
+                            '%s.' % (mod_path, ', '.join(candidates))
+                        )
+                    elif len(app_configs) == 1:
+                        app_config_class = app_configs[0][1]
+                        app_config_name = '%s.%s' % (mod_path, app_configs[0][0])
+
+            # If app_module specifies a default_app_config, follow the link.
+            # default_app_config is deprecated, but still takes over the
+            # automatic detection for backwards compatibility during the
+            # deprecation period.
+            try:
+                new_entry = app_module.default_app_config
+            except AttributeError:
+                # Use the default app config class if we didn't find anything.
+                if app_config_class is None:
+                    app_config_class = cls
+                    app_name = entry
             else:
-                raise
+                message = (
+                    '%r defines default_app_config = %r. ' % (entry, new_entry)
+                )
+                if new_entry == app_config_name:
+                    message += (
+                        'Django now detects this configuration automatically. '
+                        'You can remove default_app_config.'
+                    )
+                else:
+                    message += (
+                        "However, Django's automatic detection picked another "
+                        "configuration, %r. You should move the default "
+                        "config class to the apps submodule of your "
+                        "application and, if this module defines several "
+                        "config classes, mark the default one with default = "
+                        "True." % app_config_name
+                    )
+                warnings.warn(message, RemovedInDjango41Warning, stacklevel=2)
+                entry = new_entry
+                app_config_class = None
+
+        # If import_string succeeds, entry is an app config class.
+        if app_config_class is None:
+            try:
+                app_config_class = import_string(entry)
+            except Exception:
+                pass
+        # If both import_module and import_string failed, it means that entry
+        # doesn't have a valid value.
+        if app_module is None and app_config_class is None:
+            # If the last component of entry starts with an uppercase letter,
+            # then it was likely intended to be an app config class; if not,
+            # an app module. Provide a nice error message in both cases.
+            mod_path, _, cls_name = entry.rpartition('.')
+            if mod_path and cls_name[0].isupper():
+                # We could simply re-trigger the string import exception, but
+                # we're going the extra mile and providing a better error
+                # message for typos in INSTALLED_APPS.
+                # This may raise ImportError, which is the best exception
+                # possible if the module at mod_path cannot be imported.
+                mod = import_module(mod_path)
+                candidates = [
+                    repr(name)
+                    for name, candidate in inspect.getmembers(mod, inspect.isclass)
+                    if issubclass(candidate, cls) and candidate is not cls
+                ]
+                msg = "Module '%s' does not contain a '%s' class." % (mod_path, cls_name)
+                if candidates:
+                    msg += ' Choices are: %s.' % ', '.join(candidates)
+                raise ImportError(msg)
+            else:
+                # Re-trigger the module import exception.
+                import_module(entry)
 
         # Check for obvious errors. (This check prevents duck typing, but
         # it could be removed if it became a problem in practice.)
-        if not issubclass(cls, AppConfig):
+        if not issubclass(app_config_class, AppConfig):
             raise ImproperlyConfigured(
                 "'%s' isn't a subclass of AppConfig." % entry)
 
         # Obtain app name here rather than in AppClass.__init__ to keep
         # all error checking for entries in INSTALLED_APPS in one place.
-        try:
-            app_name = cls.name
-        except AttributeError:
-            raise ImproperlyConfigured(
-                "'%s' must supply a name attribute." % entry)
+        if app_name is None:
+            try:
+                app_name = app_config_class.name
+            except AttributeError:
+                raise ImproperlyConfigured(
+                    "'%s' must supply a name attribute." % entry
+                )
 
         # Ensure app_name points to a valid module.
         try:
@@ -157,12 +227,14 @@ class AppConfig:
         except ImportError:
             raise ImproperlyConfigured(
                 "Cannot import '%s'. Check that '%s.%s.name' is correct." % (
-                    app_name, mod_path, cls_name,
+                    app_name,
+                    app_config_class.__module__,
+                    app_config_class.__qualname__,
                 )
             )
 
         # Entry is a path to an app config class.
-        return cls(app_name, app_module)
+        return app_config_class(app_name, app_module)
 
     def get_model(self, model_name, require_ready=True):
         """

tests/apps/tests.py
@@ -5,11 +5,17 @@ from django.apps.registry import Apps
 from django.contrib.admin.models import LogEntry
 from django.core.exceptions import AppRegistryNotReady, ImproperlyConfigured
 from django.db import models
-from django.test import SimpleTestCase, override_settings
+from django.test import SimpleTestCase, ignore_warnings, override_settings
 from django.test.utils import extend_sys_path, isolate_apps
+from django.utils.deprecation import RemovedInDjango41Warning
 
-from .default_config_app.apps import CustomConfig
+from .explicit_default_config_app.apps import ExplicitDefaultConfig
+from .explicit_default_config_mismatch_app.not_apps import (
+    ExplicitDefaultConfigMismatch,
+)
 from .models import SoAlternative, TotallyNormal, new_apps
+from .one_config_app.apps import OneConfig
+from .two_configs_one_default_app.apps import TwoConfig
 
 # Small list with a variety of cases for tests that iterate on installed apps.
 # Intentionally not in alphabetical order to check if the order is preserved.
@@ -84,25 +90,56 @@ class AppsTests(SimpleTestCase):
                 pass
 
     def test_no_such_app_config(self):
-        msg = "No module named 'apps.NoSuchConfig'"
+        msg = "Module 'apps' does not contain a 'NoSuchConfig' class."
         with self.assertRaisesMessage(ImportError, msg):
             with self.settings(INSTALLED_APPS=['apps.NoSuchConfig']):
                 pass
 
     def test_no_such_app_config_with_choices(self):
         msg = (
-            "'apps.apps' does not contain a class 'NoSuchConfig'. Choices are: "
-            "'BadConfig', 'MyAdmin', 'MyAuth', 'NoSuchApp', 'PlainAppsConfig', "
-            "'RelabeledAppsConfig'."
+            "Module 'apps.apps' does not contain a 'NoSuchConfig' class. "
+            "Choices are: 'BadConfig', 'MyAdmin', 'MyAuth', 'NoSuchApp', "
+            "'PlainAppsConfig', 'RelabeledAppsConfig'."
         )
-        with self.assertRaisesMessage(ImproperlyConfigured, msg):
+        with self.assertRaisesMessage(ImportError, msg):
             with self.settings(INSTALLED_APPS=['apps.apps.NoSuchConfig']):
                 pass
 
-    def test_default_app_config(self):
-        with self.settings(INSTALLED_APPS=['apps.default_config_app']):
-            config = apps.get_app_config('default_config_app')
-        self.assertIsInstance(config, CustomConfig)
+    def test_no_config_app(self):
+        """Load an app that doesn't provide an AppConfig class."""
+        with self.settings(INSTALLED_APPS=['apps.no_config_app']):
+            config = apps.get_app_config('no_config_app')
+        self.assertIsInstance(config, AppConfig)
+
+    def test_one_config_app(self):
+        """Load an app that provides an AppConfig class."""
+        with self.settings(INSTALLED_APPS=['apps.one_config_app']):
+            config = apps.get_app_config('one_config_app')
+        self.assertIsInstance(config, OneConfig)
+
+    def test_two_configs_app(self):
+        """Load an app that provides two AppConfig classes."""
+        with self.settings(INSTALLED_APPS=['apps.two_configs_app']):
+            config = apps.get_app_config('two_configs_app')
+        self.assertIsInstance(config, AppConfig)
+
+    def test_two_default_configs_app(self):
+        """Load an app that provides two default AppConfig classes."""
+        msg = (
+            "'apps.two_default_configs_app.apps' declares more than one "
+            "default AppConfig: 'TwoConfig', 'TwoConfigBis'."
+        )
+        with self.assertRaisesMessage(RuntimeError, msg):
+            with self.settings(INSTALLED_APPS=['apps.two_default_configs_app']):
+                pass
+
+    def test_two_configs_one_default_app(self):
+        """
+        Load an app that provides two AppConfig classes, one being the default.
+        """
+        with self.settings(INSTALLED_APPS=['apps.two_configs_one_default_app']):
+            config = apps.get_app_config('two_configs_one_default_app')
+        self.assertIsInstance(config, TwoConfig)
 
     @override_settings(INSTALLED_APPS=SOME_INSTALLED_APPS)
     def test_get_app_configs(self):
@@ -438,3 +475,48 @@ class NamespacePackageAppTests(SimpleTestCase):
             with self.settings(INSTALLED_APPS=['nsapp.apps.NSAppConfig']):
                 app_config = apps.get_app_config('nsapp')
                 self.assertEqual(app_config.path, self.app_path)
+
+
+class DeprecationTests(SimpleTestCase):
+    @ignore_warnings(category=RemovedInDjango41Warning)
+    def test_explicit_default_app_config(self):
+        with self.settings(INSTALLED_APPS=['apps.explicit_default_config_app']):
+            config = apps.get_app_config('explicit_default_config_app')
+        self.assertIsInstance(config, ExplicitDefaultConfig)
+
+    def test_explicit_default_app_config_warning(self):
+        """
+        Load an app that specifies a default AppConfig class matching the
+        autodetected one.
+        """
+        msg = (
+            "'apps.explicit_default_config_app' defines default_app_config = "
+            "'apps.explicit_default_config_app.apps.ExplicitDefaultConfig'. "
+            "Django now detects this configuration automatically. You can "
+            "remove default_app_config."
+        )
+        with self.assertRaisesMessage(RemovedInDjango41Warning, msg):
+            with self.settings(INSTALLED_APPS=['apps.explicit_default_config_app']):
+                config = apps.get_app_config('explicit_default_config_app')
+            self.assertIsInstance(config, ExplicitDefaultConfig)
+
+    def test_explicit_default_app_config_mismatch(self):
+        """
+        Load an app that specifies a default AppConfig class not matching the
+        autodetected one.
+        """
+        msg = (
+            "'apps.explicit_default_config_mismatch_app' defines "
+            "default_app_config = 'apps.explicit_default_config_mismatch_app."
+            "not_apps.ExplicitDefaultConfigMismatch'. However, Django's "
+            "automatic detection picked another configuration, 'apps."
+            "explicit_default_config_mismatch_app.apps."
+            "ImplicitDefaultConfigMismatch'. You should move the default "
+            "config class to the apps submodule of your application and, if "
+            "this module defines several config classes, mark the default one "
+            "with default = True."
+        )
+        with self.assertRaisesMessage(RemovedInDjango41Warning, msg):
+            with self.settings(INSTALLED_APPS=['apps.explicit_default_config_mismatch_app']):
+                config = apps.get_app_config('explicit_default_config_mismatch_app')
+            self.assertIsInstance(config, ExplicitDefaultConfigMismatch)

django/contrib/admin/__init__.py
@@ -22,6 +22,3 @@ __all__ = [
 
 def autodiscover():
     autodiscover_modules('admin', register_to=site)
-
-
-default_app_config = 'django.contrib.admin.apps.AdminConfig'

django/contrib/auth/__init__.py
@@ -217,6 +217,3 @@ def update_session_auth_hash(request, user):
     request.session.cycle_key()
     if hasattr(user, 'get_session_auth_hash') and request.user == user:
         request.session[HASH_SESSION_KEY] = user.get_session_auth_hash()
-
-
-default_app_config = 'django.contrib.auth.apps.AuthConfig'

django/contrib/contenttypes/__init__.py
@@ -1,1 +1,0 @@
-default_app_config = 'django.contrib.contenttypes.apps.ContentTypesConfig'

django/contrib/flatpages/__init__.py
@@ -1,1 +1,0 @@
-default_app_config = 'django.contrib.flatpages.apps.FlatPagesConfig'

django/contrib/gis/__init__.py
@@ -1,1 +1,0 @@
-default_app_config = 'django.contrib.gis.apps.GISConfig'

django/contrib/humanize/__init__.py
@@ -1,1 +1,0 @@
-default_app_config = 'django.contrib.humanize.apps.HumanizeConfig'

commit b2733639530c646f567d72c22d6514cc52c5a197

Fixed #31870 -- Fixed crash when populating app registry with empty or without apps module.
Regression in cbdfd96e32d3ee883811d35132f5182db4452db3.

django/apps/config.py
@@ -89,6 +89,7 @@ class AppConfig:
         """
         # create() eventually returns app_config_class(app_name, app_module).
         app_config_class = None
+        app_config_name = None
         app_name = None
         app_module = None
 
@@ -161,12 +162,15 @@ class AppConfig:
                     )
                 else:
                     message += (
-                        "However, Django's automatic detection picked another "
-                        "configuration, %r. You should move the default "
-                        "config class to the apps submodule of your "
-                        "application and, if this module defines several "
-                        "config classes, mark the default one with default = "
-                        "True." % app_config_name
+                        "However, Django's automatic detection %s. You should "
+                        "move the default config class to the apps submodule "
+                        "of your application and, if this module defines "
+                        "several config classes, mark the default one with "
+                        "default = True." % (
+                            "picked another configuration, %r" % app_config_name
+                            if app_config_name
+                            else "did not find this configuration"
+                        )
                     )
                 warnings.warn(message, RemovedInDjango41Warning, stacklevel=2)
                 entry = new_entry

tests/apps/explicit_default_config_empty_apps/__init__.py
@@ -1,0 +1,7 @@
+from django.apps import AppConfig
+
+default_app_config = 'apps.explicit_default_config_empty_apps.ExplicitDefaultConfigEmptyApps'
+
+
+class ExplicitDefaultConfigEmptyApps(AppConfig):
+    name = 'apps.explicit_default_config_empty_apps'

tests/apps/explicit_default_config_without_apps/__init__.py
@@ -1,0 +1,7 @@
+from django.apps import AppConfig
+
+default_app_config = 'apps.explicit_default_config_without_apps.ExplicitDefaultConfigWithoutApps'
+
+
+class ExplicitDefaultConfigWithoutApps(AppConfig):
+    name = 'apps.explicit_default_config_without_apps'

tests/apps/tests.py
@@ -10,8 +10,12 @@ from django.test.utils import extend_sys_path, isolate_apps
 from django.utils.deprecation import RemovedInDjango41Warning
 
 from .explicit_default_config_app.apps import ExplicitDefaultConfig
+from .explicit_default_config_empty_apps import ExplicitDefaultConfigEmptyApps
 from .explicit_default_config_mismatch_app.not_apps import (
     ExplicitDefaultConfigMismatch,
+)
+from .explicit_default_config_without_apps import (
+    ExplicitDefaultConfigWithoutApps,
 )
 from .models import SoAlternative, TotallyNormal, new_apps
 from .one_config_app.apps import OneConfig
@@ -520,3 +524,51 @@ class DeprecationTests(SimpleTestCase):
             with self.settings(INSTALLED_APPS=['apps.explicit_default_config_mismatch_app']):
                 config = apps.get_app_config('explicit_default_config_mismatch_app')
             self.assertIsInstance(config, ExplicitDefaultConfigMismatch)
+
+    def test_explicit_default_app_config_empty_apps(self):
+        """
+        Load an app that specifies a default AppConfig class in __init__ and
+        have an empty apps module.
+        """
+        msg = (
+            "'apps.explicit_default_config_empty_apps' defines "
+            "default_app_config = 'apps.explicit_default_config_empty_apps."
+            "ExplicitDefaultConfigEmptyApps'. However, Django's automatic "
+            "detection did not find this configuration. You should move the "
+            "default config class to the apps submodule of your application "
+            "and, if this module defines several config classes, mark the "
+            "default one with default = True."
+        )
+        with self.assertRaisesMessage(RemovedInDjango41Warning, msg):
+            with self.settings(INSTALLED_APPS=['apps.explicit_default_config_empty_apps']):
+                pass
+        with ignore_warnings(category=RemovedInDjango41Warning):
+            with self.settings(INSTALLED_APPS=['apps.explicit_default_config_empty_apps']):
+                self.assertIsInstance(
+                    apps.get_app_config('explicit_default_config_empty_apps'),
+                    ExplicitDefaultConfigEmptyApps,
+                )
+
+    def test_explicit_default_app_config_without_apps(self):
+        """
+        Load an app that specifies a default AppConfig class in __init__ and do
+        not have an apps module.
+        """
+        msg = (
+            "'apps.explicit_default_config_without_apps' defines "
+            "default_app_config = 'apps.explicit_default_config_without_apps."
+            "ExplicitDefaultConfigWithoutApps'. However, Django's automatic "
+            "detection did not find this configuration. You should move the "
+            "default config class to the apps submodule of your application "
+            "and, if this module defines several config classes, mark the "
+            "default one with default = True."
+        )
+        with self.assertRaisesMessage(RemovedInDjango41Warning, msg):
+            with self.settings(INSTALLED_APPS=['apps.explicit_default_config_without_apps']):
+                pass
+        with ignore_warnings(category=RemovedInDjango41Warning):
+            with self.settings(INSTALLED_APPS=['apps.explicit_default_config_without_apps']):
+                self.assertIsInstance(
+                    apps.get_app_config('explicit_default_config_without_apps'),
+                    ExplicitDefaultConfigWithoutApps,
+                )