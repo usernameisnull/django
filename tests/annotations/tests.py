usernameisnull/django

commit 33178f727a7eec7882c3c2e561393d7f868c6dde

Fixed #31651 -- Made ExpressionWrapper use grouping columns from wrapped expression.

django/db/models/expressions.py
@@ -863,6 +863,9 @@ class ExpressionWrapper(Expression):
     def get_source_expressions(self):
         return [self.expression]
 
+    def get_group_by_cols(self, alias=None):
+        return self.expression.get_group_by_cols(alias=alias)
+
     def as_sql(self, compiler, connection):
         return self.expression.as_sql(compiler, connection)
 

tests/expressions/tests.py
@@ -1828,3 +1828,13 @@ class CombinableTests(SimpleTestCase):
     def test_reversed_or(self):
         with self.assertRaisesMessage(NotImplementedError, self.bitwise_msg):
             object() | Combinable()
+
+
+class ExpressionWrapperTests(SimpleTestCase):
+    def test_empty_group_by(self):
+        expr = ExpressionWrapper(Value(3), output_field=IntegerField())
+        self.assertEqual(expr.get_group_by_cols(alias=None), [])
+
+    def test_non_empty_group_by(self):
+        expr = ExpressionWrapper(Lower(Value('f')), output_field=IntegerField())
+        self.assertEqual(expr.get_group_by_cols(alias=None), [expr.expression])

commit 11bfa1b5ad50243c38c6a339dfdb9a41f527c3af

Fixed #32200 -- Fixed grouping by ExpressionWrapper() with Q objects.
Thanks Gordon Wrigley for the report.

Regression in 33178f727a7eec7882c3c2e561393d7f868c6dde.

django/db/models/expressions.py
@@ -915,9 +915,13 @@ class ExpressionWrapper(Expression):
         return [self.expression]
 
     def get_group_by_cols(self, alias=None):
-        expression = self.expression.copy()
-        expression.output_field = self.output_field
-        return expression.get_group_by_cols(alias=alias)
+        if isinstance(self.expression, Expression):
+            expression = self.expression.copy()
+            expression.output_field = self.output_field
+            return expression.get_group_by_cols(alias=alias)
+        # For non-expressions e.g. an SQL WHERE clause, the entire
+        # `expression` must be included in the GROUP BY clause.
+        return super().get_group_by_cols()
 
     def as_sql(self, compiler, connection):
         return compiler.compile(self.expression)

tests/annotations/tests.py
@@ -195,6 +195,18 @@ class NonAggregateAnnotationTestCase(TestCase):
         self.assertEqual(book.isnull_pubdate, False)
         self.assertEqual(book.rating_count, 1)
 
+    @skipUnlessDBFeature('supports_boolean_expr_in_select_clause')
+    def test_grouping_by_q_expression_annotation(self):
+        authors = Author.objects.annotate(
+            under_40=ExpressionWrapper(Q(age__lt=40), output_field=BooleanField()),
+        ).values('under_40').annotate(
+            count_id=Count('id'),
+        ).values('under_40', 'count_id')
+        self.assertCountEqual(authors, [
+            {'under_40': False, 'count_id': 3},
+            {'under_40': True, 'count_id': 6},
+        ])
+
     def test_aggregate_over_annotation(self):
         agg = Author.objects.annotate(other_age=F('age')).aggregate(otherage_sum=Sum('other_age'))
         other_agg = Author.objects.aggregate(age_sum=Sum('age'))