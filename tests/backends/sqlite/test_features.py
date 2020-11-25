usernameisnull/django

commit da7b06d04c913b21759e341a6a3313becf0b85bf

Fixed #12990, Refs #27694 -- Added JSONField model field.
Thanks to Adam Johnson, Carlton Gibson, Mariusz Felisiak, and Raphael
Michel for mentoring this Google Summer of Code 2019 project and
everyone else who helped with the patch.

Special thanks to Mads Jensen, Nick Pope, and Simon Charette for
extensive reviews.

Co-authored-by: Mariusz Felisiak <felisiak.mariusz@gmail.com>

django/db/backends/sqlite3/features.py
@@ -1,4 +1,9 @@
+import operator
+
+from django.db import transaction
 from django.db.backends.base.features import BaseDatabaseFeatures
+from django.db.utils import OperationalError
+from django.utils.functional import cached_property
 
 from .base import Database
 
@@ -45,3 +50,14 @@ class DatabaseFeatures(BaseDatabaseFeatures):
     supports_aggregate_filter_clause = Database.sqlite_version_info >= (3, 30, 1)
     supports_order_by_nulls_modifier = Database.sqlite_version_info >= (3, 30, 0)
     order_by_nulls_first = True
+
+    @cached_property
+    def supports_json_field(self):
+        try:
+            with self.connection.cursor() as cursor, transaction.atomic():
+                cursor.execute('SELECT JSON(\'{"a": "b"}\')')
+        except OperationalError:
+            return False
+        return True
+
+    can_introspect_json_field = property(operator.attrgetter('supports_json_field'))

django/db/backends/base/features.py
@@ -300,6 +300,15 @@ class BaseDatabaseFeatures:
     # Does the backend support boolean expressions in the SELECT clause?
     supports_boolean_expr_in_select_clause = True
 
+    # Does the backend support JSONField?
+    supports_json_field = True
+    # Can the backend introspect a JSONField?
+    can_introspect_json_field = True
+    # Does the backend support primitives in JSONField?
+    supports_primitives_in_json_field = True
+    # Is there a true datatype for JSON?
+    has_native_json_field = False
+
     def __init__(self, connection):
         self.connection = connection
 

django/db/backends/mysql/features.py
@@ -160,3 +160,15 @@ class DatabaseFeatures(BaseDatabaseFeatures):
     def supports_default_in_lead_lag(self):
         # To be added in https://jira.mariadb.org/browse/MDEV-12981.
         return not self.connection.mysql_is_mariadb
+
+    @cached_property
+    def supports_json_field(self):
+        if self.connection.mysql_is_mariadb:
+            return self.connection.mysql_version >= (10, 2, 7)
+        return self.connection.mysql_version >= (5, 7, 8)
+
+    @cached_property
+    def can_introspect_json_field(self):
+        if self.connection.mysql_is_mariadb:
+            return self.supports_json_field and self.can_introspect_check_constraints
+        return self.supports_json_field

django/db/backends/oracle/features.py
@@ -60,3 +60,4 @@ class DatabaseFeatures(BaseDatabaseFeatures):
     supports_slicing_ordering_in_compound = True
     allows_multiple_constraints_on_same_fields = False
     supports_boolean_expr_in_select_clause = False
+    supports_primitives_in_json_field = False

django/db/backends/postgresql/features.py
@@ -12,6 +12,7 @@ class DatabaseFeatures(BaseDatabaseFeatures):
     has_real_datatype = True
     has_native_uuid_field = True
     has_native_duration_field = True
+    has_native_json_field = True
     can_defer_constraint_checks = True
     has_select_for_update = True
     has_select_for_update_nowait = True

AUTHORS
@@ -792,6 +792,7 @@ answer newbie questions, and generally made Django that much better:
     Ryan Rubin <ryanmrubin@gmail.com>
     Ryno Mathee <rmathee@gmail.com>
     Sachin Jat <sanch.jat@gmail.com>
+    Sage M. Abdullah <https://github.com/laymonage>
     Sam Newman <http://www.magpiebrain.com/>
     Sander Dijkhuis <sander.dijkhuis@gmail.com>
     Sanket Saurav <sanketsaurav@gmail.com>

django/contrib/postgres/aggregates/general.py
@@ -1,5 +1,5 @@
-from django.contrib.postgres.fields import ArrayField, JSONField
-from django.db.models import Aggregate, Value
+from django.contrib.postgres.fields import ArrayField
+from django.db.models import Aggregate, JSONField, Value
 
 from .mixins import OrderableAggMixin
 

django/contrib/postgres/apps.py
@@ -47,7 +47,6 @@ class PostgresConfig(AppConfig):
         for conn in connections.all():
             if conn.vendor == 'postgresql':
                 conn.introspection.data_types_reverse.update({
-                    3802: 'django.contrib.postgres.fields.JSONField',
                     3904: 'django.contrib.postgres.fields.IntegerRangeField',
                     3906: 'django.contrib.postgres.fields.DecimalRangeField',
                     3910: 'django.contrib.postgres.fields.DateTimeRangeField',

commit 3f12445a41d28d2c3eecee5e81ce6beb18c80feb

Fixed #32224 -- Avoided suppressing connection errors in supports_json_field on SQLite.`
Regression in da7b06d04c913b21759e341a6a3313becf0b85bf.

Thanks Juan Garcia Alvite for the report.

django/db/backends/sqlite3/features.py
@@ -70,11 +70,12 @@ class DatabaseFeatures(BaseDatabaseFeatures):
 
     @cached_property
     def supports_json_field(self):
-        try:
-            with self.connection.cursor() as cursor, transaction.atomic():
-                cursor.execute('SELECT JSON(\'{"a": "b"}\')')
-        except OperationalError:
-            return False
+        with self.connection.cursor() as cursor:
+            try:
+                with transaction.atomic(self.connection.alias):
+                    cursor.execute('SELECT JSON(\'{"a": "b"}\')')
+            except OperationalError:
+                return False
         return True
 
     can_introspect_json_field = property(operator.attrgetter('supports_json_field'))

tests/backends/sqlite/test_features.py
@@ -1,0 +1,18 @@
+from unittest import mock, skipUnless
+
+from django.db import OperationalError, connection
+from django.test import TestCase
+
+
+@skipUnless(connection.vendor == 'sqlite', 'SQLite tests.')
+class FeaturesTests(TestCase):
+    def test_supports_json_field_operational_error(self):
+        if hasattr(connection.features, 'supports_json_field'):
+            del connection.features.supports_json_field
+        msg = 'unable to open database file'
+        with mock.patch(
+            'django.db.backends.base.base.BaseDatabaseWrapper.cursor',
+            side_effect=OperationalError(msg),
+        ):
+            with self.assertRaisesMessage(OperationalError, msg):
+                connection.features.supports_json_field