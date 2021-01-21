usernameisnull/django

commit a7038600966b4c4f021033103f3d715b76778b0c

Fixed #31007 -- Allowed specifying type of auto-created primary keys.
This also changes the default type of auto-created primary keys
for new apps and projects to BigAutoField.

django/db/models/base.py
@@ -1290,9 +1290,34 @@ class Model(metaclass=ModelBase):
                 *cls._check_indexes(databases),
                 *cls._check_ordering(),
                 *cls._check_constraints(databases),
+                *cls._check_default_pk(),
             ]
 
         return errors
+
+    @classmethod
+    def _check_default_pk(cls):
+        if (
+            cls._meta.pk.auto_created and
+            not settings.is_overridden('DEFAULT_AUTO_FIELD') and
+            not cls._meta.app_config._is_default_auto_field_overridden
+        ):
+            return [
+                checks.Warning(
+                    f"Auto-created primary key used when not defining a "
+                    f"primary key type, by default "
+                    f"'{settings.DEFAULT_AUTO_FIELD}'.",
+                    hint=(
+                        f"Configure the DEFAULT_AUTO_FIELD setting or the "
+                        f"{cls._meta.app_config.__class__.__qualname__}."
+                        f"default_auto_field attribute to point to a subclass "
+                        f"of AutoField, e.g. 'django.db.models.BigAutoField'."
+                    ),
+                    obj=cls,
+                    id='models.W042',
+                ),
+            ]
+        return []
 
     @classmethod
     def _check_swappable(cls):

tests/check_framework/test_model_checks.py
@@ -1,3 +1,5 @@
+from unittest import mock
+
 from django.core import checks
 from django.core.checks import Error, Warning
 from django.db import models
@@ -356,5 +358,60 @@ class ConstraintNameTests(TestCase):
             class Meta:
                 app_label = 'check_framework'
                 constraints = [constraint]
+
+        self.assertEqual(checks.run_checks(app_configs=apps.get_app_configs()), [])
+
+
+def mocked_is_overridden(self, setting):
+    # Force treating DEFAULT_AUTO_FIELD = 'django.db.models.AutoField' as a not
+    # overridden setting.
+    return (
+        setting != 'DEFAULT_AUTO_FIELD' or
+        self.DEFAULT_AUTO_FIELD != 'django.db.models.AutoField'
+    )
+
+
+@mock.patch('django.conf.UserSettingsHolder.is_overridden', mocked_is_overridden)
+@override_settings(DEFAULT_AUTO_FIELD='django.db.models.AutoField')
+@isolate_apps('check_framework.apps.CheckDefaultPKConfig', attr_name='apps')
+@override_system_checks([checks.model_checks.check_all_models])
+class ModelDefaultAutoFieldTests(SimpleTestCase):
+    def test_auto_created_pk(self):
+        class Model(models.Model):
+            pass
+
+        self.assertEqual(checks.run_checks(app_configs=self.apps.get_app_configs()), [
+            Warning(
+                "Auto-created primary key used when not defining a primary "
+                "key type, by default 'django.db.models.AutoField'.",
+                hint=(
+                    "Configure the DEFAULT_AUTO_FIELD setting or the "
+                    "CheckDefaultPKConfig.default_auto_field attribute to "
+                    "point to a subclass of AutoField, e.g. "
+                    "'django.db.models.BigAutoField'."
+                ),
+                obj=Model,
+                id='models.W042',
+            ),
+        ])
+
+    @override_settings(DEFAULT_AUTO_FIELD='django.db.models.BigAutoField')
+    def test_default_auto_field_setting(self):
+        class Model(models.Model):
+            pass
+
+        self.assertEqual(checks.run_checks(app_configs=self.apps.get_app_configs()), [])
+
+    def test_explicit_pk(self):
+        class Model(models.Model):
+            id = models.BigAutoField(primary_key=True)
+
+        self.assertEqual(checks.run_checks(app_configs=self.apps.get_app_configs()), [])
+
+    @isolate_apps('check_framework.apps.CheckPKConfig', kwarg_name='apps')
+    def test_app_default_auto_field(self, apps):
+        class ModelWithPkViaAppConfig(models.Model):
+            class Meta:
+                app_label = 'check_framework.apps.CheckPKConfig'
 
         self.assertEqual(checks.run_checks(app_configs=apps.get_app_configs()), [])

django/apps/config.py
@@ -5,6 +5,7 @@ from importlib import import_module
 
 from django.core.exceptions import ImproperlyConfigured
 from django.utils.deprecation import RemovedInDjango41Warning
+from django.utils.functional import cached_property
 from django.utils.module_loading import import_string, module_has_submodule
 
 APPS_MODULE_NAME = 'apps'
@@ -54,6 +55,15 @@ class AppConfig:
 
     def __repr__(self):
         return '<%s: %s>' % (self.__class__.__name__, self.label)
+
+    @cached_property
+    def default_auto_field(self):
+        from django.conf import settings
+        return settings.DEFAULT_AUTO_FIELD
+
+    @property
+    def _is_default_auto_field_overridden(self):
+        return self.__class__.default_auto_field is not AppConfig.default_auto_field
 
     def _path_from_module(self, module):
         """Attempt to determine app's filesystem path from its module."""

django/conf/app_template/apps.py-tpl
@@ -2,4 +2,5 @@ from django.apps import AppConfig
 
 
 class {{ camel_case_app_name }}Config(AppConfig):
+    default_auto_field = 'django.db.models.BigAutoField'
     name = '{{ app_name }}'

django/conf/global_settings.py
@@ -414,6 +414,9 @@ THOUSAND_SEPARATOR = ','
 DEFAULT_TABLESPACE = ''
 DEFAULT_INDEX_TABLESPACE = ''
 
+# Default primary key field type.
+DEFAULT_AUTO_FIELD = 'django.db.models.AutoField'
+
 # Default X-Frame-Options header value
 X_FRAME_OPTIONS = 'DENY'
 

django/conf/project_template/project_name/settings.py-tpl
@@ -118,3 +118,8 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/{{ docs_version }}/howto/static-files/
 
 STATIC_URL = '/static/'
+
+# Default primary key field type
+# https://docs.djangoproject.com/en/{{ docs_version }}/ref/settings/#default-auto-field
+
+DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'

django/contrib/admin/apps.py
@@ -7,6 +7,7 @@ from django.utils.translation import gettext_lazy as _
 class SimpleAdminConfig(AppConfig):
     """Simple AppConfig which does not do automatic discovery."""
 
+    default_auto_field = 'django.db.models.AutoField'
     default_site = 'django.contrib.admin.sites.AdminSite'
     name = 'django.contrib.admin'
     verbose_name = _("Administration")

django/contrib/auth/apps.py
@@ -11,6 +11,7 @@ from .signals import user_logged_in
 
 
 class AuthConfig(AppConfig):
+    default_auto_field = 'django.db.models.AutoField'
     name = 'django.contrib.auth'
     verbose_name = _("Authentication and Authorization")
 

commit 4435d0ef7f1dac067f71ef238c213045f36ff382

Fixed #32367 -- Fixed system check for specifying type of auto-created primary keys for inherited PKs.
Regression in a7038600966b4c4f021033103f3d715b76778b0c.

Thanks אורי for the report.

django/db/models/base.py
@@ -1299,6 +1299,11 @@ class Model(metaclass=ModelBase):
     def _check_default_pk(cls):
         if (
             cls._meta.pk.auto_created and
+            # Inherited PKs are checked in parents models.
+            not (
+                isinstance(cls._meta.pk, OneToOneField) and
+                cls._meta.pk.remote_field.parent_link
+            ) and
             not settings.is_overridden('DEFAULT_AUTO_FIELD') and
             not cls._meta.app_config._is_default_auto_field_overridden
         ):

tests/check_framework/test_model_checks.py
@@ -376,23 +376,62 @@ def mocked_is_overridden(self, setting):
 @isolate_apps('check_framework.apps.CheckDefaultPKConfig', attr_name='apps')
 @override_system_checks([checks.model_checks.check_all_models])
 class ModelDefaultAutoFieldTests(SimpleTestCase):
+    msg = (
+        "Auto-created primary key used when not defining a primary key type, "
+        "by default 'django.db.models.AutoField'."
+    )
+    hint = (
+        "Configure the DEFAULT_AUTO_FIELD setting or the "
+        "CheckDefaultPKConfig.default_auto_field attribute to point to a "
+        "subclass of AutoField, e.g. 'django.db.models.BigAutoField'."
+    )
+
     def test_auto_created_pk(self):
         class Model(models.Model):
             pass
 
         self.assertEqual(checks.run_checks(app_configs=self.apps.get_app_configs()), [
-            Warning(
-                "Auto-created primary key used when not defining a primary "
-                "key type, by default 'django.db.models.AutoField'.",
-                hint=(
-                    "Configure the DEFAULT_AUTO_FIELD setting or the "
-                    "CheckDefaultPKConfig.default_auto_field attribute to "
-                    "point to a subclass of AutoField, e.g. "
-                    "'django.db.models.BigAutoField'."
-                ),
-                obj=Model,
-                id='models.W042',
-            ),
+            Warning(self.msg, hint=self.hint, obj=Model, id='models.W042'),
+        ])
+
+    def test_explicit_inherited_pk(self):
+        class Parent(models.Model):
+            id = models.AutoField(primary_key=True)
+
+        class Child(Parent):
+            pass
+
+        self.assertEqual(checks.run_checks(app_configs=self.apps.get_app_configs()), [])
+
+    def test_explicit_inherited_parent_link(self):
+        class Parent(models.Model):
+            id = models.AutoField(primary_key=True)
+
+        class Child(Parent):
+            parent_ptr = models.OneToOneField(Parent, models.CASCADE, parent_link=True)
+
+        self.assertEqual(checks.run_checks(app_configs=self.apps.get_app_configs()), [])
+
+    def test_auto_created_inherited_pk(self):
+        class Parent(models.Model):
+            pass
+
+        class Child(Parent):
+            pass
+
+        self.assertEqual(checks.run_checks(app_configs=self.apps.get_app_configs()), [
+            Warning(self.msg, hint=self.hint, obj=Parent, id='models.W042'),
+        ])
+
+    def test_auto_created_inherited_parent_link(self):
+        class Parent(models.Model):
+            pass
+
+        class Child(Parent):
+            parent_ptr = models.OneToOneField(Parent, models.CASCADE, parent_link=True)
+
+        self.assertEqual(checks.run_checks(app_configs=self.apps.get_app_configs()), [
+            Warning(self.msg, hint=self.hint, obj=Parent, id='models.W042'),
         ])
 
     @override_settings(DEFAULT_AUTO_FIELD='django.db.models.BigAutoField')