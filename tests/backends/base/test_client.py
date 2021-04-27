usernameisnull/django

commit c47b5b2f3ec71f3de4e2838b27348ec0337b0c86

Refs #32061 -- Unified DatabaseClient.runshell() in db backends.

django/db/backends/base/client.py
@@ -1,3 +1,7 @@
+import os
+import subprocess
+
+
 class BaseDatabaseClient:
     """Encapsulate backend-specific methods for opening a client shell."""
     # This should be a string representing the name of the executable
@@ -8,5 +12,15 @@ class BaseDatabaseClient:
         # connection is an instance of BaseDatabaseWrapper.
         self.connection = connection
 
+    @classmethod
+    def settings_to_cmd_args_env(cls, settings_dict, parameters):
+        raise NotImplementedError(
+            'subclasses of BaseDatabaseClient must provide a '
+            'settings_to_cmd_args_env() method or override a runshell().'
+        )
+
     def runshell(self, parameters):
-        raise NotImplementedError('subclasses of BaseDatabaseClient must provide a runshell() method')
+        args, env = self.settings_to_cmd_args_env(self.connection.settings_dict, parameters)
+        if env:
+            env = {**os.environ, **env}
+        subprocess.run(args, env=env, check=True)

django/db/backends/postgresql/client.py
@@ -1,6 +1,4 @@
-import os
 import signal
-import subprocess
 
 from django.db.backends.base.client import BaseDatabaseClient
 
@@ -9,18 +7,19 @@ class DatabaseClient(BaseDatabaseClient):
     executable_name = 'psql'
 
     @classmethod
-    def runshell_db(cls, conn_params, parameters):
+    def settings_to_cmd_args_env(cls, settings_dict, parameters):
         args = [cls.executable_name]
+        options = settings_dict.get('OPTIONS', {})
 
-        host = conn_params.get('host', '')
-        port = conn_params.get('port', '')
-        dbname = conn_params.get('database', '')
-        user = conn_params.get('user', '')
-        passwd = conn_params.get('password', '')
-        sslmode = conn_params.get('sslmode', '')
-        sslrootcert = conn_params.get('sslrootcert', '')
-        sslcert = conn_params.get('sslcert', '')
-        sslkey = conn_params.get('sslkey', '')
+        host = settings_dict.get('HOST')
+        port = settings_dict.get('PORT')
+        dbname = settings_dict.get('NAME') or 'postgres'
+        user = settings_dict.get('USER')
+        passwd = settings_dict.get('PASSWORD')
+        sslmode = options.get('sslmode')
+        sslrootcert = options.get('sslrootcert')
+        sslcert = options.get('sslcert')
+        sslkey = options.get('sslkey')
 
         if user:
             args += ['-U', user]
@@ -31,25 +30,25 @@ class DatabaseClient(BaseDatabaseClient):
         args += [dbname]
         args.extend(parameters)
 
-        sigint_handler = signal.getsignal(signal.SIGINT)
-        subprocess_env = os.environ.copy()
+        env = {}
         if passwd:
-            subprocess_env['PGPASSWORD'] = str(passwd)
+            env['PGPASSWORD'] = str(passwd)
         if sslmode:
-            subprocess_env['PGSSLMODE'] = str(sslmode)
+            env['PGSSLMODE'] = str(sslmode)
         if sslrootcert:
-            subprocess_env['PGSSLROOTCERT'] = str(sslrootcert)
+            env['PGSSLROOTCERT'] = str(sslrootcert)
         if sslcert:
-            subprocess_env['PGSSLCERT'] = str(sslcert)
+            env['PGSSLCERT'] = str(sslcert)
         if sslkey:
-            subprocess_env['PGSSLKEY'] = str(sslkey)
+            env['PGSSLKEY'] = str(sslkey)
+        return args, env
+
+    def runshell(self, parameters):
+        sigint_handler = signal.getsignal(signal.SIGINT)
         try:
             # Allow SIGINT to pass to psql to abort queries.
             signal.signal(signal.SIGINT, signal.SIG_IGN)
-            subprocess.run(args, check=True, env=subprocess_env)
+            super().runshell(parameters)
         finally:
             # Restore the original SIGINT handler.
             signal.signal(signal.SIGINT, sigint_handler)
-
-    def runshell(self, parameters):
-        self.runshell_db(self.connection.get_connection_params(), parameters)

tests/backends/base/test_client.py
@@ -1,0 +1,16 @@
+from django.db import connection
+from django.db.backends.base.client import BaseDatabaseClient
+from django.test import SimpleTestCase
+
+
+class SimpleDatabaseClientTests(SimpleTestCase):
+    def setUp(self):
+        self.client = BaseDatabaseClient(connection=connection)
+
+    def test_settings_to_cmd_args_env(self):
+        msg = (
+            'subclasses of BaseDatabaseClient must provide a '
+            'settings_to_cmd_args_env() method or override a runshell().'
+        )
+        with self.assertRaisesMessage(NotImplementedError, msg):
+            self.client.settings_to_cmd_args_env(None, None)

tests/dbshell/test_postgresql.py
@@ -1,41 +1,25 @@
-import os
 import signal
-import subprocess
-from unittest import mock
+from unittest import mock, skipUnless
 
+from django.db import connection
 from django.db.backends.postgresql.client import DatabaseClient
 from django.test import SimpleTestCase
 
 
 class PostgreSqlDbshellCommandTestCase(SimpleTestCase):
-
-    def _run_it(self, dbinfo, parameters=None):
-        """
-        That function invokes the runshell command, while mocking
-        subprocess.run(). It returns a 2-tuple with:
-        - The command line list
-        - The dictionary of PG* environment variables, or {}.
-        """
-        def _mock_subprocess_run(*args, env=os.environ, **kwargs):
-            self.subprocess_args = list(*args)
-            # PostgreSQL environment variables.
-            self.pg_env = {key: env[key] for key in env if key.startswith('PG')}
-            return subprocess.CompletedProcess(self.subprocess_args, 0)
-
+    def settings_to_cmd_args_env(self, settings_dict, parameters=None):
         if parameters is None:
             parameters = []
-        with mock.patch('subprocess.run', new=_mock_subprocess_run):
-            DatabaseClient.runshell_db(dbinfo, parameters)
-        return self.subprocess_args, self.pg_env
+        return DatabaseClient.settings_to_cmd_args_env(settings_dict, parameters)
 
     def test_basic(self):
         self.assertEqual(
-            self._run_it({
-                'database': 'dbname',
-                'user': 'someuser',
-                'password': 'somepassword',
-                'host': 'somehost',
-                'port': '444',
+            self.settings_to_cmd_args_env({
+                'NAME': 'dbname',
+                'USER': 'someuser',
+                'PASSWORD': 'somepassword',
+                'HOST': 'somehost',
+                'PORT': '444',
             }), (
                 ['psql', '-U', 'someuser', '-h', 'somehost', '-p', '444', 'dbname'],
                 {'PGPASSWORD': 'somepassword'},
@@ -44,11 +28,11 @@ class PostgreSqlDbshellCommandTestCase(SimpleTestCase):
 
     def test_nopass(self):
         self.assertEqual(
-            self._run_it({
-                'database': 'dbname',
-                'user': 'someuser',
-                'host': 'somehost',
-                'port': '444',
+            self.settings_to_cmd_args_env({
+                'NAME': 'dbname',
+                'USER': 'someuser',
+                'HOST': 'somehost',
+                'PORT': '444',
             }), (
                 ['psql', '-U', 'someuser', '-h', 'somehost', '-p', '444', 'dbname'],
                 {},
@@ -57,15 +41,17 @@ class PostgreSqlDbshellCommandTestCase(SimpleTestCase):
 
     def test_ssl_certificate(self):
         self.assertEqual(
-            self._run_it({
-                'database': 'dbname',
-                'user': 'someuser',
-                'host': 'somehost',
-                'port': '444',
-                'sslmode': 'verify-ca',
-                'sslrootcert': 'root.crt',
-                'sslcert': 'client.crt',
-                'sslkey': 'client.key',
+            self.settings_to_cmd_args_env({
+                'NAME': 'dbname',
+                'USER': 'someuser',
+                'HOST': 'somehost',
+                'PORT': '444',
+                'OPTIONS': {
+                    'sslmode': 'verify-ca',
+                    'sslrootcert': 'root.crt',
+                    'sslcert': 'client.crt',
+                    'sslkey': 'client.key',
+                },
             }), (
                 ['psql', '-U', 'someuser', '-h', 'somehost', '-p', '444', 'dbname'],
                 {
@@ -79,12 +65,12 @@ class PostgreSqlDbshellCommandTestCase(SimpleTestCase):
 
     def test_column(self):
         self.assertEqual(
-            self._run_it({
-                'database': 'dbname',
-                'user': 'some:user',
-                'password': 'some:password',
-                'host': '::1',
-                'port': '444',
+            self.settings_to_cmd_args_env({
+                'NAME': 'dbname',
+                'USER': 'some:user',
+                'PASSWORD': 'some:password',
+                'HOST': '::1',
+                'PORT': '444',
             }), (
                 ['psql', '-U', 'some:user', '-h', '::1', '-p', '444', 'dbname'],
                 {'PGPASSWORD': 'some:password'},
@@ -95,12 +81,12 @@ class PostgreSqlDbshellCommandTestCase(SimpleTestCase):
         username = 'rôle'
         password = 'sésame'
         self.assertEqual(
-            self._run_it({
-                'database': 'dbname',
-                'user': username,
-                'password': password,
-                'host': 'somehost',
-                'port': '444',
+            self.settings_to_cmd_args_env({
+                'NAME': 'dbname',
+                'USER': username,
+                'PASSWORD': password,
+                'HOST': 'somehost',
+                'PORT': '444',
             }), (
                 ['psql', '-U', username, '-h', 'somehost', '-p', '444', 'dbname'],
                 {'PGPASSWORD': password},
@@ -109,10 +95,11 @@ class PostgreSqlDbshellCommandTestCase(SimpleTestCase):
 
     def test_parameters(self):
         self.assertEqual(
-            self._run_it({'database': 'dbname'}, ['--help']),
+            self.settings_to_cmd_args_env({'NAME': 'dbname'}, ['--help']),
             (['psql', 'dbname', '--help'], {}),
         )
 
+    @skipUnless(connection.vendor == 'postgresql', 'Requires a PostgreSQL connection')
     def test_sigint_handler(self):
         """SIGINT is ignored in Python and passed to psql to abort queries."""
         def _mock_subprocess_run(*args, **kwargs):
@@ -123,6 +110,6 @@ class PostgreSqlDbshellCommandTestCase(SimpleTestCase):
         # The default handler isn't SIG_IGN.
         self.assertNotEqual(sigint_handler, signal.SIG_IGN)
         with mock.patch('subprocess.run', new=_mock_subprocess_run):
-            DatabaseClient.runshell_db({}, [])
+            connection.client.runshell([])
         # dbshell restores the original handler.
         self.assertEqual(sigint_handler, signal.getsignal(signal.SIGINT))

django/db/backends/mysql/client.py
@@ -1,5 +1,3 @@
-import subprocess
-
 from django.db.backends.base.client import BaseDatabaseClient
 
 
@@ -7,7 +5,7 @@ class DatabaseClient(BaseDatabaseClient):
     executable_name = 'mysql'
 
     @classmethod
-    def settings_to_cmd_args(cls, settings_dict, parameters):
+    def settings_to_cmd_args_env(cls, settings_dict, parameters):
         args = [cls.executable_name]
         db = settings_dict['OPTIONS'].get('db', settings_dict['NAME'])
         user = settings_dict['OPTIONS'].get('user', settings_dict['USER'])
@@ -48,8 +46,4 @@ class DatabaseClient(BaseDatabaseClient):
         if db:
             args += [db]
         args.extend(parameters)
-        return args
-
-    def runshell(self, parameters):
-        args = DatabaseClient.settings_to_cmd_args(self.connection.settings_dict, parameters)
-        subprocess.run(args, check=True)
+        return args, None

django/db/backends/oracle/client.py
@@ -1,5 +1,4 @@
 import shutil
-import subprocess
 
 from django.db.backends.base.client import BaseDatabaseClient
 
@@ -8,11 +7,21 @@ class DatabaseClient(BaseDatabaseClient):
     executable_name = 'sqlplus'
     wrapper_name = 'rlwrap'
 
-    def runshell(self, parameters):
-        conn_string = self.connection._connect_string()
-        args = [self.executable_name, "-L", conn_string]
-        wrapper_path = shutil.which(self.wrapper_name)
+    @staticmethod
+    def connect_string(settings_dict):
+        from django.db.backends.oracle.utils import dsn
+
+        return '%s/"%s"@%s' % (
+            settings_dict['USER'],
+            settings_dict['PASSWORD'],
+            dsn(settings_dict),
+        )
+
+    @classmethod
+    def settings_to_cmd_args_env(cls, settings_dict, parameters):
+        args = [cls.executable_name, '-L', cls.connect_string(settings_dict)]
+        wrapper_path = shutil.which(cls.wrapper_name)
         if wrapper_path:
             args = [wrapper_path, *args]
         args.extend(parameters)
-        subprocess.run(args, check=True)
+        return args, None

django/db/backends/sqlite3/client.py
@@ -1,15 +1,16 @@
-import subprocess
-
 from django.db.backends.base.client import BaseDatabaseClient
 
 
 class DatabaseClient(BaseDatabaseClient):
     executable_name = 'sqlite3'
 
-    def runshell(self, parameters):
-        # TODO: Remove str() when dropping support for PY37.
-        # args parameter accepts path-like objects on Windows since Python 3.8.
-        args = [self.executable_name,
-                str(self.connection.settings_dict['NAME'])]
-        args.extend(parameters)
-        subprocess.run(args, check=True)
+    @classmethod
+    def settings_to_cmd_args_env(cls, settings_dict, parameters):
+        args = [
+            cls.executable_name,
+            # TODO: Remove str() when dropping support for PY37. args
+            # parameter accepts path-like objects on Windows since Python 3.8.
+            str(settings_dict['NAME']),
+            *parameters,
+        ]
+        return args, None

django/db/backends/mysql/creation.py
@@ -1,3 +1,4 @@
+import os
 import subprocess
 import sys
 
@@ -55,12 +56,13 @@ class DatabaseCreation(BaseDatabaseCreation):
         self._clone_db(source_database_name, target_database_name)
 
     def _clone_db(self, source_database_name, target_database_name):
-        dump_args = DatabaseClient.settings_to_cmd_args(self.connection.settings_dict, [])[1:]
-        dump_cmd = ['mysqldump', *dump_args[:-1], '--routines', '--events', source_database_name]
-        load_cmd = DatabaseClient.settings_to_cmd_args(self.connection.settings_dict, [])
+        cmd_args, cmd_env = DatabaseClient.settings_to_cmd_args_env(self.connection.settings_dict, [])
+        dump_cmd = ['mysqldump', *cmd_args[1:-1], '--routines', '--events', source_database_name]
+        dump_env = load_env = {**os.environ, **cmd_env} if cmd_env else None
+        load_cmd = cmd_args
         load_cmd[-1] = target_database_name
 
-        with subprocess.Popen(dump_cmd, stdout=subprocess.PIPE) as dump_proc:
-            with subprocess.Popen(load_cmd, stdin=dump_proc.stdout, stdout=subprocess.DEVNULL):
+        with subprocess.Popen(dump_cmd, stdout=subprocess.PIPE, env=dump_env) as dump_proc:
+            with subprocess.Popen(load_cmd, stdin=dump_proc.stdout, stdout=subprocess.DEVNULL, env=load_env):
                 # Allow dump_proc to receive a SIGPIPE if the load process exits.
                 dump_proc.stdout.close()

commit 66dd28a013908dab8f96165a736db13097cc08a8

Fixed #32687 -- Restored passing process’ environment to underlying tool in dbshell on PostgreSQL.
Regression in c47b5b2f3ec71f3de4e2838b27348ec0337b0c86.

django/db/backends/base/client.py
@@ -21,6 +21,5 @@ class BaseDatabaseClient:
 
     def runshell(self, parameters):
         args, env = self.settings_to_cmd_args_env(self.connection.settings_dict, parameters)
-        if env:
-            env = {**os.environ, **env}
+        env = {**os.environ, **env} if env else None
         subprocess.run(args, env=env, check=True)

django/db/backends/postgresql/client.py
@@ -51,7 +51,7 @@ class DatabaseClient(BaseDatabaseClient):
             env['PGSSLKEY'] = str(sslkey)
         if passfile:
             env['PGPASSFILE'] = str(passfile)
-        return args, env
+        return args, (env or None)
 
     def runshell(self, parameters):
         sigint_handler = signal.getsignal(signal.SIGINT)

tests/backends/base/test_client.py
@@ -1,3 +1,5 @@
+from unittest import mock
+
 from django.db import connection
 from django.db.backends.base.client import BaseDatabaseClient
 from django.test import SimpleTestCase
@@ -14,3 +16,15 @@ class SimpleDatabaseClientTests(SimpleTestCase):
         )
         with self.assertRaisesMessage(NotImplementedError, msg):
             self.client.settings_to_cmd_args_env(None, None)
+
+    def test_runshell_use_environ(self):
+        for env in [None, {}]:
+            with self.subTest(env=env):
+                with mock.patch('subprocess.run') as run:
+                    with mock.patch.object(
+                        BaseDatabaseClient,
+                        'settings_to_cmd_args_env',
+                        return_value=([], env),
+                    ):
+                        self.client.runshell(None)
+                    run.assert_called_once_with([], env=None, check=True)

tests/dbshell/test_postgresql.py
@@ -39,7 +39,7 @@ class PostgreSqlDbshellCommandTestCase(SimpleTestCase):
                 'PORT': '444',
             }), (
                 ['psql', '-U', 'someuser', '-h', 'somehost', '-p', '444', 'dbname'],
-                {},
+                None,
             )
         )
 
@@ -134,7 +134,7 @@ class PostgreSqlDbshellCommandTestCase(SimpleTestCase):
     def test_parameters(self):
         self.assertEqual(
             self.settings_to_cmd_args_env({'NAME': 'dbname'}, ['--help']),
-            (['psql', 'dbname', '--help'], {}),
+            (['psql', 'dbname', '--help'], None),
         )
 
     @skipUnless(connection.vendor == 'postgresql', 'Requires a PostgreSQL connection')