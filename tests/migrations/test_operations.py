usernameisnull/django

commit 0ecbf49d43af2fc6944f7bffd6c635ba8a2617ca

Fixed #26167 -- Added support for functional indexes.
Thanks Simon Charette, Mads Jensen, and Mariusz Felisiak for reviews.

Co-authored-by: Markus Holtermann <info@markusholtermann.eu>

django/db/backends/ddl_references.py
@@ -2,6 +2,7 @@
 Helpers to manipulate deferred DDL statements that might need to be adjusted or
 discarded within when executing a migration.
 """
+from copy import deepcopy
 
 
 class Reference:
@@ -198,3 +199,38 @@ class Statement(Reference):
 
     def __str__(self):
         return self.template % self.parts
+
+
+class Expressions(TableColumns):
+    def __init__(self, table, expressions, compiler, quote_value):
+        self.compiler = compiler
+        self.expressions = expressions
+        self.quote_value = quote_value
+        columns = [col.target.column for col in self.compiler.query._gen_cols([self.expressions])]
+        super().__init__(table, columns)
+
+    def rename_table_references(self, old_table, new_table):
+        if self.table != old_table:
+            return
+        expressions = deepcopy(self.expressions)
+        self.columns = []
+        for col in self.compiler.query._gen_cols([expressions]):
+            col.alias = new_table
+        self.expressions = expressions
+        super().rename_table_references(old_table, new_table)
+
+    def rename_column_references(self, table, old_column, new_column):
+        if self.table != table:
+            return
+        expressions = deepcopy(self.expressions)
+        self.columns = []
+        for col in self.compiler.query._gen_cols([expressions]):
+            if col.target.column == old_column:
+                col.target.column = new_column
+            self.columns.append(col.target.column)
+        self.expressions = expressions
+
+    def __str__(self):
+        sql, params = self.compiler.compile(self.expressions)
+        params = map(self.quote_value, params)
+        return sql % tuple(params)

tests/backends/test_ddl_references.py
@@ -1,7 +1,13 @@
+from django.db import connection
 from django.db.backends.ddl_references import (
-    Columns, ForeignKeyName, IndexName, Statement, Table,
+    Columns, Expressions, ForeignKeyName, IndexName, Statement, Table,
 )
-from django.test import SimpleTestCase
+from django.db.models import ExpressionList, F
+from django.db.models.functions import Upper
+from django.db.models.indexes import IndexExpression
+from django.test import SimpleTestCase, TransactionTestCase
+
+from .models import Person
 
 
 class TableTests(SimpleTestCase):
@@ -181,3 +187,66 @@ class StatementTests(SimpleTestCase):
         reference = MockReference('reference', {}, {})
         statement = Statement("%(reference)s - %(non_reference)s", reference=reference, non_reference='non_reference')
         self.assertEqual(str(statement), 'reference - non_reference')
+
+
+class ExpressionsTests(TransactionTestCase):
+    available_apps = []
+
+    def setUp(self):
+        compiler = Person.objects.all().query.get_compiler(connection.alias)
+        self.editor = connection.schema_editor()
+        self.expressions = Expressions(
+            table=Person._meta.db_table,
+            expressions=ExpressionList(
+                IndexExpression(F('first_name')),
+                IndexExpression(F('last_name').desc()),
+                IndexExpression(Upper('last_name')),
+            ).resolve_expression(compiler.query),
+            compiler=compiler,
+            quote_value=self.editor.quote_value,
+        )
+
+    def test_references_table(self):
+        self.assertIs(self.expressions.references_table(Person._meta.db_table), True)
+        self.assertIs(self.expressions.references_table('other'), False)
+
+    def test_references_column(self):
+        table = Person._meta.db_table
+        self.assertIs(self.expressions.references_column(table, 'first_name'), True)
+        self.assertIs(self.expressions.references_column(table, 'last_name'), True)
+        self.assertIs(self.expressions.references_column(table, 'other'), False)
+
+    def test_rename_table_references(self):
+        table = Person._meta.db_table
+        self.expressions.rename_table_references(table, 'other')
+        self.assertIs(self.expressions.references_table(table), False)
+        self.assertIs(self.expressions.references_table('other'), True)
+        self.assertIn(
+            '%s.%s' % (
+                self.editor.quote_name('other'),
+                self.editor.quote_name('first_name'),
+            ),
+            str(self.expressions),
+        )
+
+    def test_rename_column_references(self):
+        table = Person._meta.db_table
+        self.expressions.rename_column_references(table, 'first_name', 'other')
+        self.assertIs(self.expressions.references_column(table, 'other'), True)
+        self.assertIs(self.expressions.references_column(table, 'first_name'), False)
+        self.assertIn(
+            '%s.%s' % (self.editor.quote_name(table), self.editor.quote_name('other')),
+            str(self.expressions),
+        )
+
+    def test_str(self):
+        table_name = self.editor.quote_name(Person._meta.db_table)
+        expected_str = '%s.%s, %s.%s DESC, (UPPER(%s.%s))' % (
+            table_name,
+            self.editor.quote_name('first_name'),
+            table_name,
+            self.editor.quote_name('last_name'),
+            table_name,
+            self.editor.quote_name('last_name'),
+        )
+        self.assertEqual(str(self.expressions), expected_str)

tests/migrations/test_operations.py
@@ -5,6 +5,7 @@ from django.db import (
 from django.db.migrations.migration import Migration
 from django.db.migrations.operations.fields import FieldOperation
 from django.db.migrations.state import ModelState, ProjectState
+from django.db.models.functions import Abs
 from django.db.transaction import atomic
 from django.test import SimpleTestCase, override_settings, skipUnlessDBFeature
 
@@ -1938,6 +1939,76 @@ class OperationTests(OperationTestBase):
         operation.state_forwards('test_rminsf', new_state)
         new_model = new_state.apps.get_model('test_rminsf', 'Pony')
         self.assertIsNot(old_model, new_model)
+
+    @skipUnlessDBFeature('supports_expression_indexes')
+    def test_add_func_index(self):
+        app_label = 'test_addfuncin'
+        index_name = f'{app_label}_pony_abs_idx'
+        table_name = f'{app_label}_pony'
+        project_state = self.set_up_test_model(app_label)
+        index = models.Index(Abs('weight'), name=index_name)
+        operation = migrations.AddIndex('Pony', index)
+        self.assertEqual(
+            operation.describe(),
+            'Create index test_addfuncin_pony_abs_idx on Abs(F(weight)) on model Pony',
+        )
+        self.assertEqual(
+            operation.migration_name_fragment,
+            'pony_test_addfuncin_pony_abs_idx',
+        )
+        new_state = project_state.clone()
+        operation.state_forwards(app_label, new_state)
+        self.assertEqual(len(new_state.models[app_label, 'pony'].options['indexes']), 1)
+        self.assertIndexNameNotExists(table_name, index_name)
+        # Add index.
+        with connection.schema_editor() as editor:
+            operation.database_forwards(app_label, editor, project_state, new_state)
+        self.assertIndexNameExists(table_name, index_name)
+        # Reversal.
+        with connection.schema_editor() as editor:
+            operation.database_backwards(app_label, editor, new_state, project_state)
+        self.assertIndexNameNotExists(table_name, index_name)
+        # Deconstruction.
+        definition = operation.deconstruct()
+        self.assertEqual(definition[0], 'AddIndex')
+        self.assertEqual(definition[1], [])
+        self.assertEqual(definition[2], {'model_name': 'Pony', 'index': index})
+
+    @skipUnlessDBFeature('supports_expression_indexes')
+    def test_remove_func_index(self):
+        app_label = 'test_rmfuncin'
+        index_name = f'{app_label}_pony_abs_idx'
+        table_name = f'{app_label}_pony'
+        project_state = self.set_up_test_model(app_label, indexes=[
+            models.Index(Abs('weight'), name=index_name),
+        ])
+        self.assertTableExists(table_name)
+        self.assertIndexNameExists(table_name, index_name)
+        operation = migrations.RemoveIndex('Pony', index_name)
+        self.assertEqual(
+            operation.describe(),
+            'Remove index test_rmfuncin_pony_abs_idx from Pony',
+        )
+        self.assertEqual(
+            operation.migration_name_fragment,
+            'remove_pony_test_rmfuncin_pony_abs_idx',
+        )
+        new_state = project_state.clone()
+        operation.state_forwards(app_label, new_state)
+        self.assertEqual(len(new_state.models[app_label, 'pony'].options['indexes']), 0)
+        # Remove index.
+        with connection.schema_editor() as editor:
+            operation.database_forwards(app_label, editor, project_state, new_state)
+        self.assertIndexNameNotExists(table_name, index_name)
+        # Reversal.
+        with connection.schema_editor() as editor:
+            operation.database_backwards(app_label, editor, new_state, project_state)
+        self.assertIndexNameExists(table_name, index_name)
+        # Deconstruction.
+        definition = operation.deconstruct()
+        self.assertEqual(definition[0], 'RemoveIndex')
+        self.assertEqual(definition[1], [])
+        self.assertEqual(definition[2], {'model_name': 'Pony', 'name': index_name})
 
     def test_alter_field_with_index(self):
         """

django/contrib/postgres/apps.py
@@ -6,10 +6,13 @@ from django.apps import AppConfig
 from django.db import connections
 from django.db.backends.signals import connection_created
 from django.db.migrations.writer import MigrationWriter
-from django.db.models import CharField, TextField
+from django.db.models import CharField, OrderBy, TextField
+from django.db.models.functions import Collate
+from django.db.models.indexes import IndexExpression
 from django.test.signals import setting_changed
 from django.utils.translation import gettext_lazy as _
 
+from .indexes import OpClass
 from .lookups import SearchLookup, TrigramSimilar, Unaccent
 from .serializers import RangeSerializer
 from .signals import register_type_handlers
@@ -63,3 +66,4 @@ class PostgresConfig(AppConfig):
         CharField.register_lookup(TrigramSimilar)
         TextField.register_lookup(TrigramSimilar)
         MigrationWriter.register_serializer(RANGE_TYPES, RangeSerializer)
+        IndexExpression.register_wrappers(OrderBy, OpClass, Collate)

django/contrib/postgres/indexes.py
@@ -1,5 +1,5 @@
 from django.db import NotSupportedError
-from django.db.models import Index
+from django.db.models import Func, Index
 from django.utils.functional import cached_property
 
 __all__ = [
@@ -39,8 +39,8 @@ class PostgresIndex(Index):
 class BloomIndex(PostgresIndex):
     suffix = 'bloom'
 
-    def __init__(self, *, length=None, columns=(), **kwargs):
-        super().__init__(**kwargs)
+    def __init__(self, *expressions, length=None, columns=(), **kwargs):
+        super().__init__(*expressions, **kwargs)
         if len(self.fields) > 32:
             raise ValueError('Bloom indexes support a maximum of 32 fields.')
         if not isinstance(columns, (list, tuple)):
@@ -83,12 +83,12 @@ class BloomIndex(PostgresIndex):
 class BrinIndex(PostgresIndex):
     suffix = 'brin'
 
-    def __init__(self, *, autosummarize=None, pages_per_range=None, **kwargs):
+    def __init__(self, *expressions, autosummarize=None, pages_per_range=None, **kwargs):
         if pages_per_range is not None and pages_per_range <= 0:
             raise ValueError('pages_per_range must be None or a positive integer')
         self.autosummarize = autosummarize
         self.pages_per_range = pages_per_range
-        super().__init__(**kwargs)
+        super().__init__(*expressions, **kwargs)
 
     def deconstruct(self):
         path, args, kwargs = super().deconstruct()
@@ -114,9 +114,9 @@ class BrinIndex(PostgresIndex):
 class BTreeIndex(PostgresIndex):
     suffix = 'btree'
 
-    def __init__(self, *, fillfactor=None, **kwargs):
+    def __init__(self, *expressions, fillfactor=None, **kwargs):
         self.fillfactor = fillfactor
-        super().__init__(**kwargs)
+        super().__init__(*expressions, **kwargs)
 
     def deconstruct(self):
         path, args, kwargs = super().deconstruct()
@@ -134,10 +134,10 @@ class BTreeIndex(PostgresIndex):
 class GinIndex(PostgresIndex):
     suffix = 'gin'
 
-    def __init__(self, *, fastupdate=None, gin_pending_list_limit=None, **kwargs):
+    def __init__(self, *expressions, fastupdate=None, gin_pending_list_limit=None, **kwargs):
         self.fastupdate = fastupdate
         self.gin_pending_list_limit = gin_pending_list_limit
-        super().__init__(**kwargs)
+        super().__init__(*expressions, **kwargs)
 
     def deconstruct(self):
         path, args, kwargs = super().deconstruct()
@@ -159,10 +159,10 @@ class GinIndex(PostgresIndex):
 class GistIndex(PostgresIndex):
     suffix = 'gist'
 
-    def __init__(self, *, buffering=None, fillfactor=None, **kwargs):
+    def __init__(self, *expressions, buffering=None, fillfactor=None, **kwargs):
         self.buffering = buffering
         self.fillfactor = fillfactor
-        super().__init__(**kwargs)
+        super().__init__(*expressions, **kwargs)
 
     def deconstruct(self):
         path, args, kwargs = super().deconstruct()
@@ -188,9 +188,9 @@ class GistIndex(PostgresIndex):
 class HashIndex(PostgresIndex):
     suffix = 'hash'
 
-    def __init__(self, *, fillfactor=None, **kwargs):
+    def __init__(self, *expressions, fillfactor=None, **kwargs):
         self.fillfactor = fillfactor
-        super().__init__(**kwargs)
+        super().__init__(*expressions, **kwargs)
 
     def deconstruct(self):
         path, args, kwargs = super().deconstruct()
@@ -208,9 +208,9 @@ class HashIndex(PostgresIndex):
 class SpGistIndex(PostgresIndex):
     suffix = 'spgist'
 
-    def __init__(self, *, fillfactor=None, **kwargs):
+    def __init__(self, *expressions, fillfactor=None, **kwargs):
         self.fillfactor = fillfactor
-        super().__init__(**kwargs)
+        super().__init__(*expressions, **kwargs)
 
     def deconstruct(self):
         path, args, kwargs = super().deconstruct()
@@ -223,3 +223,10 @@ class SpGistIndex(PostgresIndex):
         if self.fillfactor is not None:
             with_params.append('fillfactor = %d' % self.fillfactor)
         return with_params
+
+
+class OpClass(Func):
+    template = '%(expressions)s %(name)s'
+
+    def __init__(self, expression, name):
+        super().__init__(expression, name=name)

django/db/backends/base/features.py
@@ -281,6 +281,10 @@ class BaseDatabaseFeatures:
     supports_functions_in_partial_indexes = True
     # Does the backend support covering indexes (CREATE INDEX ... INCLUDE ...)?
     supports_covering_indexes = False
+    # Does the backend support indexes on expressions?
+    supports_expression_indexes = False
+    # Does the backend treat COLLATE as an indexed expression?
+    collate_as_index_expression = False
 
     # Does the database allow more than one constraint or index on the same
     # field(s)?

django/db/backends/base/schema.py
@@ -2,10 +2,11 @@ import logging
 from datetime import datetime
 
 from django.db.backends.ddl_references import (
-    Columns, ForeignKeyName, IndexName, Statement, Table,
+    Columns, Expressions, ForeignKeyName, IndexName, Statement, Table,
 )
 from django.db.backends.utils import names_digest, split_identifier
 from django.db.models import Deferrable, Index
+from django.db.models.sql import Query
 from django.db.transaction import TransactionManagementError, atomic
 from django.utils import timezone
 
@@ -354,10 +355,20 @@ class BaseDatabaseSchemaEditor:
 
     def add_index(self, model, index):
         """Add an index on a model."""
+        if (
+            index.contains_expressions and
+            not self.connection.features.supports_expression_indexes
+        ):
+            return None
         self.execute(index.create_sql(model, self), params=None)
 
     def remove_index(self, model, index):
         """Remove an index from a model."""
+        if (
+            index.contains_expressions and
+            not self.connection.features.supports_expression_indexes
+        ):
+            return None
         self.execute(index.remove_sql(model, self))
 
     def add_constraint(self, model, constraint):
@@ -992,12 +1003,17 @@ class BaseDatabaseSchemaEditor:
 
     def _create_index_sql(self, model, *, fields=None, name=None, suffix='', using='',
                           db_tablespace=None, col_suffixes=(), sql=None, opclasses=(),
-                          condition=None, include=None):
+                          condition=None, include=None, expressions=None):
         """
-        Return the SQL statement to create the index for one or several fields.
-        `sql` can be specified if the syntax differs from the standard (GIS
-        indexes, ...).
+        Return the SQL statement to create the index for one or several fields
+        or expressions. `sql` can be specified if the syntax differs from the
+        standard (GIS indexes, ...).
         """
+        fields = fields or []
+        expressions = expressions or []
+        compiler = Query(model, alias_cols=False).get_compiler(
+            connection=self.connection,
+        )
         tablespace_sql = self._get_index_tablespace_sql(model, fields, db_tablespace=db_tablespace)
         columns = [field.column for field in fields]
         sql_create_index = sql or self.sql_create_index
@@ -1014,7 +1030,11 @@ class BaseDatabaseSchemaEditor:
             table=Table(table, self.quote_name),
             name=IndexName(table, columns, suffix, create_index_name),
             using=using,
-            columns=self._index_columns(table, columns, col_suffixes, opclasses),
+            columns=(
+                self._index_columns(table, columns, col_suffixes, opclasses)
+                if columns
+                else Expressions(table, expressions, compiler, self.quote_value)
+            ),
             extra=tablespace_sql,
             condition=self._index_condition_sql(condition),
             include=self._index_include_sql(model, include),
@@ -1046,7 +1066,11 @@ class BaseDatabaseSchemaEditor:
             output.append(self._create_index_sql(model, fields=fields, suffix='_idx'))
 
         for index in model._meta.indexes:
-            output.append(index.create_sql(model, self))
+            if (
+                not index.contains_expressions or
+                self.connection.features.supports_expression_indexes
+            ):
+                output.append(index.create_sql(model, self))
         return output
 
     def _field_indexes_sql(self, model, field):

django/db/backends/mysql/features.py
@@ -41,6 +41,10 @@ class DatabaseFeatures(BaseDatabaseFeatures):
     """
     # Neither MySQL nor MariaDB support partial indexes.
     supports_partial_indexes = False
+    # COLLATE must be wrapped in parentheses because MySQL treats COLLATE as an
+    # indexed expression.
+    collate_as_index_expression = True
+
     supports_order_by_nulls_modifier = False
     order_by_nulls_first = True
     test_collations = {
@@ -59,6 +63,10 @@ class DatabaseFeatures(BaseDatabaseFeatures):
             'Running on MySQL requires utf8mb4 encoding (#18392).': {
                 'model_fields.test_textfield.TextFieldTests.test_emoji',
                 'model_fields.test_charfield.TestCharField.test_emoji',
+            },
+            "MySQL doesn't support functional indexes on a function that "
+            "returns JSON": {
+                'schema.tests.SchemaTests.test_func_index_json_key_transform',
             },
         }
         if 'ONLY_FULL_GROUP_BY' in self.connection.sql_mode:
@@ -224,4 +232,11 @@ class DatabaseFeatures(BaseDatabaseFeatures):
         return (
             not self.connection.mysql_is_mariadb and
             self.connection.mysql_version >= (8, 0, 1)
+        )
+
+    @cached_property
+    def supports_expression_indexes(self):
+        return (
+            not self.connection.mysql_is_mariadb and
+            self.connection.mysql_version >= (8, 0, 13)
         )

commit 82bd899532506df4ef6bdd4d02fa859e7eb51dc3

Fixed #33194 -- Fixed migrations when altering a field with functional indexes/unique constraints on SQLite.
This adjusts Expressions.rename_table_references() to only update alias
when needed.

Regression in 0ecbf49d43af2fc6944f7bffd6c635ba8a2617ca.

Co-authored-by: Simon Charette <charettes@users.noreply.github.com>

django/db/backends/ddl_references.py
@@ -212,11 +212,7 @@ class Expressions(TableColumns):
     def rename_table_references(self, old_table, new_table):
         if self.table != old_table:
             return
-        expressions = deepcopy(self.expressions)
-        self.columns = []
-        for col in self.compiler.query._gen_cols([expressions]):
-            col.alias = new_table
-        self.expressions = expressions
+        self.expressions = self.expressions.relabeled_clone({old_table: new_table})
         super().rename_table_references(old_table, new_table)
 
     def rename_column_references(self, table, old_column, new_column):

tests/backends/test_ddl_references.py
@@ -5,6 +5,7 @@ from django.db.backends.ddl_references import (
 from django.db.models import ExpressionList, F
 from django.db.models.functions import Upper
 from django.db.models.indexes import IndexExpression
+from django.db.models.sql import Query
 from django.test import SimpleTestCase, TransactionTestCase
 
 from .models import Person
@@ -228,6 +229,27 @@ class ExpressionsTests(TransactionTestCase):
             ),
             str(self.expressions),
         )
+
+    def test_rename_table_references_without_alias(self):
+        compiler = Query(Person, alias_cols=False).get_compiler(connection=connection)
+        table = Person._meta.db_table
+        expressions = Expressions(
+            table=table,
+            expressions=ExpressionList(
+                IndexExpression(Upper('last_name')),
+                IndexExpression(F('first_name')),
+            ).resolve_expression(compiler.query),
+            compiler=compiler,
+            quote_value=self.editor.quote_value,
+        )
+        expressions.rename_table_references(table, 'other')
+        self.assertIs(expressions.references_table(table), False)
+        self.assertIs(expressions.references_table('other'), True)
+        expected_str = '(UPPER(%s)), %s' % (
+            self.editor.quote_name('last_name'),
+            self.editor.quote_name('first_name'),
+        )
+        self.assertEqual(str(expressions), expected_str)
 
     def test_rename_column_references(self):
         table = Person._meta.db_table

tests/migrations/test_operations.py
@@ -2106,6 +2106,25 @@ class OperationTests(OperationTestBase):
         self.assertEqual(definition[1], [])
         self.assertEqual(definition[2], {'model_name': 'Pony', 'name': index_name})
 
+    @skipUnlessDBFeature('supports_expression_indexes')
+    def test_alter_field_with_func_index(self):
+        app_label = 'test_alfuncin'
+        index_name = f'{app_label}_pony_idx'
+        table_name = f'{app_label}_pony'
+        project_state = self.set_up_test_model(
+            app_label,
+            indexes=[models.Index(Abs('pink'), name=index_name)],
+        )
+        operation = migrations.AlterField('Pony', 'pink', models.IntegerField(null=True))
+        new_state = project_state.clone()
+        operation.state_forwards(app_label, new_state)
+        with connection.schema_editor() as editor:
+            operation.database_forwards(app_label, editor, project_state, new_state)
+        self.assertIndexNameExists(table_name, index_name)
+        with connection.schema_editor() as editor:
+            operation.database_backwards(app_label, editor, new_state, project_state)
+        self.assertIndexNameExists(table_name, index_name)
+
     def test_alter_field_with_index(self):
         """
         Test AlterField operation with an index to ensure indexes created via
@@ -2663,6 +2682,26 @@ class OperationTests(OperationTestBase):
             'model_name': 'Pony',
             'name': 'covering_pink_constraint_rm',
         })
+
+    def test_alter_field_with_func_unique_constraint(self):
+        app_label = 'test_alfuncuc'
+        constraint_name = f'{app_label}_pony_uq'
+        table_name = f'{app_label}_pony'
+        project_state = self.set_up_test_model(
+            app_label,
+            constraints=[models.UniqueConstraint('pink', 'weight', name=constraint_name)]
+        )
+        operation = migrations.AlterField('Pony', 'pink', models.IntegerField(null=True))
+        new_state = project_state.clone()
+        operation.state_forwards(app_label, new_state)
+        with connection.schema_editor() as editor:
+            operation.database_forwards(app_label, editor, project_state, new_state)
+        if connection.features.supports_expression_indexes:
+            self.assertIndexNameExists(table_name, constraint_name)
+        with connection.schema_editor() as editor:
+            operation.database_backwards(app_label, editor, new_state, project_state)
+        if connection.features.supports_expression_indexes:
+            self.assertIndexNameExists(table_name, constraint_name)
 
     def test_add_func_unique_constraint(self):
         app_label = 'test_adfuncuc'