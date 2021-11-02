usernameisnull/django

commit 75c564ea760167615773f508e6c3559ad1c74919

Refs #25367 -- Moved conditional expression wrapping to the Exact lookup.

django/db/backends/oracle/operations.py
@@ -6,7 +6,7 @@ from django.conf import settings
 from django.db.backends.base.operations import BaseDatabaseOperations
 from django.db.backends.utils import strip_quotes, truncate_name
 from django.db.models.expressions import Exists, ExpressionWrapper, RawSQL
-from django.db.models.query_utils import Q
+from django.db.models.sql.where import WhereNode
 from django.db.utils import DatabaseError
 from django.utils import timezone
 from django.utils.encoding import force_bytes, force_str
@@ -633,10 +633,10 @@ END;
         Oracle supports only EXISTS(...) or filters in the WHERE clause, others
         must be compared with True.
         """
-        if isinstance(expression, Exists):
+        if isinstance(expression, (Exists, WhereNode)):
             return True
-        if isinstance(expression, ExpressionWrapper) and isinstance(expression.expression, Q):
-            return True
+        if isinstance(expression, ExpressionWrapper) and expression.conditional:
+            return self.conditional_expression_supported_in_where_clause(expression.expression)
         if isinstance(expression, RawSQL) and expression.conditional:
             return True
         return False

django/db/models/lookups.py
@@ -274,6 +274,20 @@ class Exact(FieldGetDbPrepValueMixin, BuiltinLookup):
                 )
         return super().process_rhs(compiler, connection)
 
+    def as_sql(self, compiler, connection):
+        # Avoid comparison against direct rhs if lhs is a boolean value. That
+        # turns "boolfield__exact=True" into "WHERE boolean_field" instead of
+        # "WHERE boolean_field = True" when allowed.
+        if (
+            isinstance(self.rhs, bool) and
+            getattr(self.lhs, 'conditional', False) and
+            connection.ops.conditional_expression_supported_in_where_clause(self.lhs)
+        ):
+            lhs_sql, params = self.process_lhs(compiler, connection)
+            template = '%s' if self.rhs else 'NOT %s'
+            return template % lhs_sql, params
+        return super().as_sql(compiler, connection)
+
 
 @Field.register_lookup
 class IExact(BuiltinLookup):

django/db/models/sql/query.py
@@ -1222,12 +1222,7 @@ class Query(BaseExpression):
         if isinstance(filter_expr, dict):
             raise FieldError("Cannot parse keyword query as dict")
         if hasattr(filter_expr, 'resolve_expression') and getattr(filter_expr, 'conditional', False):
-            if connections[DEFAULT_DB_ALIAS].ops.conditional_expression_supported_in_where_clause(filter_expr):
-                condition = filter_expr.resolve_expression(self)
-            else:
-                # Expression is not supported in the WHERE clause, add
-                # comparison with True.
-                condition = self.build_lookup(['exact'], filter_expr.resolve_expression(self), True)
+            condition = self.build_lookup(['exact'], filter_expr.resolve_expression(self), True)
             clause = self.where_class()
             clause.add(condition, AND)
             return clause, []

commit d15b52b2257426618cbdf299f988d6350ee1fb0d

Fixed #32691 -- Made Exact lookup on BooleanFields compare directly to a boolean value on MySQL.
Performance regression in 75c564ea760167615773f508e6c3559ad1c74919.

Thanks Todor Velichkov for the report.

Co-authored-by: Mariusz Felisiak <felisiak.mariusz@gmail.com>

django/db/backends/mysql/operations.py
@@ -2,6 +2,7 @@ import uuid
 
 from django.conf import settings
 from django.db.backends.base.operations import BaseDatabaseOperations
+from django.db.models import Exists, ExpressionWrapper, Lookup
 from django.utils import timezone
 from django.utils.encoding import force_str
 
@@ -378,3 +379,14 @@ class DatabaseOperations(BaseDatabaseOperations):
             ):
                 lookup = 'JSON_UNQUOTE(%s)'
         return lookup
+
+    def conditional_expression_supported_in_where_clause(self, expression):
+        # MySQL ignores indexes with boolean fields unless they're compared
+        # directly to a boolean value.
+        if isinstance(expression, (Exists, Lookup)):
+            return True
+        if isinstance(expression, ExpressionWrapper) and expression.conditional:
+            return self.conditional_expression_supported_in_where_clause(expression.expression)
+        if getattr(expression, 'conditional', False):
+            return False
+        return super().conditional_expression_supported_in_where_clause(expression)

tests/lookup/tests.py
@@ -2,6 +2,7 @@ import collections.abc
 from datetime import datetime
 from math import ceil
 from operator import attrgetter
+from unittest import skipUnless
 
 from django.core.exceptions import FieldError
 from django.db import connection, models
@@ -926,6 +927,44 @@ class LookupTests(TestCase):
             list(Article.objects.filter(author=Author.objects.all()[:2]))
         with self.assertRaisesMessage(ValueError, msg):
             list(Article.objects.filter(author=Author.objects.all()[1:]))
+
+    @skipUnless(connection.vendor == 'mysql', 'MySQL-specific workaround.')
+    def test_exact_booleanfield(self):
+        # MySQL ignores indexes with boolean fields unless they're compared
+        # directly to a boolean value.
+        product = Product.objects.create(name='Paper', qty_target=5000)
+        Stock.objects.create(product=product, short=False, qty_available=5100)
+        stock_1 = Stock.objects.create(product=product, short=True, qty_available=180)
+        qs = Stock.objects.filter(short=True)
+        self.assertSequenceEqual(qs, [stock_1])
+        self.assertIn(
+            '%s = True' % connection.ops.quote_name('short'),
+            str(qs.query),
+        )
+
+    @skipUnless(connection.vendor == 'mysql', 'MySQL-specific workaround.')
+    def test_exact_booleanfield_annotation(self):
+        # MySQL ignores indexes with boolean fields unless they're compared
+        # directly to a boolean value.
+        qs = Author.objects.annotate(case=Case(
+            When(alias='a1', then=True),
+            default=False,
+            output_field=BooleanField(),
+        )).filter(case=True)
+        self.assertSequenceEqual(qs, [self.au1])
+        self.assertIn(' = True', str(qs.query))
+
+        qs = Author.objects.annotate(
+            wrapped=ExpressionWrapper(Q(alias='a1'), output_field=BooleanField()),
+        ).filter(wrapped=True)
+        self.assertSequenceEqual(qs, [self.au1])
+        self.assertIn(' = True', str(qs.query))
+        # EXISTS(...) shouldn't be compared to a boolean value.
+        qs = Author.objects.annotate(
+            exists=Exists(Author.objects.filter(alias='a1', pk=OuterRef('pk'))),
+        ).filter(exists=True)
+        self.assertSequenceEqual(qs, [self.au1])
+        self.assertNotIn(' = True', str(qs.query))
 
     def test_custom_field_none_rhs(self):
         """