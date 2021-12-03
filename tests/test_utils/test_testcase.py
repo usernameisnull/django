usernameisnull/django

commit 051bc0da8da9c358517253061d0215d2f7a7fcb4

Fixed #31395 -- Made setUpTestData enforce in-memory data isolation.
Since it's introduction in Django 1.8 setUpTestData has been suffering
from a documented but confusing caveat due to its sharing of attributes
assigned during its execution with all test instances.

By keeping track of class attributes assigned during the setUpTestData
phase its possible to ensure only deep copies are provided to test
instances on attribute retreival and prevent manual setUp gymnastic to
work around the previous lack of in-memory data isolation.

Thanks Adam Johnson for the extensive review.

tests/test_utils/models.py
@@ -12,4 +12,4 @@ class Person(models.Model):
 
 class PossessedCar(models.Model):
     car = models.ForeignKey(Car, models.CASCADE)
-    belongs_to = models.ForeignKey(Person, models.CASCADE)
+    belongs_to = models.ForeignKey(Person, models.CASCADE, related_name='possessed_cars')

tests/test_utils/test_testcase.py
@@ -1,7 +1,11 @@
-from django.db import IntegrityError, connections, transaction
-from django.test import TestCase, skipUnlessDBFeature
+from functools import wraps
 
-from .models import Car, PossessedCar
+from django.db import IntegrityError, connections, transaction
+from django.test import TestCase, ignore_warnings, skipUnlessDBFeature
+from django.test.testcases import TestData
+from django.utils.deprecation import RemovedInDjango41Warning
+
+from .models import Car, Person, PossessedCar
 
 
 class TestTestCase(TestCase):
@@ -38,3 +42,95 @@ class TestTestCase(TestCase):
         )
         with self.assertRaisesMessage(AssertionError, message):
             Car.objects.using('other').get()
+
+
+class NonDeepCopyAble:
+    def __deepcopy__(self, memo):
+        raise TypeError
+
+
+def assert_no_queries(test):
+    @wraps(test)
+    def inner(self):
+        with self.assertNumQueries(0):
+            test(self)
+    return inner
+
+
+class TestDataTests(TestCase):
+    # setUpTestData re-assignment are also wrapped in TestData.
+    jim_douglas = None
+
+    @classmethod
+    def setUpTestData(cls):
+        cls.jim_douglas = Person.objects.create(name='Jim Douglas')
+        cls.car = Car.objects.create(name='1963 Volkswagen Beetle')
+        cls.herbie = cls.jim_douglas.possessed_cars.create(
+            car=cls.car,
+            belongs_to=cls.jim_douglas,
+        )
+        cls.non_deepcopy_able = NonDeepCopyAble()
+
+    @assert_no_queries
+    def test_class_attribute_equality(self):
+        """Class level test data is equal to instance level test data."""
+        self.assertEqual(self.jim_douglas, self.__class__.jim_douglas)
+
+    @assert_no_queries
+    def test_class_attribute_identity(self):
+        """
+        Class level test data is not identical to instance level test data.
+        """
+        self.assertIsNot(self.jim_douglas, self.__class__.jim_douglas)
+
+    @assert_no_queries
+    def test_identity_preservation(self):
+        """Identity of test data is preserved between accesses."""
+        self.assertIs(self.jim_douglas, self.jim_douglas)
+
+    @assert_no_queries
+    def test_known_related_objects_identity_preservation(self):
+        """Known related objects identity is preserved."""
+        self.assertIs(self.herbie.car, self.car)
+        self.assertIs(self.herbie.belongs_to, self.jim_douglas)
+
+    @ignore_warnings(category=RemovedInDjango41Warning)
+    def test_undeepcopyable(self):
+        self.assertIs(self.non_deepcopy_able, self.__class__.non_deepcopy_able)
+
+    def test_undeepcopyable_warning(self):
+        msg = (
+            "Assigning objects which don't support copy.deepcopy() during "
+            "setUpTestData() is deprecated. Either assign the "
+            "non_deepcopy_able attribute during setUpClass() or setUp(), or "
+            "add support for deepcopy() to "
+            "test_utils.test_testcase.TestDataTests.non_deepcopy_able."
+        )
+        with self.assertRaisesMessage(RemovedInDjango41Warning, msg):
+            self.non_deepcopy_able
+
+    def test_repr(self):
+        self.assertEqual(
+            repr(TestData('attr', 'value')),
+            "<TestData: name='attr', data='value'>",
+        )
+
+
+class SetupTestDataIsolationTests(TestCase):
+    """
+    In-memory data isolation is respected for model instances assigned to class
+    attributes during setUpTestData.
+    """
+    @classmethod
+    def setUpTestData(cls):
+        cls.car = Car.objects.create(name='Volkswagen Beetle')
+
+    def test_book_name_deutsh(self):
+        self.assertEqual(self.car.name, 'Volkswagen Beetle')
+        self.car.name = 'VW sKäfer'
+        self.car.save()
+
+    def test_book_name_french(self):
+        self.assertEqual(self.car.name, 'Volkswagen Beetle')
+        self.car.name = 'Volkswagen Coccinelle'
+        self.car.save()

django/test/testcases.py
@@ -5,9 +5,10 @@ import posixpath
 import sys
 import threading
 import unittest
+import warnings
 from collections import Counter
 from contextlib import contextmanager
-from copy import copy
+from copy import copy, deepcopy
 from difflib import get_close_matches
 from functools import wraps
 from unittest.suite import _DebugResult
@@ -40,6 +41,7 @@ from django.test.utils import (
     CaptureQueriesContext, ContextList, compare_xml, modify_settings,
     override_settings,
 )
+from django.utils.deprecation import RemovedInDjango41Warning
 from django.utils.functional import classproperty
 from django.views.static import serve
 
@@ -1071,6 +1073,59 @@ def connections_support_transactions(aliases=None):
     return all(conn.features.supports_transactions for conn in conns)
 
 
+class TestData:
+    """
+    Descriptor to provide TestCase instance isolation for attributes assigned
+    during the setUpTestData() phase.
+
+    Allow safe alteration of objects assigned in setUpTestData() by test
+    methods by exposing deep copies instead of the original objects.
+
+    Objects are deep copied using a memo kept on the test case instance in
+    order to maintain their original relationships.
+    """
+    memo_attr = '_testdata_memo'
+
+    def __init__(self, name, data):
+        self.name = name
+        self.data = data
+
+    def get_memo(self, testcase):
+        try:
+            memo = getattr(testcase, self.memo_attr)
+        except AttributeError:
+            memo = {}
+            setattr(testcase, self.memo_attr, memo)
+        return memo
+
+    def __get__(self, instance, owner):
+        if instance is None:
+            return self.data
+        memo = self.get_memo(instance)
+        try:
+            data = deepcopy(self.data, memo)
+        except TypeError:
+            # RemovedInDjango41Warning.
+            msg = (
+                "Assigning objects which don't support copy.deepcopy() during "
+                "setUpTestData() is deprecated. Either assign the %s "
+                "attribute during setUpClass() or setUp(), or add support for "
+                "deepcopy() to %s.%s.%s."
+            ) % (
+                self.name,
+                owner.__module__,
+                owner.__qualname__,
+                self.name,
+            )
+            warnings.warn(msg, category=RemovedInDjango41Warning, stacklevel=2)
+            data = self.data
+        setattr(instance, self.name, data)
+        return data
+
+    def __repr__(self):
+        return '<TestData: name=%r, data=%r>' % (self.name, self.data)
+
+
 class TestCase(TransactionTestCase):
     """
     Similar to TransactionTestCase, but use `transaction.atomic()` to achieve
@@ -1119,12 +1174,16 @@ class TestCase(TransactionTestCase):
                     cls._rollback_atomics(cls.cls_atomics)
                     cls._remove_databases_failures()
                     raise
+        pre_attrs = cls.__dict__.copy()
         try:
             cls.setUpTestData()
         except Exception:
             cls._rollback_atomics(cls.cls_atomics)
             cls._remove_databases_failures()
             raise
+        for name, value in cls.__dict__.items():
+            if value is not pre_attrs.get(name):
+                setattr(cls, name, TestData(name, value))
 
     @classmethod
     def tearDownClass(cls):

commit 2c3edadef58836087be93a0b700d3b47f9b8bf3f

Fixed #33333 -- Fixed setUpTestData() crash with models.BinaryField on PostgreSQL.
This makes models.BinaryField pickleable on PostgreSQL.

Regression in 051bc0da8da9c358517253061d0215d2f7a7fcb4.

Thanks Adam Zimmerman for the report.

django/db/models/base.py
@@ -572,6 +572,16 @@ class Model(metaclass=ModelBase):
         """Hook to allow choosing the attributes to pickle."""
         state = self.__dict__.copy()
         state['_state'] = copy.copy(state['_state'])
+        # memoryview cannot be pickled, so cast it to bytes and store
+        # separately.
+        _memoryview_attrs = []
+        for attr, value in state.items():
+            if isinstance(value, memoryview):
+                _memoryview_attrs.append((attr, bytes(value)))
+        if _memoryview_attrs:
+            state['_memoryview_attrs'] = _memoryview_attrs
+            for attr, value in _memoryview_attrs:
+                state.pop(attr)
         return state
 
     def __setstate__(self, state):
@@ -591,6 +601,9 @@ class Model(metaclass=ModelBase):
                 RuntimeWarning,
                 stacklevel=2,
             )
+        if '_memoryview_attrs' in state:
+            for attr, value in state.pop('_memoryview_attrs'):
+                state[attr] = memoryview(value)
         self.__dict__.update(state)
 
     def _get_pk_val(self, meta=None):

tests/queryset_pickle/models.py
@@ -46,6 +46,7 @@ class Happening(models.Model):
     number1 = models.IntegerField(blank=True, default=standalone_number)
     number2 = models.IntegerField(blank=True, default=Numbers.get_static_number)
     event = models.OneToOneField(Event, models.CASCADE, null=True)
+    data = models.BinaryField(null=True)
 
 
 class Container:

tests/queryset_pickle/tests.py
@@ -16,6 +16,10 @@ class PickleabilityTestCase(TestCase):
     def assert_pickles(self, qs):
         self.assertEqual(list(pickle.loads(pickle.dumps(qs))), list(qs))
 
+    def test_binaryfield(self):
+        Happening.objects.create(data=b'binary data')
+        self.assert_pickles(Happening.objects.all())
+
     def test_related_field(self):
         g = Group.objects.create(name="Ponies Who Own Maybachs")
         self.assert_pickles(Event.objects.filter(group=g.id))

tests/test_utils/models.py
@@ -8,6 +8,7 @@ class Car(models.Model):
 class Person(models.Model):
     name = models.CharField(max_length=100)
     cars = models.ManyToManyField(Car, through='PossessedCar')
+    data = models.BinaryField(null=True)
 
 
 class PossessedCar(models.Model):

tests/test_utils/test_testcase.py
@@ -74,10 +74,15 @@ class TestDataTests(TestCase):
             belongs_to=cls.jim_douglas,
         )
 
+        cls.person_binary = Person.objects.create(name='Person', data=b'binary data')
+        cls.person_binary_get = Person.objects.get(pk=cls.person_binary.pk)
+
     @assert_no_queries
     def test_class_attribute_equality(self):
         """Class level test data is equal to instance level test data."""
         self.assertEqual(self.jim_douglas, self.__class__.jim_douglas)
+        self.assertEqual(self.person_binary, self.__class__.person_binary)
+        self.assertEqual(self.person_binary_get, self.__class__.person_binary_get)
 
     @assert_no_queries
     def test_class_attribute_identity(self):
@@ -85,6 +90,21 @@ class TestDataTests(TestCase):
         Class level test data is not identical to instance level test data.
         """
         self.assertIsNot(self.jim_douglas, self.__class__.jim_douglas)
+        self.assertIsNot(self.person_binary, self.__class__.person_binary)
+        self.assertIsNot(self.person_binary_get, self.__class__.person_binary_get)
+
+    @assert_no_queries
+    def test_binaryfield_data_type(self):
+        self.assertEqual(bytes(self.person_binary.data), b'binary data')
+        self.assertEqual(bytes(self.person_binary_get.data), b'binary data')
+        self.assertEqual(
+            type(self.person_binary_get.data),
+            type(self.__class__.person_binary_get.data),
+        )
+        self.assertEqual(
+            type(self.person_binary.data),
+            type(self.__class__.person_binary.data),
+        )
 
     @assert_no_queries
     def test_identity_preservation(self):