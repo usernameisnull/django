usernameisnull/django

commit a8d1df77e9429fb6f1f8097c7ef1a237daa19f6e

Fixed #33482 -- Fixed QuerySet filtering againts negated Exists() with empty queryset.
Thanks Tobias Bengfort for the report.

django/db/models/expressions.py
@@ -1211,13 +1211,18 @@ class Exists(Subquery):
 
     def as_sql(self, compiler, connection, template=None, **extra_context):
         query = self.query.exists(using=connection.alias)
-        sql, params = super().as_sql(
-            compiler,
-            connection,
-            template=template,
-            query=query,
-            **extra_context,
-        )
+        try:
+            sql, params = super().as_sql(
+                compiler,
+                connection,
+                template=template,
+                query=query,
+                **extra_context,
+            )
+        except EmptyResultSet:
+            if self.negated:
+                return '', ()
+            raise
         if self.negated:
             sql = 'NOT {}'.format(sql)
         return sql, params

tests/expressions/tests.py
@@ -1905,6 +1905,13 @@ class ExistsTests(TestCase):
         )
         self.assertNotIn('ORDER BY', captured_sql)
 
+    def test_negated_empty_exists(self):
+        manager = Manager.objects.create()
+        qs = Manager.objects.filter(
+            ~Exists(Manager.objects.none()) & Q(pk=manager.pk)
+        )
+        self.assertSequenceEqual(qs, [manager])
+
 
 class FieldTransformTests(TestCase):
 

commit 3865bff90b3e7fbfffebb62c89687b26cb959389

Refs #33482 -- Fixed QuerySet selecting and filtering againts negated Exists() with empty queryset.
Regression in a8d1df77e9429fb6f1f8097c7ef1a237daa19f6e.

django/db/models/expressions.py
@@ -1221,7 +1221,10 @@ class Exists(Subquery):
             )
         except EmptyResultSet:
             if self.negated:
-                return '', ()
+                features = compiler.connection.features
+                if not features.supports_boolean_expr_in_select_clause:
+                    return "1=1", ()
+                return compiler.compile(Value(True))
             raise
         if self.negated:
             sql = 'NOT {}'.format(sql)

tests/expressions/tests.py
@@ -1918,6 +1918,14 @@ class ExistsTests(TestCase):
         )
         self.assertSequenceEqual(qs, [manager])
 
+    def test_select_negated_empty_exists(self):
+        manager = Manager.objects.create()
+        qs = Manager.objects.annotate(
+            not_exists=~Exists(Manager.objects.none())
+        ).filter(pk=manager.pk)
+        self.assertSequenceEqual(qs, [manager])
+        self.assertIs(qs.get().not_exists, True)
+
 
 class FieldTransformTests(TestCase):
 