usernameisnull/django

commit 2dde6aec5a3dc4f1957ab53de9f034c93b386050

Fixed #27394 -- Added scientific notation support for big integers in floatformat filter.

django/template/defaultfilters.py
@@ -134,7 +134,7 @@ def floatformat(text, arg=-1):
     """
 
     try:
-        input_val = force_text(text)
+        input_val = repr(text)
         d = Decimal(input_val)
     except UnicodeEncodeError:
         return ''
@@ -165,7 +165,8 @@ def floatformat(text, arg=-1):
     try:
         # Set the precision high enough to avoid an exception, see #15789.
         tupl = d.as_tuple()
-        units = len(tupl[1]) - tupl[2]
+        units = len(tupl[1])
+        units += -tupl[2] if m else tupl[2]
         prec = abs(p) + units + 1
 
         # Avoid conversion to scientific notation by accessing `sign`, `digits`

tests/template_tests/filter_tests/test_floatformat.py
@@ -54,6 +54,10 @@ class FunctionTests(SimpleTestCase):
         self.assertEqual(floatformat('foo', 'bar'), '')
         self.assertEqual(floatformat('¿Cómo esta usted?'), '')
         self.assertEqual(floatformat(None), '')
+        self.assertEqual(floatformat(-1.323297138040798e+35, 2), '-132329713804079800000000000000000000.00')
+        self.assertEqual(floatformat(-1.323297138040798e+35, -2), '-132329713804079800000000000000000000')
+        self.assertEqual(floatformat(1.5e-15, 20), '0.00000000000000150000')
+        self.assertEqual(floatformat(1.5e-15, -20), '0.00000000000000150000')
 
     def test_zero_values(self):
         self.assertEqual(floatformat(0, 6), '0.000000')

commit 1c4cb3feb7562198fe2a26fbc732650c74963a36

Fixed #34098 -- Fixed loss of precision for Decimal values in floatformat filter.
Regression in 2dde6aec5a3dc4f1957ab53de9f034c93b386050.

django/template/defaultfilters.py
@@ -151,7 +151,7 @@ def floatformat(text, arg=-1):
             use_l10n = False
             arg = arg[:-1] or -1
     try:
-        input_val = repr(text)
+        input_val = str(text)
         d = Decimal(input_val)
     except InvalidOperation:
         try:

tests/template_tests/filter_tests/test_floatformat.py
@@ -56,6 +56,10 @@ class FunctionTests(SimpleTestCase):
         self.assertEqual(floatformat(0.12345, 2), "0.12")
         self.assertEqual(floatformat(Decimal("555.555"), 2), "555.56")
         self.assertEqual(floatformat(Decimal("09.000")), "9")
+        self.assertEqual(
+            floatformat(Decimal("123456.123456789012345678901"), 21),
+            "123456.123456789012345678901",
+        )
         self.assertEqual(floatformat("foo"), "")
         self.assertEqual(floatformat(13.1031, "bar"), "13.1031")
         self.assertEqual(floatformat(18.125, 2), "18.13")