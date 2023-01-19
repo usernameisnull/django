usernameisnull/django

commit 1c4cb3feb7562198fe2a26fbc732650c74963a36

Fixed #34098 -- Fixed loss of precision for Decimal values in floatformat filter.
Regression in 2dde6aec5a3dc4f1957ab53de9f034c93b386050.

django/template/defaultfilters.py
@@ -151,7 +151,7 @@ def floatformat(text, arg=-1):
             use_l10n = False
             arg = arg[:-1] or -1
     try:
-        input_val = repr(text)
+        input_val = str(text)
         d = Decimal(input_val)
     except InvalidOperation:
         try:

tests/template_tests/filter_tests/test_floatformat.py
@@ -56,6 +56,10 @@ class FunctionTests(SimpleTestCase):
         self.assertEqual(floatformat(0.12345, 2), "0.12")
         self.assertEqual(floatformat(Decimal("555.555"), 2), "555.56")
         self.assertEqual(floatformat(Decimal("09.000")), "9")
+        self.assertEqual(
+            floatformat(Decimal("123456.123456789012345678901"), 21),
+            "123456.123456789012345678901",
+        )
         self.assertEqual(floatformat("foo"), "")
         self.assertEqual(floatformat(13.1031, "bar"), "13.1031")
         self.assertEqual(floatformat(18.125, 2), "18.13")

commit b0c729747c7c5dd6d696baf344a1fe36f5439a3c

[4.2.x] Fixed #34272 -- Fixed floatformat crash on zero with trailing zeros to zero decimal places.
Regression in 1c4cb3feb7562198fe2a26fbc732650c74963a36.

Thanks Andrii Lahuta for the report.

Backport of 110a62309a66e02e122d3f5df49fa2ecad770a83 from main

django/template/defaultfilters.py
@@ -168,7 +168,7 @@ def floatformat(text, arg=-1):
     except (ValueError, OverflowError, InvalidOperation):
         return input_val
 
-    if not m and p < 0:
+    if not m and p <= 0:
         return mark_safe(
             formats.number_format(
                 "%d" % (int(d)),

tests/template_tests/filter_tests/test_floatformat.py
@@ -111,6 +111,8 @@ class FunctionTests(SimpleTestCase):
         self.assertEqual(
             floatformat(0.000000000000000000015, 20), "0.00000000000000000002"
         )
+        self.assertEqual(floatformat("0.00", 0), "0")
+        self.assertEqual(floatformat(Decimal("0.00"), 0), "0")
 
     def test_negative_zero_values(self):
         tests = [