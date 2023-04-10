usernameisnull/django

commit e52f8522bb819f103a950b6c63a0f9c159c5f6c3

Fixed #34069 -- Made LocaleMiddleware respect language from requests when i18n patterns are used.

tests/i18n/tests.py
@@ -2137,8 +2137,22 @@ class UnprefixedDefaultLanguageTests(SimpleTestCase):
         response = self.client.get("/fr/simple/")
         self.assertEqual(response.content, b"Oui")
 
-    def test_unprefixed_language_other_than_accept_language(self):
+    def test_unprefixed_language_with_accept_language(self):
+        """'Accept-Language' is respected."""
         response = self.client.get("/simple/", HTTP_ACCEPT_LANGUAGE="fr")
+        self.assertRedirects(response, "/fr/simple/")
+
+    def test_unprefixed_language_with_cookie_language(self):
+        """A language set in the cookies is respected."""
+        self.client.cookies.load({settings.LANGUAGE_COOKIE_NAME: "fr"})
+        response = self.client.get("/simple/")
+        self.assertRedirects(response, "/fr/simple/")
+
+    def test_unprefixed_language_with_non_valid_language(self):
+        response = self.client.get("/simple/", HTTP_ACCEPT_LANGUAGE="fi")
+        self.assertEqual(response.content, b"Yes")
+        self.client.cookies.load({settings.LANGUAGE_COOKIE_NAME: "fi"})
+        response = self.client.get("/simple/")
         self.assertEqual(response.content, b"Yes")
 
     def test_page_with_dash(self):
@@ -2214,10 +2228,7 @@ class CountrySpecificLanguageTests(SimpleTestCase):
 
     def test_get_language_from_request_null(self):
         lang = trans_null.get_language_from_request(None)
-        self.assertEqual(lang, "en")
-        with override_settings(LANGUAGE_CODE="de"):
-            lang = trans_null.get_language_from_request(None)
-            self.assertEqual(lang, "de")
+        self.assertEqual(lang, None)
 
     def test_specific_language_codes(self):
         # issue 11915

django/middleware/locale.py
@@ -16,28 +16,37 @@ class LocaleMiddleware(MiddlewareMixin):
 
     response_redirect_class = HttpResponseRedirect
 
+    def get_fallback_language(self, request):
+        """
+        Return the fallback language for the current request based on the
+        settings. If LANGUAGE_CODE is a variant not included in the supported
+        languages, get_fallback_language() will try to fallback to a supported
+        generic variant.
+
+        Can be overridden to have a fallback language depending on the request,
+        e.g. based on top level domain.
+        """
+        try:
+            return translation.get_supported_language_variant(settings.LANGUAGE_CODE)
+        except LookupError:
+            return settings.LANGUAGE_CODE
+
     def process_request(self, request):
         urlconf = getattr(request, "urlconf", settings.ROOT_URLCONF)
-        (
-            i18n_patterns_used,
-            prefixed_default_language,
-        ) = is_language_prefix_patterns_used(urlconf)
+        i18n_patterns_used, _ = is_language_prefix_patterns_used(urlconf)
         language = translation.get_language_from_request(
             request, check_path=i18n_patterns_used
         )
-        language_from_path = translation.get_language_from_path(request.path_info)
-        if (
-            not language_from_path
-            and i18n_patterns_used
-            and not prefixed_default_language
-        ):
-            language = settings.LANGUAGE_CODE
+        if not language:
+            language = self.get_fallback_language(request)
+
         translation.activate(language)
         request.LANGUAGE_CODE = translation.get_language()
 
     def process_response(self, request, response):
         language = translation.get_language()
         language_from_path = translation.get_language_from_path(request.path_info)
+        language_from_request = translation.get_language_from_request(request)
         urlconf = getattr(request, "urlconf", settings.ROOT_URLCONF)
         (
             i18n_patterns_used,
@@ -48,7 +57,7 @@ class LocaleMiddleware(MiddlewareMixin):
             response.status_code == 404
             and not language_from_path
             and i18n_patterns_used
-            and prefixed_default_language
+            and (prefixed_default_language or language_from_request)
         ):
             # Maybe the language code is missing in the URL? Try adding the
             # language prefix and redirecting to that URL.

django/utils/translation/trans_null.py
@@ -53,7 +53,7 @@ def check_for_language(x):
 
 
 def get_language_from_request(request, check_path=False):
-    return settings.LANGUAGE_CODE
+    return None
 
 
 def get_language_from_path(request):

django/utils/translation/trans_real.py
@@ -578,11 +578,7 @@ def get_language_from_request(request, check_path=False):
             return get_supported_language_variant(accept_lang)
         except LookupError:
             continue
-
-    try:
-        return get_supported_language_variant(settings.LANGUAGE_CODE)
-    except LookupError:
-        return settings.LANGUAGE_CODE
+    return None
 
 
 @functools.lru_cache(maxsize=1000)

commit 11e431bb2e846df52131ad0efbe17033b247a888

[4.2.x] Fixed #34455 -- Restored i18n_patterns() respect of prefix_default_language argument when fallback language is used.
Regression in e52f8522bb819f103a950b6c63a0f9c159c5f6c3.

Thanks Oussama Jarrousse for the report.

Backport of f66445aed96517b5167ea9ab5c50831dabcd8fc1 from main

django/urls/resolvers.py
@@ -23,7 +23,7 @@ from django.utils.datastructures import MultiValueDict
 from django.utils.functional import cached_property
 from django.utils.http import RFC3986_SUBDELIMS, escape_leading_slashes
 from django.utils.regex_helper import _lazy_re_compile, normalize
-from django.utils.translation import get_language
+from django.utils.translation import get_language, get_supported_language_variant
 
 from .converters import get_converter
 from .exceptions import NoReverseMatch, Resolver404
@@ -351,7 +351,8 @@ class LocalePrefixPattern:
     @property
     def language_prefix(self):
         language_code = get_language() or settings.LANGUAGE_CODE
-        if language_code == settings.LANGUAGE_CODE and not self.prefix_default_language:
+        default_language = get_supported_language_variant(settings.LANGUAGE_CODE)
+        if language_code == default_language and not self.prefix_default_language:
             return ""
         else:
             return "%s/" % language_code

django/utils/translation/__init__.py
@@ -17,6 +17,7 @@ __all__ = [
     "get_language_from_request",
     "get_language_info",
     "get_language_bidi",
+    "get_supported_language_variant",
     "check_for_language",
     "to_language",
     "to_locale",

tests/i18n/tests.py
@@ -2132,6 +2132,12 @@ class UnprefixedDefaultLanguageTests(SimpleTestCase):
         response = self.client.get("/simple/")
         self.assertEqual(response.content, b"Yes")
 
+    @override_settings(LANGUAGE_CODE="en-us")
+    def test_default_lang_fallback_without_prefix(self):
+        response = self.client.get("/simple/")
+        self.assertEqual(response.status_code, 200)
+        self.assertEqual(response.content, b"Yes")
+
     def test_other_lang_with_prefix(self):
         response = self.client.get("/fr/simple/")
         self.assertEqual(response.content, b"Oui")