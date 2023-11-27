usernameisnull/django

commit b878794803cb7e06ba894c1a2f9158fa9cd25162

Fixed #31331 -- Switched MySQL to group by selected primary keys.
MySQL 5.7.15 supports group by functional dependences so there is no
need to special case group by main table primary key anymore and
special case the ONLY_FULL_GROUP_BY sql mode.

django/db/backends/mysql/features.py
@@ -6,7 +6,7 @@ from django.utils.functional import cached_property
 
 class DatabaseFeatures(BaseDatabaseFeatures):
     empty_fetchmany_value = ()
-    allows_group_by_pk = True
+    allows_group_by_selected_pks = True
     related_fields_match_type = True
     # MySQL doesn't support sliced subqueries with IN/ALL/ANY/SOME.
     allow_sliced_subqueries_with_in = False
@@ -109,18 +109,6 @@ class DatabaseFeatures(BaseDatabaseFeatures):
                 "update.tests.AdvancedTests.test_update_ordered_by_m2m_annotation",
             },
         }
-        if "ONLY_FULL_GROUP_BY" in self.connection.sql_mode:
-            skips.update(
-                {
-                    "GROUP BY optimization does not work properly when "
-                    "ONLY_FULL_GROUP_BY mode is enabled on MySQL, see #31331.": {
-                        "aggregation.tests.AggregateTestCase."
-                        "test_aggregation_subquery_annotation_multivalued",
-                        "annotations.tests.NonAggregateAnnotationTestCase."
-                        "test_annotation_aggregate_with_m2o",
-                    },
-                }
-            )
         if self.connection.mysql_is_mariadb and (
             10,
             4,

django/db/models/sql/compiler.py
@@ -235,7 +235,9 @@ class SQLCompiler:
             expressions = [
                 expr
                 for expr in expressions
-                if expr in pks or getattr(expr, "alias", None) not in aliases
+                if expr in pks
+                or expr in having
+                or getattr(expr, "alias", None) not in aliases
             ]
         return expressions
 

commit c41e8298ebeb24f6f5f0242fc5b40a39928de1f9

[4.2.x] Fixed #34992 -- Fixed DatabaseFeatures.allows_group_by_selected_pks on MariaDB with ONLY_FULL_GROUP_BY sql mode.
Regression in b878794803cb7e06ba894c1a2f9158fa9cd25162.

Backport of a23036400b1a7eac99f015de538d848c50d8783d from main.

django/db/backends/mysql/features.py
@@ -6,7 +6,6 @@ from django.utils.functional import cached_property
 
 class DatabaseFeatures(BaseDatabaseFeatures):
     empty_fetchmany_value = ()
-    allows_group_by_selected_pks = True
     related_fields_match_type = True
     # MySQL doesn't support sliced subqueries with IN/ALL/ANY/SOME.
     allow_sliced_subqueries_with_in = False
@@ -347,4 +346,10 @@ class DatabaseFeatures(BaseDatabaseFeatures):
     def can_rename_index(self):
         if self.connection.mysql_is_mariadb:
             return self.connection.mysql_version >= (10, 5, 2)
+        return True
+
+    @cached_property
+    def allows_group_by_selected_pks(self):
+        if self.connection.mysql_is_mariadb:
+            return "ONLY_FULL_GROUP_BY" not in self.connection.sql_mode
         return True

tests/backends/mysql/test_features.py
@@ -42,3 +42,21 @@ class TestFeatures(TestCase):
             _connection.sql_mode = {"NO_AUTO_VALUE_ON_ZERO"}
             database_features = DatabaseFeatures(_connection)
             self.assertIs(database_features.allows_auto_pk_0, True)
+
+    def test_allows_group_by_selected_pks(self):
+        with mock.MagicMock() as _connection:
+            _connection.mysql_is_mariadb = False
+            database_features = DatabaseFeatures(_connection)
+            self.assertIs(database_features.allows_group_by_selected_pks, True)
+
+        with mock.MagicMock() as _connection:
+            _connection.mysql_is_mariadb = False
+            _connection.sql_mode = {}
+            database_features = DatabaseFeatures(_connection)
+            self.assertIs(database_features.allows_group_by_selected_pks, True)
+
+        with mock.MagicMock() as _connection:
+            _connection.mysql_is_mariadb = True
+            _connection.sql_mode = {"ONLY_FULL_GROUP_BY"}
+            database_features = DatabaseFeatures(_connection)
+            self.assertIs(database_features.allows_group_by_selected_pks, False)